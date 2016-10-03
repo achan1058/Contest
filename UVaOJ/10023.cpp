31d71d51303fdd33f8a46f95e7e222268542ec1b
Aaron <Aaron@AARON-PC>
2016-10-03 04:24:37 -0400

    Created Python calling stub for 10023.

--- UVaOJ/10023.cpp
+++ UVaOJ/10023.cpp
@@ -1,29 +1,5 @@
 #include <bits/stdc++.h>
-#include "header.h"
 
 int main() {
-	int n, mx = 0;
-	cin >> n;
-	vvi arr = mi(n, n + 1, 0);
-	rep(i, 0, n) {
-		arr[i][0] = 0;
-		rep(j, 0, n) {
-			cin >> arr[i][j + 1];
-			arr[i][j + 1] += arr[i][j];
-		}
-	}
-
-	rep(j1, 0, n) {
-		rep(j2, j1, n) {
-			int sum = 0;
-			rep(i, 0, n) {
-				sum += arr[i][j2 + 1] - arr[i][j1];
-				mx = max(mx, sum);
-
-				if (sum < 0)
-					sum = 0;
-			}
-		}
-	}
-	printf("%d\n", mx);
+	system("python UVaOJ/10023.py < input.txt");
 }
