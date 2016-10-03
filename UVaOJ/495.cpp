749a4341df97990b8fd6917ab1d612b734243a7c
Aaron <Aaron@AARON-PC>
2016-10-03 19:50:22 -0400

    Used python instead of bigint.

---
+++ UVaOJ/495.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main() {
+	system("python UVaOJ/495.py < input.txt");
+}
---
+++ UVaOJ/495.py
@@ -0,0 +1,12 @@
+a = [0] * 5001
+a[1] = 1
+for i in range(2, 5001):
+    a[i] = a[i - 1] + a[i - 2]
+    
+while True:
+    try:
+        n = int(input())
+    except EOFError:
+        exit()
+    
+    print("The Fibonacci number for %d is %s" % (n, a[n]))
