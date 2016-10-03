f1acc2f6300a0e5641caa6cd9783d8480aa1b785
Aaron <Aaron@AARON-PC>
2016-10-03 15:37:05 -0400

    Changed from using bigint to python.

---
+++ UVaOJ/424.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main() {
+	system("python UVaOJ/424.py < input.txt");
+}
---
+++ UVaOJ/424.py
@@ -0,0 +1,7 @@
+total = 0
+while True:
+    v = int(input())
+    if v == 0:
+        print(total)
+        exit()
+    total += v
