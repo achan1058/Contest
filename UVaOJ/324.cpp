46fc8b797b4cde49b136e2fca3f2654fa68e0722
Aaron <Aaron@AARON-PC>
2016-10-03 11:30:54 -0400

    Changed 324 to using python instead of custom factorial and bigint class.

---
+++ UVaOJ/324.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main() {
+	system("python UVaOJ/324.py < input.txt");
+}
---
+++ UVaOJ/324.py
@@ -0,0 +1,16 @@
+import math
+
+while True:
+    n = int(input())
+    if n == 0:
+        exit()
+    f = str(math.factorial(n))
+
+    print("%d! --" % n)
+    for i in range(0, 10):
+        print("   (%d)%5d" % (i, f.count(str(i))), end="")
+
+        if i % 5 == 4:
+            print()
+        else:
+            print(" ", end="")
