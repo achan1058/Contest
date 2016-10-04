6120b1c6f693bb6abcf7c5f5546bcc110c4382e7
Aaron <Aaron@AARON-PC>
2016-10-04 00:34:23 -0400

    Changed to python powmod.

---
+++ UVaOJ/1230.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main() {
+	system("python UVaOJ/1230.py < input.txt");
+}
---
+++ UVaOJ/1230.py
@@ -0,0 +1,4 @@
+n = int(input())
+for i in range(0, n):
+    x,y,z = map(int,input().split())
+    print(pow(x,y,z))
