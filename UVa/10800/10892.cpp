f721603b30267d6a2229690ad4dc54b7a4734e5c
Aaron <Aaron@AARON-PC>
2016-10-07 19:43:55 -0400

    factoring, LCM

--- UVa/10800/10892.cpp
+++ UVa/10800/10892.cpp
@@ -4,13 +4,9 @@
 int main() {
 	int n;
 	while (cin >> n && n != 0) {
-		vi primes = factor(n);
-		map<int, int> fact;
-		irep(p, primes)
-			fact[p]++;
-
+		auto primes = tally(factor(n));
 		int d = 1;
-		irep(p, fact)
+		irep(p, primes)
 			d *= 2 * p.second + 1;
 		printf("%d %d\n", n, (d + 1) / 2);
 	}
