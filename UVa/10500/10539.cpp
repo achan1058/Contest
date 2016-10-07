c7b7a772a41af6d4ead5084a207958eaef582a96
Aaron <Aaron@AARON-PC>
2016-10-07 14:42:38 -0400

    prime sieve

---
+++ UVa/10500/10539.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+
+int main() {
+	vi primes = sieve(1000000);
+	vl powers;
+	int N;
+	ll b1, b2;
+	cin >> N;
+	irep(p, primes) {
+		for (ll i = (ll)p * p; i <= 1000000000000ll; i *= p)
+			powers.pb(i);
+	}
+
+	sort(all(powers));
+	rep(X, 0, N) {
+		cin >> b1 >> b2;
+		printf("%d\n", upper_bound(all(powers), b2) - lower_bound(all(powers), b1));
+	}
+}
