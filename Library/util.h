0d6313710bc96ada67940cf28c95c13b38fb2ac5
Aaron <Aaron@AARON-PC>
2016-10-07 13:19:26 -0400

    Added Index class

---
+++ Library/util.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "header.h"
+
+// maps elements of a set to [1..n]
+template<class T>
+class Index {
+	unordered_map<T, int> index;
+	vector<T> label;
+public:
+	inline T getL(int ind) const { return label[ind]; }
+	inline int size() const { return sz(label); }
+	inline int getI(const T& e) {
+		if (index.find(e) == index.end()) {
+			int t = sz(index);
+			index[e] = t;
+			label.pb(e);
+		}
+		return index[e];
+	}
+};
