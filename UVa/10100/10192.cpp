714bced6941622f314a0d652e065fdb06b15df88
Aaron <Aaron@AARON-PC>
2016-10-07 01:33:39 -0400

    LCS

---
+++ UVa/10100/10192.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+
+int main() {
+	string s1, s2;
+	int X = 1;
+	while (getline(cin, s1) && getline(cin, s2) && s1[0] != '#') {
+		vector<char> t1(all(s1)), t2(all(s2));
+		int output = sz(lcs(t1, t2));
+		printf("Case #%d: you can visit at most %d cities.\n", X, output);
+		X++;
+	}
+}
