efb87dcfced7f448363ecc73f2e5b2ba874977f8
Aaron <Aaron@AARON-PC>
2016-10-06 23:54:54 -0400

    big int, math, closed form

---
+++ UVa/10200/10213.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main() {
+	system("python UVa/10200/10213.py < input.txt");
+}
---
+++ UVa/10200/10213.py
@@ -0,0 +1,5 @@
+T = int(input())
+
+for X in range(T):
+    n = int(input())
+    print((n*n*n*n - 6*n*n*n + 23*n*n - 18*n + 24) // 24)
