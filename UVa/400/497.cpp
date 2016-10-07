37f8e9e5e256bd51ba5e03dcf4c3c9f2353f6e12
Aaron <Aaron@AARON-PC>
2016-10-07 12:11:26 -0400

    LIS

---
+++ UVa/400/497.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+
+int main() {
+	int N, X = 1;
+	string s;
+	cin >> N;
+	getline(cin, s);
+	getline(cin, s);
+	vi missiles;
+	while (X <= N) {
+		if (getline(cin, s) && s != "") {
+			missiles.pb(stoi(s));
+			continue;
+		}
+		printf("%s", X > 1 ? "\n" : "");
+		vi result = lis(missiles);
+		printf("Max hits: %d\n", sz(result));
+		irep(v, result)
+			printf("%d\n", v);
+
+		X++;
+		missiles.clear();
+	}
+}
