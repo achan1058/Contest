7b46cac6eb643a28dd375bcab84825bd5b3ec658
Aaron <Aaron@AARON-PC>
2016-10-06 23:50:20 -0400

    DP, big int

---
+++ UVa/10100/10198.py
@@ -0,0 +1,14 @@
+import bisect
+
+fib = [1, 2]
+while fib[-1] < 10**100:
+    fib.append(fib[-1] + fib[-2])
+while True:
+    try:
+        small,big = map(int,input().split())
+    except EOFError:
+        exit()
+    if small == 0 and big == 0:
+        exit()
+
+    print(bisect.bisect_right(fib, big) - bisect.bisect_left(fib, small))
