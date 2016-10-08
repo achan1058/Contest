59f3f4474989d2dbde11ecac7d63aa4af509154f
Aaron <Aaron@AARON-PC>
2016-10-08 01:45:16 -0400

    max flow

---
+++ UVa/800/820.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "max_flow.h"
+
+int main() {
+	int n, s, t, e, v1, v2, c, X = 1;
+	while (cin >> n && n != 0) {
+		vvi graph = mi(n, n, 0);
+		cin >> s >> t >> e;
+		s--;
+		t--;
+		rep(i, 0, e) {
+			cin >> v1 >> v2 >> c;
+			graph[v1 - 1][v2 - 1] += c;
+			graph[v2 - 1][v1 - 1] += c;
+		}
+		printf("Network %d\n", X);
+		printf("The bandwidth is %d.\n\n", get<0>(pushRelabel(graph, s, t)));
+		X++;
+	}
+}
