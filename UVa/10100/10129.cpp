7e7182b9c3e87e1bedaa35ccac74efeafcf74f7f
Aaron <Aaron@AARON-PC>
2016-10-08 17:00:23 -0400

    eularian path

--- UVa/10100/10129.cpp
+++ UVa/10100/10129.cpp
@@ -1,63 +1,22 @@
 #include <bits/stdc++.h>
-#include "shortest_path.h"
+#include "eularian.h"
 
 int main() {
 	int N, n;
+	string s;
 	cin >> N;
 	rep(X, 0, N) {
 		cin >> n;
-		vs strs(n);
-		vi count(26);
-		vb used(26);
-		vvi graph = mi(26, 26, inf);
-		int begin = -1, end = -1;
-		rep(i, 0, 26)
-			graph[i][i] = 0;
-		irep(s, strs) {
+		vvi graph = mi(26, 26, 0);
+		rep(i, 0, n) {
 			cin >> s;
 			int a = s[0] - 'a', b = s.back() - 'a';
-			used[a] = true;
-			count[a]++;
-			count[b]--;
-			graph[a][b] = 1;
+			graph[a][b]++;
 		}
-		rep(i, 0, 26) {
-			if (abs(count[i]) > 1) {
-				begin = inf;
-				break;
-			} else if (count[i] == 1) {
-				if (begin != -1) {
-					begin = inf;
-					break;
-				} else {
-					begin = i;
-				}
-			} else if (count[i] == -1)
-				end = i;
-		}
-		if (begin == inf)
+
+		if (sz(eularianPath(graph, true)) == n + 1)
+			printf("Ordering is possible.\n");
+		else
 			printf("The door cannot be opened.\n");
-		else {
-			if (begin == -1) {
-				rep(i, 0, 26) {
-					if (used[i]) {
-						begin = i;
-						break;
-					}
-				}
-			}
-			vvi dist = floydWarshall(graph).x;
-			bool bad = false;
-			rep(i, 0, 26) {
-				if (used[i] && dist[begin][i] == inf) {
-					bad = true;
-					break;
-				}
-			}
-			if (bad)
-				printf("The door cannot be opened.\n");
-			else
-				printf("Ordering is possible.\n");
-		}
 	}
 }
