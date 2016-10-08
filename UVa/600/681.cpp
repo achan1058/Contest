b7b8a85e6986caf7a57191ace5b88ec2835c84ef
Aaron <Aaron@AARON-PC>
2016-10-08 12:33:44 -0400

    convex hull

---
+++ UVa/600/681.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "graham.h"
+
+int main() {
+	int N, n;
+	cin >> N;
+	printf("%d\n", N);
+	rep(X, 0, N) {
+		cin >> n;
+		printf("%s", X > 0 ? "-1\n" : "");
+		vector<pti> pts(n);
+		irep(p, pts)
+			cin >> p.y >> p.x;
+		vector<pti> hull = graham(pts);
+		printf("%d\n%d %d\n", sz(hull) + 1, hull[0].y, hull[0].x);
+		rrep(i, sz(hull) - 1, 0)
+			printf("%d %d\n", hull[i].y, hull[i].x);
+		cin >> n;
+	}
+}
