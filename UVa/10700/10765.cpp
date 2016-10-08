43eaaf0e70945817a442842f92bf6233c0ba481d
Aaron <Aaron@AARON-PC>
2016-10-08 02:00:18 -0400

    strongly connected component

---
+++ UVa/10700/10765.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include "components.h"
+
+int main() {
+	int n, m, v1, v2;
+	while (cin >> n >> m && (n | m) != 0) {
+		vvi graph(n);
+		while (cin >> v1 >> v2 && v1 != -1) {
+			graph[v1].pb(v2);
+			graph[v2].pb(v1);
+		}
+
+		vvi blocks = twoConnected(graph);
+		vi count(sz(graph), 0);
+		vector<pii> ans;
+
+		irep(b, blocks) {
+			irep(v, b)
+				count[v]++;
+		}
+		rep(i, 0, sz(graph))
+			ans.pb({ -count[i], i });
+
+		sort(all(ans));
+		rep(i, 0, m)
+			printf("%d %d\n", ans[i].y, -ans[i].x);
+		printf("\n");
+	}
+}
