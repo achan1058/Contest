ddcf9de69b3a07aa9efb2cf4b5097be2bf386968
Aaron <Aaron@AARON-PC>
2016-10-07 22:53:28 -0400

    factoring

---
+++ UVa/200/294.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+
+int main() {
+	int N, L, U;
+	cin >> N;
+	rep(X, 0, N) {
+		cin >> L >> U;
+		int max_factor = 0, factor_index = 0;
+		rep(n, L, U + 1) {
+			int numfactor = numFactors(factor(n));
+			if (numfactor > max_factor) {
+				max_factor = numfactor;
+				factor_index = n;
+			}
+		}
+		printf("Between %d and %d, %d has a maximum of %d divisors.\n", L, U, factor_index, max_factor);
+	}
+}
