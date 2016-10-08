6344d2735d39b03154c4bb829cb1dc6b922f0b1a
Aaron <Aaron@AARON-PC>
2016-10-08 13:59:45 -0400

    shortest path, min weight

---
+++ UVa/10000/10099.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "shortest_path.h"
+
+int main() {
+	int X = 1, n, e, v1, v2, cost;
+	while (cin >> n >> e && (n | e) != 0) {
+		vvi graph = mi(n, n, 0);
+		rep(i, 0, e) {
+			cin >> v1 >> v2 >> cost;
+			graph[v1 - 1][v2 - 1] = -cost;
+			graph[v2 - 1][v1 - 1] = -cost;
+		}
+		vvi result = floydWarshall(graph, 0, true).x;
+		cin >> v1 >> v2 >> cost;
+		printf("Scenario #%d\n", X);
+		printf("Minimum Number of Trips = %d\n\n", (cost - 1) / (-result[v1 - 1][v2 - 1] - 1) + 1);
+		X++;
+	}
+}
