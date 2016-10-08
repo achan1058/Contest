29a0fab5ab7605eb2237678f77737f81062b359c
Aaron <Aaron@AARON-PC>
2016-10-07 20:19:45 -0400

    factoring, LCM

---
+++ UVa/10700/10791.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+
+int main() {
+	int X = 1;
+	ll n;
+	while (cin >> n && n != 0) {
+		if (n == 1) {
+			printf("Case %d: 2\n", X);
+			X++;
+			continue;
+		}
+
+		auto primes = tally(factor(n));
+		ll ans = 0;
+		irep(p, primes) {
+			ll add = 1;
+			rep(i, 0, p.y)
+				add *= p.x;
+			ans += add;
+		}
+		printf("Case %d: %lld\n", X, ans + (sz(primes) == 1 ? 1 : 0));
+		X++;
+	}
+}
