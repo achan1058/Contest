8c33c52b00347e86a53ebd7431ec93149e2d36f6
Aaron <Aaron@AARON-PC>
2016-10-08 02:16:27 -0400

    two connected components, articulation points

---
+++ UVa/300/315.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "components.h"
+
+int main() {
+	int n;
+	string s;
+	while (cin >> n && n != 0) {
+		vvi graph(n);
+		getline(cin, s);
+		while (getline(cin, s) && s != "0") {
+			stringstream ss;
+			int v1, v2;
+			ss << s;
+			ss >> v1;
+			while (ss >> v2) {
+				graph[v1 - 1].pb(v2 - 1);
+				graph[v2 - 1].pb(v1 - 1);
+			}
+		}
+		printf("%d\n", sz(getArticulationPoints(graph)));
+	}
+}
