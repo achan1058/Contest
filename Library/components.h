deb4c66f6f97bf7f0705992132e239771793b1d3
Aaron <Aaron@AARON-PC>
2016-10-08 01:59:32 -0400

    added twoConnected() and getArticulationPoint()

---
+++ Library/components.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "header.h"
+
+void twoConnectedHelper(const vvi& graph, int n, int u, int& count, vi& num, vi& low, stack<int>& stk, vvi& blocks) {
+	num[u] = low[u] = count++;
+	stk.push(u);
+
+	irep(v, graph[u]) {
+		if (num[v] == 0) {
+			twoConnectedHelper(graph, n, v, count, num, low, stk, blocks);
+			low[u] = min(low[u], low[v]);
+			if (low[v] >= num[u]) {
+				blocks.pb({ u, v });
+				while (stk.top() != v) {
+					blocks.back().pb(stk.top());
+					stk.pop();
+				}
+				stk.pop();
+			}
+		} else {
+			low[u] = min(low[u], num[v]);
+		}
+	}
+}
+// return bridges and strongly connected blocks
+vvi twoConnected(const vvi& graph) {
+	int n = sz(graph), count = 1;
+	vvi blocks;
+	vi num(n, 0), low(n);
+	stack<int> stk;
+	rep(i, 0, n) {
+		if (num[i] == 0)
+			twoConnectedHelper(graph, n, i, count, num, low, stk, blocks);
+	}
+	return blocks;
+}
+
+vi getArticulationPoints(const vvi& blocks) {
+	vi count, pt;
+	irep(b, blocks) {
+		irep(v, b) {
+			if (v >= sz(count))
+				count.resize(v + 1);
+
+			count[v]++;
+		}
+	}
+	rep(i, 0, sz(count)) {
+		if (count[i] > 1)
+			pt.pb(i);
+	}
+	return pt;
+}
