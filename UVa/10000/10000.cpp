894335f857b81a4aad7b736cf6739e39a36df8fb
Aaron <Aaron@AARON-PC>
2016-10-09 17:24:44 -0400

    topological sort, longest DAG path

--- UVa/10000/10000.cpp
+++ UVa/10000/10000.cpp
@@ -4,11 +4,11 @@
 int main() {
 	int n, s, X = 1, e1, e2;
 	while (cin >> n >> s && n != 0) {
-		AdjList<int> graph(n);
+		vvi graph(n);
 		while (cin >> e1 >> e2 && (e1 != 0 || e2 != 0))
-			graph.push(e1 - 1, e2 - 1);
-		pair<int, vi> result = longestDAG(graph, s - 1, -1);
-		printf("Case %d: The longest path from %d has length %d, finishing at %d.\n\n", X, s, result.x, result.y.back() + 1);
+			graph[e1 - 1].pb(e2 - 1);
+		vi result = longestDAG(graph, s - 1, -1);
+		printf("Case %d: The longest path from %d has length %d, finishing at %d.\n\n", X, s, sz(result) - 1, result.back() + 1);
 		X++;
 	}
 }
