4618ae4bb92a28b34d4aa2106279c6724e401a1f
Aaron <Aaron@AARON-PC>
2016-10-09 01:24:29 -0400

    connected components, union find

--- UVa/10500/10583.cpp
+++ UVa/10500/10583.cpp
@@ -5,7 +5,7 @@ int main() {
 	int v, e, X = 1, e1, e2;
 	while (cin >> v >> e && (v | e) != 0) {
 		UnionFind u(v);
-		int e1, e2, num_comp = v;
+		int num_comp = v;
 		rep(i, 0, e) {
 			cin >> e1 >> e2;
 			e1--;
