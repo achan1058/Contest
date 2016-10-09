c4f6f9fff3ff3f737685a8091c0663cb1827b8fc
Aaron <Aaron@AARON-PC>
2016-10-09 00:23:15 -0400

    union find

---
+++ UVa/400/459.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "union_find.h"
+
+int main() {
+	int N;
+	char en;
+	string s;
+	cin >> N;
+	rep(X, 0, N) {
+		printf("%s", X > 0 ? "\n" : "");
+		cin >> en;
+		int n = en - 'A' + 1;
+		UnionFind un(n);
+		getline(cin, s);
+		while (getline(cin, s) && s != "")
+			un.join(s[0] - 'A', s[1] - 'A');
+
+		printf("%d\n", sz(un.getComponentSizes()));
+	}
+}
