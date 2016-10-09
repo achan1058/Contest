e47d79b0e8b0a8e08a4060c17ec306f2da8ed4ce
Aaron <Aaron@AARON-PC>
2016-10-09 00:22:50 -0400

    union find

--- UVa/700/793.cpp
+++ UVa/700/793.cpp
@@ -2,20 +2,19 @@
 #include "union_find.h"
 
 int main() {
-	int N, n;
+	int N, n, v1, v2;
+	char c;
 	cin >> N;
-	string s, s0, s1, s2;
+	string s;
 	rep(X, 0, N) {
 		printf("%s", X > 0 ? "\n" : "");
 		cin >> n;
 		getline(cin, s);
-		UnionFind un(n);
+		UnionFind un(n + 1);
 		int yes = 0, no = 0;
 		while (getline(cin, s) && s != "") {
-			stringstream ss(s);
-			ss >> s0 >> s1 >> s2;
-			int v1 = stoi(s1) - 1, v2 = stoi(s2) - 1;
-			if (s0 == "c")
+			stringstream(s) >> c >> v1 >> v2;
+			if (c == 'c')
 				un.join(v1, v2);
 			else if (un.find(v1) == un.find(v2))
 				yes++;
