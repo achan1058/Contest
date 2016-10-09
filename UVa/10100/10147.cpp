e7a694075ff50b474f6c5aac60c47d34c8cbe188
Aaron <Aaron@AARON-PC>
2016-10-09 19:28:13 -0400

    minimum spanning tree, kruskal

--- UVa/10100/10147.cpp
+++ UVa/10100/10147.cpp
@@ -11,7 +11,7 @@ int main() {
 		vector<pti> points(n);
 		EdgeList<int> edges;
 		rep(i, 0, n) {
-			cin >> points[i].x >> points[i].y;
+			cin >> points[i];
 			rep(j, 0, i)
 				edges.push(i, j, points[i].dist2(points[j]));
 		}
@@ -21,18 +21,16 @@ int main() {
 			cin >> v1 >> v2;
 			edges.push(v1 - 1, v2 - 1, 0);
 		}
-		EdgeList<int> tree = get<2>(kruskal(edges));
+
+		auto result = kruskal(edges);
+		EdgeList<int> tree = get<2>(result);
 		if (X > 0)
 			printf("\n");
-
-		bool newEdges = false;
-		for (int i = 0; i < tree.numE(); i++) {
-			if (get<2>(tree[i]) > 0) {
-				printf("%d %d\n", get<0>(tree[i]) + 1, get<1>(tree[i]) + 1);
-				newEdges = true;
-			}
-		}
-		if (!newEdges)
+		if (get<0>(result) == 0)
 			printf("No new highways need\n");
+		for (int i = 0; i < tree.numE(); i++) {
+			if (get<2>(tree[i]) > 0)
+				printf("%d %d\n", get<0>(tree[i]) + 1, get<1>(tree[i]) + 1);
+		}
 	}
 }
