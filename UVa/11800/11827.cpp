2798ad70c561e7ae93251ad35a62ae01d1778218
Aaron <Aaron@AARON-PC>
2016-10-09 02:06:34 -0400

    gcd

---
+++ UVa/11800/11827.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main() {
+	system("python UVa/11800/11827.py < input.txt");
+}
---
+++ UVa/11800/11827.py
@@ -0,0 +1,12 @@
+import fractions
+
+N = int(input())
+for X in range (N):
+    nums = [int(x) for x in input().split()]
+    maxgcd = 0
+
+    for i in range (len(nums)):
+        for j in range(i + 1, len(nums)):
+            maxgcd = max(maxgcd, fractions.gcd(nums[i], nums[j]))
+    print(maxgcd)
+    
