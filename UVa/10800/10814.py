1962595905ab948a857b227513458da5d5f2670e
Aaron <Aaron@AARON-PC>
2016-10-09 02:23:53 -0400

    fractions

--- UVa/10800/10814.py
+++ UVa/10800/10814.py
@@ -1,6 +1,6 @@
-import fractions
+from fractions import Fraction
 
 T = int(input())
 for X in range(T):
-    f = fractions.Fraction(input().replace(" ", ""))
+    f = Fraction(input().replace(" ", ""))
     print("%d / %d" % (f.numerator, f.denominator))
