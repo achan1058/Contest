98337123fbd26a73632f59f728ddb1f2c69cf672
Aaron <Aaron@AARON-PC>
2016-10-09 13:07:39 -0400

    topological sort

---
+++ UVa/11600/11686.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "topo_sort.h"
+
+int main() {
+	int v, e, e1, e2;
+	while (cin >> v >> e && (v | e) != 0) {
+		vvi graph(v);
+		rep(i, 0, e) {
+			cin >> e1 >> e2;
+			graph[e1 - 1].pb(e2 - 1);
+		}
+		vi result = topoSort(graph);
+		if (result.empty())
+			printf("IMPOSSIBLE\n");
+		else {
+			irep(v, result)
+				printf("%d\n", v + 1);
+		}
+	}
+}
