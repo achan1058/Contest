49a0a621fa3260e71fd84b8b29122ed3f61662ab
Aaron <Aaron@AARON-PC>
2016-10-08 20:36:08 -0400

    shortest path

---
+++ UVa/300/341.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "shortest_path.h"
+
+int main() {
+	int n, r, v, d, X = 1;
+	while (cin >> n && n != 0) {
+		vvi grid = mi(n, n, inf);
+		rep(i, 0, n) {
+			cin >> r;
+			rep(j, 0, r) {
+				cin >> v >> d;
+				grid[i][v - 1] = d;
+			}
+		}
+
+		pair<vvi, vvi> dist = floydWarshall(grid);
+		cin >> r >> v;
+		vi path = getPath(dist.y[r - 1], r - 1, v - 1);
+		printf("Case %d: Path =", X);
+		irep(p, path)
+			printf(" %d", p + 1);
+		printf("; %d second delay\n", dist.x[r - 1][v - 1]);
+		X++;
+	}
+}
