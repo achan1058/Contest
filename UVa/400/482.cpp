18fad6f6ef00071808a2cee56519ee18413a71d6
Aaron <Aaron@AARON-PC>
2016-10-09 01:46:46 -0400

    ad hoc, permutation

---
+++ UVa/400/482.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s1, s2;
+	int N;
+	cin >> N;
+	getline(cin, s1);
+	rep(X, 0, N) {
+		getline(cin, s1);
+		getline(cin, s1);
+		getline(cin, s2);
+		stringstream ss1(s1), ss2(s2);
+		map<int, string> out;
+		while (ss1 >> s1 && ss2 >> s2)
+			out[stoi(s1)] = s2;
+
+		printf("%s", X > 0 ? "\n" : "");
+		irep(v, out)
+			printf("%s\n", v.y.c_str());
+	}
+}
