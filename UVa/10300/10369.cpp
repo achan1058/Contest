70926b1901dfa3ebbd64a8175a448a5b0c557beb
Aaron <Aaron@AARON-PC>
2016-10-09 19:18:02 -0400

    minimum spanning tree, kruskal, variant

---
+++ UVa/10300/10369.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include "point.h"
+#include "graph.h"
+#include "union_find.h"
+
+int kruskal(EdgeList<int>& edges, int min_comps) {
+	UnionFind un(edges.numV());
+	int num = edges.numV();
+	edges.sortEdges();
+	rep(i, 0, edges.numE()) {
+		if (un.join(get<0>(edges[i]), get<1>(edges[i]))) {
+			num--;
+			if (num == min_comps)
+				return get<2>(edges[i]);
+		}
+	}
+	return 0;
+}
+
+int main() {
+	int N, p, min_comps;
+	cin >> N;
+
+	rep(X, 0, N) {
+		cin >> min_comps >> p;
+		vector<pti> points(p);
+		EdgeList<int> edges;
+		rep(i, 0, p) {
+			cin >> points[i].x >> points[i].y;
+			rep(j, 0, i)
+				edges.push(i, j, points[i].dist2(points[j]));
+		}
+		printf("%.2lf\n", sqrt(kruskal(edges, min_comps)));
+	}
+}
