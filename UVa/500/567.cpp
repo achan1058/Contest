45688579e5f370465fd52de071d5a23ecb43db37
Aaron <Aaron@AARON-PC>
2016-10-08 20:49:37 -0400

    shortest path

---
+++ UVa/500/567.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "shortest_path.h"
+
+int main() {
+	int X = 1;
+	while (true) {
+		vvi graph = mi(21, 21, inf);
+		int num = 0, t, q, c1, c2;
+		rep(i, 1, 20) {
+			if (!(cin >> num))
+				return 0;
+			rep(j, 0, num) {
+				cin >> t;
+				graph[i][t] = 1;
+				graph[t][i] = 1;
+			}
+		}
+		vvi result = floydWarshall(graph).x;
+		cin >> q;
+		printf("Test Set #%d\n", X);
+		rep(i, 0, q) {
+			cin >> c1 >> c2;
+			printf("%2d to %2d: %d\n", c1, c2, result[c1][c2]);
+		}
+		printf("\n");
+		X++;
+	}
+}
