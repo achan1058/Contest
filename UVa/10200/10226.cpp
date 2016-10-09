3a4708cc69fb36f212a35f58248fa6572c09252e
Aaron <Aaron@AARON-PC>
2016-10-09 11:23:59 -0400

    counting

--- UVa/10200/10226.cpp
+++ UVa/10200/10226.cpp
@@ -2,29 +2,24 @@
 #include "header.h"
 
 int main() {
-	int N, X = 0, total = 0;
+	int N, total = 0;
 	cin >> N;
 	string s;
 	getline(cin, s);
 	getline(cin, s);
 	map<string, int> trees;
-	while (getline(cin, s)) {
-		if (s != "") {
+	rep(X, 0, N) {
+		while (getline(cin, s) && s != "") {
 			trees[s]++;
 			total++;
-		} else {
-			if (X > 0)
-				printf("\n");
-			irep(t, trees)
-				printf("%s %.4f\n", t.x.c_str(), t.y * 100. / total);
-			X++;
-			total = 0;
-			trees.clear();
 		}
-	}
 
-	if (X > 0)
-		printf("\n");
-	irep(t, trees)
-		printf("%s %.4f\n", t.x.c_str(), t.y * 100. / total);
+		if (X > 0)
+			printf("\n");
+		irep(t, trees)
+			printf("%s %.4f\n", t.x.c_str(), t.y * 100. / total);
+
+		total = 0;
+		trees.clear();
+	}
 }
