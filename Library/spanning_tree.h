bf18af25a496b9ac4ddb77ec8b14d1e9ba21f314
Aaron <Aaron@AARON-PC>
2016-10-09 01:16:51 -0400

    min span tree

---
+++ Library/spanning_tree.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "graph.h"
+#include "union_find.h"
+
+/*
+// CHANGES EDGE ORDER!! int is num component
+FIXME
+template<class T>
+tuple<T, int, EdgeList<T>> kruskal(EdgeList<T>& edges) {
+	UnionFind un(edges.numV());
+	T result = 0;
+	int num = edges.numV();
+	EdgeList<T> tree;
+	sort(all(edges.edges), [&](tuple<int, int, T> v1, tuple<int, int, T> v2) {return get<2>(v1) < get<2>(v2); });
+	irep(e, edges.edges) {
+		if (un.join(get<0>(e), get<1>(e))) {
+			result += get<2>(e);
+			tree.edges.pb(e);
+			num--;
+		}
+	}
+	return make_tuple(result, num, tree);
+}
+*/
+// int is component num vertex
+template<class T>
+tuple<T, int, AdjList<T>> prim(const AdjList<T>& graph, int start = 0, T non_edge = inf) {
+	int n = sz(graph), num = 1;
+	T result = 0;
+	vector<T> dist(n, non_edge);
+	AdjList<T> tree(n);
+	vi prev(n, -1);
+	priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> q;
+	dist[start] = -inf;
+
+	irep(e, graph[start]) {
+		dist[e.x] = e.y;
+		prev[e.x] = start;
+		q.push(make_pair(dist[e.x], e.x));
+	}
+
+	while (!q.empty()) {
+		T d = q.top().x;
+		int v1 = q.top().y;
+		q.pop();
+
+		if (dist[v1] == -inf)
+			continue;
+
+		num++;
+		result += d;
+		dist[v1] = -inf;
+		tree.push(v1, prev[v1], d);
+		tree.push(prev[v1], v1, d);
+		irep(e, graph[v1]) {
+			if (e.y < dist[e.x]) {
+				dist[e.x] = e.y;
+				prev[e.x] = v1;
+				q.push(make_pair(dist[e.x], e.x));
+			}
+		}
+	}
+	return make_tuple(result, num, tree);
+}
---
+++ UVa/10000/10034.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "spanning_tree.h"
+#include "point.h"
+
+int main() {
+	int N, n;
+	cin >> N;
+	rep(X, 0, N) {
+		printf("%s", X > 0 ? "\n" : "");
+		cin >> n;
+		vector<ptd> points(n);
+		AdjList<double> graph;
+		irep(p, points)
+			cin >> p;
+		rep(i, 0, n) {
+			rep(j, 0, n)
+				graph.push(i, j, points[i].dist(points[j]));
+		}
+		printf("%.2f\n", get<0>(prim(graph)));
+	}
+}
