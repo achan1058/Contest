6dad59456e70916d46f9afd0b41834a26d716042
Aaron <Aaron@AARON-PC>
2016-10-09 22:05:39 -0400

    point in polygon, point in circle

---
+++ Library/circle.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "point.h"
+#include "triangle.h"
+
+template<class T>
+struct Circle {
+	point<T> c; T r;
+	Circle(T x_ = 0, T y_ = 0, T r_ = 0) : c({ x_, y_ }), r(r_) {}
+	Circle(point<T> c_, T r_ = 0) : c(c_), r(r_) {}
+	Circle(point<T> p1, point<T> p2, point<T> p3) {
+		point<T> a = p2 - p1, b = p3 - p1;
+		T d = 2 * a.cross(b);
+		c.x = (a.norm2() * b.y - b.norm2() * a.y) / d + p1.x;
+		c.y = (b.norm2() * a.x - a.norm2() * b.x) / d + p1.y;
+		r = p1.dist(c);
+	}
+	//check this
+	static Circle inscribedCircle(point<T> p1, point<T> p2, point<T> p3) {
+		T a = p2.dist(p3), b = p1.dist(p3), c = p1.dist(p2);
+		return Circle((a * p1.x + b * p2.x + c * p3.x) / (a + b + c), (a * p1.y + b * p2.y + c * p3.y) / (a + b + c), sss_area(a, b, c) / (a + b + c));
+	}
+	//returns 1 on inside, 0 on boundary, -1 on non-intersection
+	int inCircle(point<T> p) const {
+		T d = r * r - c.dist2(p);
+		return d > T(0) ? 1 : (d < T(0) ? -1 : 0);
+	}
+};
---
+++ UVa/400/477.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include "double.h"
+#include "polygon.h"
+#include "circle.h"
+
+int main() {
+	vector<Polygon<Double>> poly;
+	vector<Circle<Double>> circ;
+	vb is_circle;
+	char c;
+
+	while (cin >> c && c != '*') {
+		poly.resize(sz(is_circle) + 1);
+		circ.resize(sz(is_circle) + 1);
+		is_circle.resize(sz(is_circle) + 1);
+
+		if (c == 'c') {
+			is_circle.back() = true;
+			Double x, y, r;
+			cin >> x >> y >> r;
+			circ.back() = Circle<Double>(x, y, r);
+		} else if (c == 'r') {
+			Double x1, y1, x2, y2;
+			cin >> x1 >> y1 >> x2 >> y2;
+			poly.back() = Polygon<Double>(4);
+			poly.back().v[0].x = poly.back().v[1].x = x1;
+			poly.back().v[1].y = poly.back().v[2].y = y1;
+			poly.back().v[2].x = poly.back().v[3].x = x2;
+			poly.back().v[3].y = poly.back().v[0].y = y2;
+		}
+	}
+
+	Double x, y;
+	int X = 1;
+	while (cin >> x >> y && (x != Double(9999.9) || y != Double(9999.9))) {
+		point<Double> p(x, y);
+		bool in_figure = false;
+		rep(i, 0, sz(is_circle)) {
+			bool found = false;
+			if (is_circle[i] && circ[i].inCircle(p) == 1)
+				found = true;
+			else if (!is_circle[i] && poly[i].inPolygon(p) == 1)
+				found = true;
+			if (found) {
+				printf("Point %d is contained in figure %d\n", X, i + 1);
+				in_figure = true;
+			}
+		}
+
+		if (!in_figure)
+			printf("Point %d is not contained in any figure\n", X);
+
+		X++;
+	}
+}
