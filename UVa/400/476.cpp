cb8f28f7540e1f30439047003011aca8c543bee3
Aaron <Aaron@AARON-PC>
2016-10-09 21:57:41 -0400

    point in polygon

---
+++ UVa/400/476.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include "double.h"
+#include "polygon.h"
+
+int main() {
+	vector<Polygon<Double>> poly;
+	char c;
+	while (cin >> c && c != '*') {
+		poly.resize(sz(poly) + 1);
+		Double x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		poly.back() = Polygon<Double>(4);
+		poly.back().v[0].x = poly.back().v[1].x = x1;
+		poly.back().v[1].y = poly.back().v[2].y = y1;
+		poly.back().v[2].x = poly.back().v[3].x = x2;
+		poly.back().v[3].y = poly.back().v[0].y = y2;
+	}
+
+	Double x, y;
+	int X = 1;
+	while (cin >> x >> y && (x != Double(9999.9) || y != Double(9999.9))) {
+		point<Double> p(x, y);
+		bool in_figure = false;
+		rep(i, 0, sz(poly)) {
+			if (poly[i].inPolygon(p) == 1) {
+				printf("Point %d is contained in figure %d\n", X, i + 1);
+				in_figure = true;
+			}
+		}
+
+		if (!in_figure)
+			printf("Point %d is not contained in any figure\n", X);
+
+		X++;
+	}
+}
