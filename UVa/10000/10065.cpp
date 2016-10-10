cd5db8431dd1e0b399720873b8c24a65f3bc117e
Aaron <Aaron@AARON-PC>
2016-10-09 22:26:39 -0400

    convex hull, polygon area

---
+++ UVa/10000/10065.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "polygon.h"
+#include "graham.h"
+
+int main() {
+	int n, X = 1;
+	while (cin >> n && n != 0) {
+		vector<pti> pts(n);
+		irep(p, pts)
+			cin >> p;
+
+		int area = abs(Polygon<int>(pts).sa2());
+		int hull = abs(Polygon<int>(graham(pts)).sa2());
+		double ans = (1 - double(area) / hull) * 100;
+		printf("Tile #%d\nWasted Space = %.2lf %%\n\n", X, ans);
+		X++;
+	}
+}
