7b27f666e54395ddba46f76e38fc091b6f0d037f
Aaron <Aaron@AARON-PC>
2016-10-09 22:55:31 -0400

    line intersection

---
+++ Library/line.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "point.h"
+
+// records ax + by = c
+template<class T>
+struct Line {
+public:
+	T a, b, c;
+	Line(T a_ = 0, T b_ = 0, T c_ = 0) : a(a_), b(b_), c(c_) {}
+	Line(point<T> p1, point<T> p2) : a(p2.y - p1.y), b(p1.x - p2.x) { c = a * p1.x + b * p1.y; }
+	Line(T dx, T dy, point<T> p) : a(-dy), b(dx) { c = a * p.x + b * p.y; }
+	Line perp(point<T> p) { return Line(-b, a, p); }
+	// need this?
+	void normalize() {
+		T m = abs(a) >= abs(b) ? a : b;
+		a /= m; b /= m; c /= m;
+	}
+	point<T> intersect(Line<T> l) const {
+		T det = a * l.b - l.a * b;
+		if (det == T(0)) {
+			if (a * l.c - l.a * c == T(0) && b * l.c - l.b * c == T(0)) return point<T>(inf, inf);
+			else return point<T>(-inf, -inf);
+		}
+		return point<T>((c * l.b - l.c * b) / det, (a * l.c - l.a * c) / det);
+	}
+};
---
+++ UVa/300/378.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "double.h"
+#include "line.h"
+
+int main() {
+	int N;
+	cin >> N;
+	point<Double> p11, p12, p21, p22;
+	printf("INTERSECTING LINES OUTPUT\n");
+
+	rep(X, 0, N) {
+		cin >> p11 >> p12 >> p21 >> p22;
+		Line<Double> l1(p11, p12), l2(p21, p22);
+		point<Double> p = l1.intersect(l2);
+
+		if (p.x.v == inf)
+			printf("LINE\n");
+		else if (p.x.v == -inf)
+			printf("NONE\n");
+		else
+			printf("POINT %.2f %.2f\n", p.x, p.y);
+	}
+	printf("END OF OUTPUT\n");
+}
