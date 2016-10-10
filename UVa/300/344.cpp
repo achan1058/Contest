f5b92097a6adeaf5c975c7ce148d8973e11aed4f
Aaron <Aaron@AARON-PC>
2016-10-10 18:50:15 -0400

    roman numerals

---
+++ UVa/300/344.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "misc.h"
+
+string pos = "IVXLC";
+int main() {
+	int n;
+	vvi arr = mi(5, 101, 0);
+	rep(i, 1, 101) {
+		int n = i;
+		rep(k, 0, 5)
+			arr[k][i] = arr[k][i - 1];
+		rep(j, 0, 3) {
+			irep(c, roman[j][n % 10]) {
+				rep(k, 0, 5) {
+					if (c == pos[k]) {
+						arr[k][i]++;
+						break;
+					}
+				}
+			}
+
+			n /= 10;
+		}
+	}
+
+	while (cin >> n && n != 0)
+		printf("%d: %d i, %d v, %d x, %d l, %d c\n", n, arr[0][n], arr[1][n], arr[2][n], arr[3][n], arr[4][n]);
+}
