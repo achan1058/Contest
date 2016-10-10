b2ca2bd95c76cf6eebe27911c443e2fba51b4468
Aaron <Aaron@AARON-PC>
2016-10-10 16:16:26 -0400

    computational geometry, convexity

---
+++ UVa/10000/10078.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "point.h"
+
+int main() {
+	int n;
+	while (cin >> n && n != 0) {
+		bool cw = true, ccw = true;
+		vector<pti> pts(n);
+		irep(p, pts)
+			cin >> p;
+		rep(i, 0, n) {
+			if (pts[i].sa2(pts[(i + 1) % n], pts[(i + 2) % n]) < 0)
+				ccw = false;
+			else
+				cw = false;
+		}
+		printf("%s\n", ccw || cw ? "No" : "Yes");
+	}
+}
