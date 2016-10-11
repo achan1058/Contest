9948f6ecc18b1352808e4e58bd8a692b25c1eaa0
Aaron <Aaron@AARON-PC>
2016-10-10 21:25:29 -0400

    fibonacci, big int

--- UVa/10100/10183.py
+++ UVa/10100/10183.py
@@ -4,10 +4,7 @@ fib = [1, 2]
 while fib[-1] < 10**100:
     fib.append(fib[-1] + fib[-2])
 while True:
-    try:
-        small,big = map(int,input().split())
-    except EOFError:
-        exit()
+    small,big = map(int,input().split())
     if small == 0 and big == 0:
         exit()
 
