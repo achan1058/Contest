7f19d4cf913dfd3bb56708bc10c317c3c5021dc7
Aaron <Aaron@AARON-PC>
2016-10-11 02:39:10 -0400

    longest DAG path

---
+++ UVa/11000/11039.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int N, n, v;
+	cin >> N;
+	rep(X, 0, N) {
+		cin >> n;
+		vi p, m;
+		rep(i, 0, n) {
+			cin >> v;
+			if (v > 0)
+				p.pb(v);
+			else
+				m.pb(-v);
+		}
+
+		sort(all(p));
+		sort(all(m));
+
+		if (sz(p) == 0 || sz(m) == 0)
+			printf("1\n");
+		else {
+			int pi = 0, mi = 0, pl = 1, ml = 1;
+			while (pi < sz(p) && mi < sz(m)) {
+				if (p[pi] < m[mi]) {
+					ml = pl + 1;
+					pi++;
+				} else if (p[pi] > m[mi]) {
+					pl = ml + 1;
+					mi++;
+				} else {
+					int temp = ml;
+					pi++;
+					mi++;
+
+					if (mi < sz(m))
+						ml = pl + 1;
+					if (pi < sz(p))
+						pl = temp + 1;
+				}
+			}
+
+			printf("%d\n", max(pl, ml));
+		}
+	}
+}
