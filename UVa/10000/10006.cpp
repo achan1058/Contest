1c6605988ca0b3bcfa6f950540027e54899772f7
Aaron <Aaron@AARON-PC>
2016-10-11 12:59:29 -0400

    prime test, powmod

--- UVa/10000/10006.cpp
+++ UVa/10000/10006.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
+#include "number_theory.h"
+#include "primes.h"
 
 int main() {
-	system("python UVa/10000/10006.py < input.txt");
+	ll N;
+	while (cin >> N && N != 0) {
+		bool carmichael = true;
+		if (isPrime(N))
+			carmichael = false;
+		for (int n = 2; n < N && carmichael; n++) {
+			if (power<ll>(n, N, N) != n)
+				carmichael = false;
+		}
+		if (carmichael)
+			printf("The number %d is a Carmichael number.\n", N);
+		else
+			printf("%d is normal.\n", N);
+	}
 }
