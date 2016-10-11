08e60002d899754a303c565614a5ebfe9d130f92
Aaron <Aaron@AARON-PC>
2016-10-11 03:44:11 -0400

    mathematics, permutation, multinomial coefficients

---
+++ UVa/11000/11076.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include "combinatorics.h"
+
+int main() {
+	int n, v;
+	vl fact = factTable<ll>(12);
+
+	while (cin >> n && n != 0) {
+		map<int, int> mp;
+		int total = 0;
+		rep(i, 0, n) {
+			cin >> v;
+			mp[v]++;
+			total += v;
+		}
+
+		ll mul = 0;
+		rep(i, 0, n) {
+			mul *= 10;
+			mul++;
+		}
+
+		ll value = fact[n];
+		irep(m, mp)
+			value /= fact[m.y];
+
+		mul *= total * value / n;
+		printf("%lld\n", mul);
+	}
+}
