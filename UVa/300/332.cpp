fa433e4982ca7fbe1669d6203f64313a9c1495ad
Aaron <Aaron@AARON-PC>
2016-10-11 01:47:10 -0400

    repeating decimal

---
+++ UVa/300/332.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include "number_theory.h"
+
+int main() {
+	int n, X = 1;
+	ll numer;
+	char c;
+	string s;
+	while (cin >> n && n != -1) {
+		cin >> numer >> c >> s;
+		int power = sz(s);
+		ll v = numer = stoll(s), denom = 1;
+
+		if (n != 0) {
+			for (int i = 0; i < n; i++) {
+				v /= 10;
+				denom *= 10;
+			}
+			numer -= v;
+			denom--;
+		}
+
+		for (int i = n; i < power; i++)
+			denom *= 10;
+
+		ll g = gcd(numer, denom);
+		if (numer == 0) {
+			numer = 0;
+			denom = 1;
+			g = 1;
+		}
+		printf("Case %d: %lld/%lld\n", X, numer / g, denom / g);
+		X++;
+	}
+}
