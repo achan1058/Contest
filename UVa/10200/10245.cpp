e3161f413fce037478a58bb78142093c49196faf
Aaron <Aaron@AARON-PC>
2016-10-10 21:15:43 -0400

    closest pair, sweep line

---
+++ UVa/10200/10245.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "sweep_line.h"
+
+int main() {
+	int n;
+	while (cin >> n && n != 0) {
+		vector<ptd> points(n);
+		irep(p, points)
+			cin >> p;
+		double max_dist2 = get<0>(closestPair(points));
+		if (max_dist2 >= 10000 * 10000 - eps)
+			printf("INFINITY\n");
+		else
+			printf("%.4f\n", sqrt(max_dist2));
+	}
+}
