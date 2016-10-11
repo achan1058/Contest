8d631eba6895fc336ee12d3572e22ec4ba698ff2
Aaron <Aaron@AARON-PC>
2016-10-11 13:20:46 -0400

    gcd

--- UVa/11800/11827.cpp
+++ UVa/11800/11827.cpp
@@ -1,5 +1,23 @@
 #include <bits/stdc++.h>
+#include "number_theory.h"
 
 int main() {
-	system("python UVa/11800/11827.py < input.txt");
+	int n;
+	string s;
+	cin >> n;
+	getline(cin, s);
+	rep(i, 0, n) {
+		int best = 0, v;
+		getline(cin, s);
+		stringstream ss;
+		ss << s;
+		vi ints;
+		while (ss >> v)
+			ints.pb(v);
+		rep(i, 0, sz(ints)) {
+			rep(j, i + 1, sz(ints))
+				best = max(best, gcd(ints[i], ints[j]));
+		}
+		printf("%d\n", best);
+	}
 }
