662d49778c6fc399ca8cacf9ac1ecd0a3c7039c0
Aaron <Aaron@AARON-PC>
2016-10-11 03:29:31 -0400

    counting, permutations, DP

---
+++ UVa/10100/10128.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include "combinatorics.h"
+
+int main() {
+	int T, n, p, r;
+	vl fact = factTable<ll>(13);
+	vvl inc = ml(14, 14, 0);
+	vvl binom = binomialTable<ll>(13);
+	inc[0][0] = 1;
+
+	for (int i = 1; i <= 13; i++) {
+		for (int j = 1; j <= i; j++) {
+			for (int k = 1; k <= i; k++)
+				inc[i][j] += binom[i - 1][k - 1] * inc[k - 1][j - 1] * fact[i - k];
+		}
+	}
+
+	cin >> T;
+	for (int X = 0; X < T; X++) {
+		cin >> n >> p >> r;
+		if (p <= 0 || p > n || r <= 0 || r > n) {
+			printf("0\n");
+			continue;
+		}
+		ll ans = 0;
+		for (int i = 1; i <= n; i++)
+			ans += binom[n - 1][i - 1] * inc[i - 1][p - 1] * inc[n - i][r - 1];
+		printf("%lld\n", ans);
+	}
+}
