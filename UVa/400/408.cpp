b91924836b6254e0178c95c6f92d879d574ad859
Aaron <Aaron@AARON-PC>
2016-10-11 04:22:30 -0400

    gcd

--- UVa/400/408.cpp
+++ UVa/400/408.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
+#include "number_theory.h"
 
 int main() {
-	system("python UVa/400/408.py < input.txt");
+	int n, m;
+	while (cin >> n >> m) {
+		if (gcd(n, m) == 1)
+			printf("%10d%10d    Good Choice\n\n", n, m);
+		else
+			printf("%10d%10d    Bad Choice\n\n", n, m);
+	}
 }
--- UVa/400/412.cpp
+++ UVa/400/412.cpp
@@ -1,5 +1,23 @@
 #include <bits/stdc++.h>
+#include "number_theory.h"
 
 int main() {
-	system("python UVa/400/412.py < input.txt");
+	int n;
+	while (cin >> n && n > 0) {
+		vi v(n);
+		int num_gcd = 0;
+		irep(l, v)
+			cin >> l;
+		rep(i, 0, n) {
+			rep(j, i + 1, n) {
+				if (gcd(v[i], v[j]) == 1)
+					num_gcd++;
+			}
+		}
+
+		if (num_gcd == 0)
+			printf("No estimate for this data set.\n");
+		else
+			printf("%.6f\n", sqrt(3 * n * (n - 1) / (double)(num_gcd)));
+	}
 }
