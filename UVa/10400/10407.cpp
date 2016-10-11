20bfbfa763a3333e2ac8525e4d878508cb1c54ba
Aaron <Aaron@AARON-PC>
2016-10-11 01:14:14 -0400

    gcd

---
+++ UVa/10400/10407.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "number_theory.h"
+
+int main() {
+	vi nums;
+	int v;
+	while (cin >> v) {
+		if (v == 0) {
+			if (sz(nums) == 0)
+				return 0;
+			else {
+				int g = nums[1] - nums[0];
+				rep(i, 2, sz(nums))
+					g = gcd(g, nums[i] - nums[i - 1]);
+
+				printf("%d\n", g);
+				nums.clear();
+			}
+		} else {
+			nums.pb(v);
+		}
+	}
+}
