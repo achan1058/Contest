32b9b5e6201dc0869dd778950446094c94ac9bf4
Aaron <Aaron@AARON-PC>
2016-10-12 11:13:29 -0400

    ad hoc, mathematics

---
+++ UVa/11000/11040.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int N;
+	cin >> N;
+	vvi triangle(9);
+	rep(i, 0, 9)
+		triangle[i].resize(i + 1);
+	rep(X, 0, N) {
+		rep(i, 0, 5) {
+			rep(j, 0, i + 1)
+				cin >> triangle[2 * i][2 * j];
+		}
+		rep(j, 0, 4)
+			triangle[8][2 * j + 1] = (triangle[6][2 * j] - triangle[8][2 * j] - triangle[8][2 * j + 2]) / 2;
+		rrep(i, 7, 0) {
+			rep(j, 0, i + 1)
+				triangle[i][j] = triangle[i + 1][j] + triangle[i + 1][j + 1];
+		}
+		rep(i, 0, 9) {
+			rep(j, 0, i + 1)
+				printf("%s%d", j > 0 ? " " : "", triangle[i][j]);
+			printf("\n");
+		}
+	}
+}
