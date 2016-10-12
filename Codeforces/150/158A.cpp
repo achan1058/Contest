df414850e48a9a107b734508df669fdaceb4455e
Aaron <Aaron@AARON-PC>
2016-10-11 22:40:53 -0400

    ad hoc

---
+++ Codeforces/150/158A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vi arr(n);
+	irep(v, arr)
+		cin >> v;
+	while (k > 0 && arr[k - 1] == 0)
+		k--;
+	while (k < n && arr[k - 1] == arr[k])
+		k++;
+	printf("%d\n", k);
+}
