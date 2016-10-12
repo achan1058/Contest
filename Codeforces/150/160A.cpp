de14bfa2ea0162d7781fcd5a1c94e0e1095721cd
Aaron <Aaron@AARON-PC>
2016-10-11 22:47:54 -0400

    sorting

---
+++ Codeforces/150/160A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, total = 0, ans = 0;
+	cin >> n;
+	vi coins(n);
+	irep(c, coins) {
+		cin >> c;
+		total += c;
+	}
+	total /= 2;
+	sort(all(coins));
+	rrep(i, n - 1, 0) {
+		if (total < 0)
+			break;
+		total -= coins[i];
+		ans++;
+	}
+	printf("%d\n", ans);
+}
