93caed1510f8d6d9faa0188dbb983e8e81f2a8be
Aaron <Aaron@AARON-PC>
2016-10-11 22:30:07 -0400

    string manipulation

---
+++ Codeforces/125/131A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	int upper = 1;
+	cin >> s;
+	rep(i, 1, sz(s))
+		upper &= isupper(s[i]);
+	if (upper) {
+		irep(c, s)
+			c = isupper(c) ? tolower(c) : toupper(c);
+	}
+	printf("%s\n", s.c_str());
+}
