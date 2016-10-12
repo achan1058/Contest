567c8104486b14b73a9d0fb42a089766eba2a3b3
Aaron <Aaron@AARON-PC>
2016-10-11 22:13:10 -0400

    bit masking

--- Codeforces/50/61A.cpp
+++ Codeforces/50/61A.cpp
@@ -4,7 +4,6 @@
 int main() {
 	string s1, s2;
 	cin >> s1 >> s2;
-	rep(i, 0, sz(s1))
-		s1[i] = (s1[i] + s2[i] - 2 * '0') % 2 + '0';
-	printf("%s\n", s1.c_str());
+	bitset<100> b1(s1), b2(s2);
+	printf("%s\n", (b1 ^ b2).to_string().substr(100 - sz(s1)).c_str());
 }
