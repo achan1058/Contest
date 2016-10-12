b969d19587b399ad2353e02351ad27029c01cad3
Aaron <Aaron@AARON-PC>
2016-10-11 22:27:03 -0400

    string manipulation

---
+++ Codeforces/100/118A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	cin >> s;
+	irep(c, s) {
+		c = tolower(c);
+		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y')
+			continue;
+		printf(".%c", c);
+	}
+	printf("\n");
+}
