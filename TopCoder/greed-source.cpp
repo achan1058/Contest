76fe3c4b0a089ade485f4226f30762cb4fb2e85d
Aaron <Aaron@AARON-PC>
2016-10-14 13:47:18 -0400

    Moved irep and added drep

--- TopCoder/greed-source.cpp
+++ TopCoder/greed-source.cpp
@@ -21,6 +21,7 @@ double eps = 1e-8;
 ll mod = 1000000007ll;
 
 #define rep(k, a, b) for (int k = (a); k < int(b); k++)
+#define irep(k, a) for (auto& k : (a))
 #define sz(a) int(a.size())
 #define all(c) (c).begin(), (c).end()
 #define pb push_back
@@ -28,7 +29,7 @@ ll mod = 1000000007ll;
 #define y second
 #define mi(r, c, v) vvi(r, vi(c, v))
 #define rrep(k, a, b) for (int k = (a); k >= int(b); k--)
-#define irep(k, a) for (auto& k : (a))
+#define drep(i, j, a, b) for (int i = 0; i < (a); i++) for (int j = 0; j < (b); j++)
 #define md(r, c, v) vvd(r, vd(c, v))
 #define mb(r, c, v) vvb(r, vb(c, v))
 #define ms(r, c, v) vvs(r, vs(c, v))
