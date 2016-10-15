d9f603c97425a399576a2d6c9a881af8060fce69
Aaron <Aaron@AARON-PC>
2016-10-15 16:45:39 -0400

    Changed to use new macro

--- UVa/10000/10000.cpp
+++ UVa/10000/10000.cpp
@@ -2,13 +2,12 @@
 #include "topo_sort.h"
 
 int main() {
-	int n, s, X = 1, e1, e2;
-	while (cin >> n >> s && n != 0) {
+	int n, s, e1, e2;
+	whileX(cin >> n >> s && n != 0) {
 		vvi graph(n);
-		while (cin >> e1 >> e2 && (e1 != 0 || e2 != 0))
+		while (cin >> e1 >> e2 && (e1 | e2) != 0)
 			graph[e1 - 1].pb(e2 - 1);
 		vi result = longestDAG(graph, s - 1, -1);
-		printf("Case %d: The longest path from %d has length %d, finishing at %d.\n\n", X, s, sz(result) - 1, result.back() + 1);
-		X++;
+		printc("The longest path from %d has length %d, finishing at %d.\n\n", s, sz(result) - 1, result.back() + 1);
 	}
 }
