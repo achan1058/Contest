33e4564c5cc4de13d12dc888df77da4f70ddfb31
Aaron <Aaron@AARON-PC>
2016-10-16 03:23:41 -0400

    minor changes

--- UVa/11000/11045.cpp
+++ UVa/11000/11045.cpp
@@ -2,21 +2,18 @@
 #include "matching.h"
 
 int main() {
-	int T, n, m;
+	int n, m;
 	vs sizes = { "XXL", "XL", "L", "M", "S", "XS" };
 	string s;
-	cin >> T;
-	for (int X = 0; X < T; X++) {
+	forX() {
 		cin >> n >> m;
 		vvb graph = mb(m, n, false);
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < 2; j++) {
-				cin >> s;
-				for (int k = 0; k < 6; k++) {
-					if (s == sizes[k]) {
-						for (int l = k * (n / 6); l < (k + 1) * n / 6; l++)
-							graph[i][l] = true;
-					}
+		drep(i, j, m, 2) {
+			cin >> s;
+			for (int k = 0; k < 6; k++) {
+				if (s == sizes[k]) {
+					for (int l = k * (n / 6); l < (k + 1) * n / 6; l++)
+						graph[i][l] = true;
 				}
 			}
 		}
