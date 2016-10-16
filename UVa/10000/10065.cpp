267be669e8a919212b9e1234613da7c493439615
Aaron <Aaron@AARON-PC>
2016-10-16 03:33:43 -0400

    Used whileX()

--- UVa/10000/10065.cpp
+++ UVa/10000/10065.cpp
@@ -3,16 +3,13 @@
 #include "graham.h"
 
 int main() {
-	int n, X = 1;
-	while (cin >> n && n != 0) {
+	int n;
+	whileX (cin >> n && n != 0) {
 		vector<pti> pts(n);
-		irep(p, pts)
-			cin >> p;
-
+		read(pts);
 		int area = abs(Polygon<int>(pts).sa2());
 		int hull = abs(Polygon<int>(graham(pts)).sa2());
 		double ans = (1 - double(area) / hull) * 100;
 		printf("Tile #%d\nWasted Space = %.2lf %%\n\n", X, ans);
-		X++;
 	}
 }
