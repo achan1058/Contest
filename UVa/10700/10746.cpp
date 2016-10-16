e53d6d8b9ba321e861dcfe456d02964f947cd682
Aaron <Aaron@AARON-PC>
2016-10-16 00:27:31 -0400

    cosmetic updates

--- UVa/10700/10746.cpp
+++ UVa/10700/10746.cpp
@@ -5,11 +5,9 @@ int main() {
 	int n, m;
 	while (cin >> n >> m && (n | m) != 0) {
 		vvd graph = md(n, m, 0);
-		rep(i, 0, n) {
-			rep(j, 0, m) {
-				cin >> graph[i][j];
-			}
-		}
+		drep(i, j, n, m)
+			cin >> graph[i][j];
+
 		printf("%.2f\n", hungarian(graph).x / n);
 	}
 }
