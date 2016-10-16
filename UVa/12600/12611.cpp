01992b1b3de51720e5400ecb066bc979dc90a485
Aaron <Aaron@AARON-PC>
2016-10-16 12:07:58 -0400

    AC

---
+++ UVa/12600/12611.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int r;
+	forX() {
+		cin >> r;
+		printf("Case %d:\n", X);
+		printf("%d %d\n", -r * 45 / 20, r * 30 / 20);
+		printf("%d %d\n", r * 55 / 20, r * 30 / 20);
+		printf("%d %d\n", r * 55 / 20, r * -30 / 20);
+		printf("%d %d\n", -r * 45 / 20, r * -30 / 20);
+	}
+}
