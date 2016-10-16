4626820ea5ad762831986b0fccbf99586865017e
Aaron <Aaron@AARON-PC>
2016-10-16 14:32:46 -0400

    Changed binary search to be more robust.

--- Library/binary_search.h
+++ Library/binary_search.h
@@ -1,28 +1,24 @@
 #pragma once
 #include "util.h"
 
+// Finds x such that func(x) = 0. Returns left most of such x if func is monotone.
 template<class T, class U = double>
-T binarySearch(U func(T value), T m = -inf, T M = inf, T bound = eps, bool precise = false) {
-	int m_sign = sgn(func(m)), M_sign = sgn(func(M));
+T binarySearch(U func(T value), T m = -inf, T M = inf, T bound = eps, bool precise = true) {
+	int m_sign = sgn(func(m));
+	T mid = (m + M) / 2;
+
 	if (m_sign == 0)
 		return m;
-	else if (M_sign == 0)
-		return M;
-	else if (m_sign == M_sign)
+	else if (m_sign == sgn(func(M)))
 		return inf;
-	while (bound < M - m) {
-		T mid = (m + M) / 2;
+	do {
 		int mid_sign = sgn(func(mid));
-		if (mid_sign == 0)
-			return mid;
-		else if (mid_sign == m_sign)
+		if (mid_sign == m_sign)
 			m = mid;
 		else
 			M = mid;
-	}
 
-	if (precise)
-		return inf;
-	else
-		return (M + m) / 2;
+		mid = (m + M) / 2;
+	} while (bound < mid - m);
+	return (sgn(func(M)) == 0 || !precise ? M : inf);
 }
