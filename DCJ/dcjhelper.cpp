d1cf2c113586bef60bc26f77318821f3f5bac50a
Aaron <Aaron@AARON-PC>
2016-10-19 16:58:29 -0400

    Added stderr redirect to output.txt

--- DCJ/dcjhelper.cpp
+++ DCJ/dcjhelper.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
 
 int main() {
-	system("cd DCJ && dcj.py test --source 2016/again.cpp --nodes 4 && PAUSE");
+	system("cd DCJ && dcj.py test --source 2016/again.cpp --nodes 4 2>&1");
 }
