ec1cf49688f2bb0245c11fa0e8703aeef005cd1a
Aaron <Aaron@AARON-PC>
2016-10-22 15:18:16 -0400

    AC

---
+++ UVa/12800/12854.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vi v1(5), v2(5);
+	while (read(v1) && read(v2)) {
+		bool good = true;
+		rep(i, 0, 5)
+			good &= v1[i] + v2[i] == 1;
+		printf("%c\n", good ? 'Y' : 'N');
+	}
+}
