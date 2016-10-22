f09b79af569d94ee7c1a502a6a4c862b125e0a3f
Aaron <Aaron@AARON-PC>
2016-10-22 19:12:53 -0400

    AC, with a cleaner method of parsing the tree than character by character.

--- UVa/100/112.cpp
+++ UVa/100/112.cpp
@@ -4,50 +4,26 @@
 int n;
 bool good;
 bool recurse(int cur = 0) {
-	int t = 0;
+	int t;
 	char c;
-	bool neg = false;
-	cin >> c;
-	if (c == ')')
+	if (!(cin >> c >> t)) {
+		cin.clear();
+		cin >> c;
 		return true;
-	else if (c == '-')
-		neg = true;
-	else
-		t = c - '0';
-	while (cin >> c && c != '(') {
-		t *= 10;
-		t += c - '0';
 	}
-	if (neg)
-		t *= -1;
 
 	cur += t;
 	bool leaf = recurse(cur);
-	cin >> c;
 	if (recurse(cur) && leaf && cur == n)
 		good = true;
+
 	cin >> c;
 	return false;
 }
 
 int main() {
-	char c;
-	while (cin >> c) {
-		bool neg = false;
+	while (cin >> n) {
 		good = false;
-		n = 0;
-
-		if (c == '-')
-			neg = true;
-		else
-			n = c - '0';
-		while (cin >> c && c != '(') {
-			n *= 10;
-			n += c - '0';
-		}
-		if (neg)
-			n *= -1;
-
 		recurse();
 		printf("%s\n", good ? "yes" : "no");
 	}
