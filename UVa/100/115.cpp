72a816848b3d4f88ea5cb54c19fbadd2ae25893c
Aaron <Aaron@AARON-PC>
2016-10-22 19:16:55 -0400

    Change to use sz() to avoid unsigned int.

--- UVa/100/115.cpp
+++ UVa/100/115.cpp
@@ -21,26 +21,26 @@ int main() {
 		if (v1.back() != v2.back())
 			printf("no relation\n");
 		else {
-			while (v1.size() > 0 && v2.size() > 0 && v1.back() == v2.back()) {
-				v1.resize(v1.size() - 1);
-				v2.resize(v2.size() - 1);
+			while (sz(v1) > 0 && sz(v2) > 0 && v1.back() == v2.back()) {
+				v1.resize(sz(v1) - 1);
+				v2.resize(sz(v2) - 1);
 			}
 			if (v1.empty()) {
 				for (int i = 2; i < sz(v2); i++)
 					printf("great ");
-				if (v2.size() >= 2)
+				if (sz(v2) >= 2)
 					printf("grand ");
 				printf("parent\n");
 			} else if (v2.empty()) {
 				for (int i = 2; i < sz(v1); i++)
 					printf("great ");
-				if (v1.size() >= 2)
+				if (sz(v1) >= 2)
 					printf("grand ");
 				printf("child\n");
-			} else if (v1.size() == 1 && v2.size() == 1)
+			} else if (sz(v1) == 1 && sz(v2) == 1)
 				printf("sibling\n");
 			else {
-				printf("%d cousin", min(v1.size(), v2.size()) - 1);
+				printf("%d cousin", min(sz(v1), sz(v2)) - 1);
 				if (abs(sz(v1) - sz(v2)) > 0)
 					printf(" removed %d\n", abs(sz(v1) - sz(v2)));
 				else
