81d06fc853be034ecb0e6d8dd1ce31132a6dba07
Aaron <Aaron@AARON-PC>
2016-10-22 21:29:25 -0400

    AC

--- UVa/12300/12372.cpp
+++ UVa/12300/12372.cpp
@@ -2,10 +2,8 @@
 #include "header.h"
 
 int main() {
-	int N;
-	cin >> N;
 	int x, y, z;
-	rep(X, 1, N + 1) {
+	forX() {
 		cin >> x >> y >> z;
 		printc("%s\n", x > 20 || y > 20 || z > 20 ? "bad" : "good");
 	}
