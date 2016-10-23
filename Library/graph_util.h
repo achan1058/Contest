9e8c6f19c8d3eec992eb399d7d9759040f6cbe72
Aaron <Aaron@AARON-PC>
2016-10-22 23:54:41 -0400

    TODO fix graph util

--- Library/graph_util.h
+++ Library/graph_util.h
@@ -1,2 +1,14 @@
 #pragma once
-#include "graph.h"
+#include "header.h"
+
+// change to use ep<T>
+template<class T>
+vector<vector<T>> edgeToMatrix(const EdgeList<T>& edge, T non_edge = inf) {
+	int n = edge.numV();
+	vector<vector<T>> graph(n, vector<T>(n, inf));
+	rep(i, 0, edge.numE())
+		graph[get<0>(edge[i])][get<1>(edge[i])] = get<2>(edge[i]);
+	return graph;
+}
+
+inline void sortEdges() { sort(all(edges), [](const tuple<int, int, T>& v1, const tuple<int, int, T>& v2) { return get<2>(v1) < get<2>(v2); }); }
