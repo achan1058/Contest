6d0ef6d8caad32be90de6d991ddcb0740ab8e045
Aaron <Aaron@AARON-PC>
2016-10-24 00:28:13 -0400

    AC

--- UVa/10300/10336.cpp
+++ UVa/10300/10336.cpp
@@ -1,45 +1,18 @@
 #include <bits/stdc++.h>
-#include "header.h"
-
-int r, c;
-vs board;
-vi dx = { 1, 0, -1, 0 }, dy = { 0, 1, 0, -1 };
-void floodfill(int x0, int y0) {
-	queue<pii> q;
-	q.push({ x0, y0 });
-	char ch = board[x0][y0];
-	board[x0][y0] = ' ';
-
-	while (!q.empty()) {
-		int x1 = q.front().x, y1 = q.front().y;
-		q.pop();
-
-		rep(i, 0, 4) {
-			int nx = x1 + dx[i], ny = y1 + dy[i];
-			if (nx < 0 || nx >= r || ny < 0 || ny >= c || board[nx][ny] != ch)
-				continue;
-
-			board[nx][ny] = ' ';
-			q.push({ nx, ny });
-		}
-	}
-}
+#include "floodfill.h"
 
 int main() {
-	int N;
-	cin >> N;
-	rep(X, 1, N + 1) {
+	int r, c;
+	forX() {
 		cin >> r >> c;
+		vs board(r);
+		read(board);
 		map<char, int> result;
-		board.resize(r);
-		rep(i, 0, r)
-			cin >> board[i];
-		rep(i, 0, r) {
-			rep(j, 0, c) {
-				if (board[i][j] != ' ') {
-					result[board[i][j]]++;
-					floodfill(i, j);
-				}
+
+		drep(i, j, r, c) {
+			if (board[i][j] != ' ') {
+				result[board[i][j]]++;
+				floodfill(board, i, j, ' ');
 			}
 		}
 
