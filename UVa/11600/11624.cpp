5665488efff118bd50d025960f35639bcbd18698
Aaron <Aaron@AARON-PC>
2016-10-24 03:56:50 -0400

    AC

--- UVa/11600/11624.cpp
+++ UVa/11600/11624.cpp
@@ -1,54 +1,50 @@
 #include <bits/stdc++.h>
 #include "header.h"
 
-int r, c;
-vi dx = { 1, 0, -1, 0 }, dy = { 0, 1, 0, -1 };
-void floodfill(vector<pii>& start, vvi& board) {
+void floodfill(vvi& grid, vector<pii>& start) {
+	vi dx = { 1, 0, -1, 0 }, dy = { 0, 1, 0, -1 };
+	int r = sz(grid), c = sz(grid[0]);
 	queue<pii> q;
 	for (auto& p : start) {
 		q.push({ p.x, p.y });
-		board[p.x][p.y] = 0;
+		grid[p.x][p.y] = 0;
 	}
 	while (!q.empty()) {
-		while (!q.empty()) {
-			int x1 = q.front().x, y1 = q.front().y;
-			q.pop();
-			rep(i, 0, sz(dx)) {
-				int nx = x1 + dx[i], ny = y1 + dy[i];
-				if (nx < 0 || nx >= r || ny < 0 || ny >= c || board[nx][ny] != inf)
-					continue;
+		int x = q.front().x, y = q.front().y;
+		q.pop();
+		rep(i, 0, sz(dx)) {
+			int nx = x + dx[i], ny = y + dy[i];
+			if (nx < 0 || nx >= r || ny < 0 || ny >= c || grid[nx][ny] != inf)
+				continue;
 
-				board[nx][ny] = board[x1][y1] + 1;
-				q.push({ nx, ny });
-			}
+			grid[nx][ny] = grid[x][y] + 1;
+			q.push({ nx, ny });
 		}
 	}
 }
 
 int main() {
-	int T;
+	int r, c;
 	char ch;
-	cin >> T;
-	for (int X = 0; X < T; X++) {
+	forX() {
 		cin >> r >> c;
 		vvi jb = mi(r, c, inf);
 		vvi fb = mi(r, c, inf);
 		vector<pii> jstart, fstart;
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				cin >> ch;
-				if (ch == '#') {
-					jb[i][j] = -1;
-					fb[i][j] = -1;
-				} else if (ch == 'J')
-					jstart.push_back({ i, j });
-				else if (ch == 'F')
-					fstart.push_back({ i, j });
-			}
+		
+		drep(i, j, r, c) {
+			cin >> ch;
+			if (ch == '#') {
+				jb[i][j] = -1;
+				fb[i][j] = -1;
+			} else if (ch == 'J')
+				jstart.push_back({ i, j });
+			else if (ch == 'F')
+				fstart.push_back({ i, j });
 		}
 
-		floodfill(jstart, jb);
-		floodfill(fstart, fb);
+		floodfill(jb, jstart);
+		floodfill(fb, fstart);
 
 		int mint = inf;
 		for (int i = 0; i < r; i++) {
