f6b451b8079af473d92ddf5f9d98e059f700dd00
Aaron <Aaron@AARON-PC>
2016-10-24 01:33:21 -0400

    AC

--- UVa/10600/10653.cpp
+++ UVa/10600/10653.cpp
@@ -1,35 +1,17 @@
 #include <bits/stdc++.h>
-#include "header.h"
+#include "floodfill.h"
 
-int r, c;
-vvi board;
-vi dx = { 1, 0, -1, 0 }, dy = { 0, 1, 0, -1 };
-int floodfill(int x0, int y0, int tx, int ty) {
-	queue<pii> q;
-	q.push({ x0, y0 });
-	board[x0][y0] = 0;
-	while (!q.empty()) {
-		int x1 = q.front().x, y1 = q.front().y;
-		q.pop();
-		rep(i, 0, sz(dx)) {
-			int nx = x1 + dx[i], ny = y1 + dy[i];
-			if (nx < 0 || nx >= r || ny < 0 || ny >= c || board[nx][ny] == 0)
-				continue;
-			if (nx == tx && ny == ty)
-				return board[x1][y1] + 1;
-			if (board[nx][ny] == -1 || board[x1][y1] + 1 >= board[nx][ny])
-				continue;
-
-			board[nx][ny] = board[x1][y1] + 1;
-			q.push({ nx, ny });
-		}
-	}
+bool rule(int& curg, int& newg, int source, int target, int& curv, int& newv) {
+	if (newg < inf)
+		return false;
+	newg = curg + 1;
+	return true;
 }
 
 int main() {
-	int nr, cr, nc, cc;
+	int r, c, nr, cr, nc, cc;
 	while (cin >> r >> c && (r | c) != 0) {
-		board = mi(r, c, inf);
+		vvi board = mi(r, c, inf);
 		cin >> nr;
 		rep(i, 0, nr) {
 			cin >> cr >> nc;
@@ -39,6 +21,6 @@ int main() {
 			}
 		}
 		cin >> nr >> nc >> cr >> cc;
-		printf("%d\n", floodfill(nr, nc, cr, cc));
+		printf("%d\n", floodfill(board, nr, nc, 0, vi({ 1, 0, -1, 0 }), vi({ 0, 1, 0, -1 }), cr, cc, rule));
 	}
 }
