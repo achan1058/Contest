a16884e38bc781f401c9c5a56563e0c91d5b9b01
Aaron <Aaron@AARON-PC>
2016-10-24 12:45:25 -0400

    AC

--- UVa/12600/12602.cpp
+++ UVa/12600/12602.cpp
@@ -2,10 +2,8 @@
 #include "header.h"
 
 int main() {
-	int T;
-	cin >> T;
 	string s;
-	for (int X = 0; X < T; X++) {
+	forX() {
 		cin >> s;
 		if (abs((26 * 26 * (s[0] - 'A') + 26 * (s[1] - 'A') + (s[2] - 'A')) -
 			(10 * 10 * 10 * (s[4] - '0') + 10 * 10 * (s[5] - '0') + 10 * (s[6] - '0') + (s[7] - '0'))) <= 100)
