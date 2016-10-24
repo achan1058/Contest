0890014c3395095a6dfb87fc46f99d532da98222
Aaron <Aaron@AARON-PC>
2016-10-24 03:19:08 -0400

    AC

--- UVa/400/469.cpp
+++ UVa/400/469.cpp
@@ -1,69 +1,42 @@
 #include <bits/stdc++.h>
-#include "header.h"
+#include "floodfill.h"
 
-int r, c;
-vs board;
-vi dx = { 1, 0, -1, 0, 1, 1, -1, -1 }, dy = { 0, 1, 0, -1, 1, -1, -1, 1 };
-int floodfill(int x0, int y0) {
-	queue<pii> q;
-	q.push({ x0, y0 });
-	board[x0][y0] = 'R';
-	int total = 1;
-
-	while (!q.empty()) {
-		int x1 = q.front().x, y1 = q.front().y;
-		q.pop();
-
-		rep(i, 0, sz(dx)) {
-			int nx = x1 + dx[i], ny = y1 + dy[i];
-			if (nx < 0 || nx >= r || ny < 0 || ny >= c || board[nx][ny] != 'W')
-				continue;
-
-			total++;
-			board[nx][ny] = 'R';
-			q.push({ nx, ny });
-		}
+int t;
+bool rule(char& curg, char& newg, char source, char target, int& curv, int& newv, bool first) {
+	if (newg == source) {
+		newg = target;
+		newv = t;
+		return true;
 	}
-	return total;
+	return false;
 }
 
 int main() {
-	int N;
 	string s;
-	cin >> N;
-	getline(cin, s);
-	getline(cin, s);
-	rep(X, 0, N) {
-		board.clear();
-		printf("%s", X > 0 ? "\n" : "");
+	forX() {
+		vs board;
+		if (X == 1)
+			getline(cin, s);
 		while (getline(cin, s) && (s[0] == 'W' || s[0] == 'L'))
 			board.pb(s);
 
-		r = sz(board);
-		c = sz(board[0]);
+		int r = sz(board), c = sz(board[0]);
+		t = 1;
 		vvi bc = mi(r, c, 0);
-		rep(i, 0, r) {
-			rep(j, 0, c) {
-				if (board[i][j] == 'W') {
-					int t = floodfill(i, j);
-					rep(x, 0, r) {
-						rep(y, 0, c) {
-							if (board[x][y] == 'R') {
-								board[x][y] = 'L';
-								bc[x][y] = t;
-							}
-						}
-					}
-				}
+		map<int, int> ans;
+		drep(i, j, r, c) {
+			if (board[i][j] == 'W') {
+				ans[t] = floodfill(board, i, j, 'L', vi({ 1, 0, -1, 0, 1, 1, -1, -1 }), vi({ 0, 1, 0, -1, 1, -1, -1, 1 }), -1, -1, rule, &bc);
+				t++;
 			}
 		}
 
+		printX("");
 		do {
-			stringstream ss;
+			stringstream ss(s);
 			int x, y;
-			ss << s;
 			ss >> x >> y;
-			printf("%d\n", bc[x - 1][y - 1]);
+			printf("%d\n", ans[bc[x - 1][y - 1]]);
 		} while (getline(cin, s) && s != "");
 	}
 }
