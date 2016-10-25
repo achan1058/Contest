6c805042df3d1871d31e2c0d6aa1c44d0c68a802
Aaron <Aaron@AARON-PC>
2016-10-25 01:24:30 -0400

    AC

--- UVa/100/190.cpp
+++ UVa/100/190.cpp
@@ -6,7 +6,7 @@ int main() {
 	while (cin >> a >> b >> c) {
 		Circle<double> circle(a, b, c);
 		char sn1 = '-', sn2 = '-', sn3 = '+';
-		double v = circle.c.norm2() - circle.r * circle.r;
+		double v = circle.c.norm2() - circle.r2;
 		if (circle.c.x < eps)
 			sn1 = '+';
 		if (circle.c.y < eps)
@@ -14,7 +14,7 @@ int main() {
 		if (v < -eps)
 			sn3 = '-';
 
-		printf("(x %c %.3f)^2 + (y %c %.3f)^2 = %.3f^2\n", sn1, fabs(circle.c.x), sn2, fabs(circle.c.y), circle.r);
+		printf("(x %c %.3f)^2 + (y %c %.3f)^2 = %.3f^2\n", sn1, fabs(circle.c.x), sn2, fabs(circle.c.y), circle.radius());
 		printf("x^2 + y^2 %c %.3fx %c %.3fy %c %.3f = 0\n\n", sn1, 2 * fabs(circle.c.x), sn2, 2 * fabs(circle.c.y), sn3, fabs(v));
 	}
 }
