3086fc7b1c8accd4b3055b10125349f0eded221c
Aaron <Aaron@AARON-PC>
2016-10-26 18:37:05 -0400

    Fixed to handle a test case that isn't in judge.

--- UVa/11100/11110.cpp
+++ UVa/11100/11110.cpp
@@ -14,11 +14,16 @@ int main() {
 				grid[r - 1][c - 1] = i;
 		}
 		bool good = true;
+		vb used(n);
 		drep(i, j, n, n) {
-			if (grid[i][j] != 0 && floodfill(grid, i, j, 0) != n) {
+			if (grid[i][j] == 0)
+				continue;
+			int color = grid[i][j] - 1;
+			if (used[color] || floodfill(grid, i, j, 0) != n) {
 				good = false;
 				break;
 			}
+			used[color] = true;
 		}
 		printf("%s\n", good ? "good" : "wrong");
 	}
