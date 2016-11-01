ca69ae43d482b8c22ccf2775a76c448421174a56
Aaron <Aaron@AARON-PC>
2016-11-01 02:14:45 -0400

    AC

--- UVa/10300/10301.cpp
+++ UVa/10300/10301.cpp
@@ -1,28 +1,26 @@
 #include <bits/stdc++.h>
 #include "point.h"
-#include "graph_util.h"
+#include "union_find.h"
 
 int main() {
 	int n;
 	while (cin >> n && n != -1) {
 		vector<ptd> rings(n);
 		vd radius(n);
-		vvi graph(n);
+		UnionFind graph(n);
 		rep(i, 0, n) {
 			cin >> rings[i] >> radius[i];
 			rep(j, 0, i) {
 				double d = rings[i].dist(rings[j]);
-				if (radius[i] + radius[j] > d + eps && abs(radius[i] - radius[j]) < d - eps) {
-					graph[i].pb(j);
-					graph[j].pb(i);
-				}
+				if (radius[i] + radius[j] > d + eps && abs(radius[i] - radius[j]) < d - eps)
+					graph.join(i, j);
 			}
 		}
 
 		int biggest = 0;
-		vvi comps = getComponents(graph);
-		irep(c, comps)
-			biggest = max(biggest, sz(c));
+		vi comps = graph.getComponentSizes();
+		irep(v, comps)
+			biggest = max(biggest, v);
 		printf("The largest component contains %d ring%s.\n", biggest, biggest == 1 ? "" : "s");
 	}
 }
