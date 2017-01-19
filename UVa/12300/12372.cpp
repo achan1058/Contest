84706aded063a46bb046d08bce048a254b4dca7a
Aaron Chan <achan1058@hotmail.com>
2017-01-18 22:29:53 -0800

    AC

---
+++ UVa/12300/12372.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int a, b, c;
+		cin >> a >> b >> c;
+		printc("%s\n", a <= 20 && b <= 20 && c <= 20 ? "good" : "bad");
+	}
+}
