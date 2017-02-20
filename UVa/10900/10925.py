c9756f9fe77ee1c1539cbe7b76fbca35f872327b
Aaron Chan <achan1058@hotmail.com>
2017-02-19 21:45:16 -0800

    AC

--- UVa/10900/10925.py
+++ UVa/10900/10925.py
@@ -2,9 +2,9 @@ X = 0
 while True:
     X += 1
     n, m = map(int, input().split())
-    total = 0
     if n == 0 and m == 0:
         exit()
-    for i in range(0, n):
+    total = 0
+    for i in range(n):
         total += int(input())
     print("Bill #%d costs %d: each friend should pay %d\n" % (X, total, total // m))
