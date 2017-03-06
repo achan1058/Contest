688c88f99c4920b6d9306142d83c649d51a283db
Aaron Chan <achan1058@hotmail.com>
2017-03-05 18:51:21 -0800

    AC

---
+++ UVa/400/424.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main() {
+	system("python UVa/400/424.py < input.txt");
+}
---
+++ UVa/400/424.py
@@ -0,0 +1,7 @@
+ans = 0
+while True:
+    n = int(input())
+    if n == 0 :
+        print(ans)
+        exit()
+    ans += n
