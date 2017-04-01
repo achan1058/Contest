1f8c15ba1aa4fbe5f8e579f8406aeceaad84e324
Aaron Chan <achan1058@hotmail.com>
2017-04-01 16:31:03 -0700

    Changed the code so that it doesn't need to handle left and right separately.

--- TopCoder/TCO/TCO 2017 Round 1A/PolygonRotation.cpp
+++ TopCoder/TCO/TCO 2017 Round 1A/PolygonRotation.cpp
@@ -19,62 +19,32 @@ public:
 		int n = sz(x);
 		x.pb(x[0]);
 		y.pb(y[0]);
-		vector<Segment<double>> right, left;
-		vector<Line<double>> lineright, lineleft;
-		rep(i, 1, n) {
-			right.pb(Segment<double>(ptd(x[i - 1], y[i - 1]), ptd(x[i], y[i])));
-			lineright.pb(Line<double>(ptd(x[i - 1], y[i - 1]), ptd(x[i], y[i])));
-			if (i != 0 && x[i] == 0)
-				break;
+		vector<Segment<double>> segs;
+		vector<Line<double>> lines;
+		rep(i, 1, n + 1) {
+			if (y[i] <= y[i - 1]) {
+				segs.pb(Segment<double>(ptd(x[i - 1], y[i - 1]), ptd(x[i], y[i])));
+				lines.pb(Line<double>(ptd(x[i - 1], y[i - 1]), ptd(x[i], y[i])));
+			} else {
+				segs.pb(Segment<double>(ptd(abs(x[i]), y[i]), ptd(-x[i - 1], y[i - 1])));
+				lines.pb(Line<double>(ptd(abs(x[i]), y[i]), ptd(-x[i - 1], y[i - 1])));
+			}
 		}
-		rrep(i, n, 0) {
-			left.pb(Segment<double>(ptd(-x[i], y[i]), ptd(-x[i - 1], y[i - 1])));
-			lineleft.pb(Line<double>(ptd(-x[i], y[i]), ptd(-x[i - 1], y[i - 1])));
-			if (x[i - 1] == 0)
-				break;
-		}
-
 		vd heights;
 		irep(h, y)
 			heights.pb(h);
-		rep(i, 0, sz(left)) {
-			rep(j, 0, sz(right)) {
-				if (left[i].intersect(right[j]) == 1) {
-					ptd inter = lineleft[i].intersect(lineright[j]);
-					if (inter.y != inf)
-						heights.pb(inter.y);
-				}
+		drep(i, j, n, n) {
+			if (segs[i].intersect(segs[j]) == 1) {
+				ptd inter = lines[i].intersect(lines[j]);
+				if (inter.y != inf)
+					heights.pb(inter.y);
 			}
 		}
 
 		sort(all(heights));
 		vd width(sz(heights));
 		rep(i, 0, sz(heights)) {
-			irep(s, right) {
-				assert(s.p2.y <= s.p1.y);
-				assert(s.p1.x > -eps);
-				assert(s.p2.x > -eps);
-				if (abs(heights[i] - s.p1.y) < eps) {
-					width[i] = max(width[i], s.p1.x);
-					continue;
-				}
-				if (abs(heights[i] - s.p2.y) < eps) {
-					width[i] = max(width[i], s.p2.x);
-					continue;
-				}
-				if (heights[i] > s.p1.y)
-					continue;
-				if (heights[i] < s.p2.y)
-					continue;
-
-				double dy = heights[i] - s.p2.y;
-				double dx = dy / (s.p1.y - s.p2.y) * (s.p1.x - s.p2.x);
-				width[i] = max(width[i], dx + s.p2.x);
-			}
-			irep(s, left) {
-				assert(s.p2.y <= s.p1.y);
-				assert(s.p1.x > -eps);
-				assert(s.p2.x > -eps);
+			irep(s, segs) {
 				if (abs(heights[i] - s.p1.y) < eps) {
 					width[i] = max(width[i], s.p1.x);
 					continue;
