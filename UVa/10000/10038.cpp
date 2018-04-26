5a5bfe09cbb130d4ea034a44cfb87736949d7ce2
Aaron Chan <achan1058@hotmail.com>
2018-04-25 23:57:40 -0700

    AC

---
+++ UVa/10000/10038.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	while (cin >> n) {
+		vi nums(n);
+		vb hit(n);
+		read(nums);
+		bool jolly = true;
+		rep(i, 1, sz(nums)) {
+			int d = abs(nums[i] - nums[i - 1]);
+			if (d >= n || hit[d]) {
+				jolly = false;
+				break;
+			}
+			hit[d] = true;
+		}
+		printf("%s\n", jolly ? "Jolly" : "Not jolly");
+	}
+}
