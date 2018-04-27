a5ec4be7b641023be474d4c04aa9fd467d04960d
Aaron Chan <achan1058@hotmail.com>
2018-04-27 07:34:25 -0700

    AC

---
+++ UVa/400/445.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include "utils.h"
+#include "header.h"
+
+int main() {
+	string s;
+	while (getline(cin, s)) {
+		int count = 0;
+		irep(c, s) {
+			if (c == '!') {
+				printf("\n");
+				continue;
+			}
+			if (c == 'b')
+				c = ' ';
+			if (isdigit(c)) {
+				count += c - '0';
+			} else {
+				rep(i, 0, count)
+					printf("%c", c);
+				count = 0;
+			}
+		}
+		printf("\n");
+	}
+}
