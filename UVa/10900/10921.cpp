54d4e96141c363b39020af4b6652d4ff33f85fae
Aaron Chan <achan1058@hotmail.com>
2018-04-27 07:59:51 -0700

    AC

---
+++ UVa/10900/10921.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vs digits = { "", "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+	map<char, int> m;
+	rep(i, 0, sz(digits)) {
+		irep(c, digits[i])
+			m[c] = i;
+	}
+	
+	string s;
+	while (cin >> s) {
+		irep(c, s) {
+			if (isupper(c))
+				printf("%d", m[c]);
+			else
+				printf("%c", c);
+		}
+		printf("\n");
+	}
+}
