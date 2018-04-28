245bbea6a1f3f808214d2052ba15a6eeb40ca523
Aaron Chan <achan1058@hotmail.com>
2018-04-27 22:14:18 -0700

    AC

---
+++ UVa/11700/11799.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, v, ans = 0;
+		cin >> n;
+		rep(i, 0, n) {
+			cin >> v;
+			ans = max(ans, v);
+		}
+		printc("%d", ans);
+	}
+}
