291e26269b5bbec297b9a1ef905af86795850c05
Aaron Chan <achan1058@hotmail.com>
2018-04-27 20:45:33 -0700

    Temp commit.

---
+++ UVa/400/414.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() { // BAD
+	int n;
+	while (cin >> n && n != 0) {
+		string s;
+		getline(cin, s);
+		int mn = inf, total = 0;
+		rep(i, 0, n) {
+			int count = 0;
+			getline(cin, s);
+			irep(c, s)
+				count += c == ' ' ? 1 : 0;
+
+			mn = min(mn, count);
+			total += count;
+		}
+		print("%d", total - mn * n);
+	}
+}
