d32c8e77681a66c5d132542922ba31bf5dce3327
Aaron Chan <achan1058@hotmail.com>
2018-04-27 21:03:30 -0700

    Reverting header.h

--- Library/header.h
+++ Library/header.h
@@ -14,6 +14,8 @@ typedef vector<vd> vvd;
 typedef vector<vb> vvb;
 typedef vector<vs> vvs;
 typedef vector<vl> vvl;
+template <class T> using vvp = vector<vector<pair<int, T>>>;
+template <class T> using vt = vector<tuple<int, int, T>>;
 template <class T> inline void read(vector<T>& v, int b = 0, int e = 0) { for (int k = b; k < int(v.size()) - e; k++) cin >> v[k]; }
 template <class T> inline void read(vector<vector<T>>& v) { for (auto& p : v) read(p); }
 
@@ -45,6 +47,6 @@ ll mod = 1000000007ll;
 #define whileX(a) int X = 0; while(X++, (a))
 #define print(...) printf(__VA_ARGS__), printf("\n")
 #define printX(...) printf("%s", (X > 1 ? "\n" : "")), printf(__VA_ARGS__)
-#define printc(...) printf("Case %d: ", X), printf(__VA_ARGS__)
-#define printp(...) printf("Case #%d: ", X), printf(__VA_ARGS__)
+#define printc(...) printf("Case %d: ", X), printf(__VA_ARGS__), printf("\n")
+#define printp(...) printf("Case #%d: ", X), printf(__VA_ARGS__), printf("\n")
 struct FastIO { FastIO() { cin.sync_with_stdio(0); cin.tie(0); } }; FastIO fio;
