692856e87b330f7a07c8faff4204407c9c147571
Aaron Chan <achan1058@hotmail.com>
2018-04-28 07:36:18 -0700

    Removed old tokenizer()

--- Library/old/util.h
+++ Library/old/util.h
@@ -32,17 +32,3 @@ map<T, int> tally(const vector<T>& arr) {
 		result[v]++;
 	return result;
 }
-
-// test include_empty = true
-vector<string> tokenizer(const string& s, int(tokenFunc)(int) = isspace, bool include_empty = false) {
-	vector<string> result;
-	int front = 0;
-	for (int i = 0; i <= int(s.size()); i++) {
-		if (i == s.size() || tokenFunc(s[i])) {
-			if (include_empty || i != front)
-				result.push_back(s.substr(front, i - front));
-			front = i + 1;
-		}
-	}
-	return result;
-}
