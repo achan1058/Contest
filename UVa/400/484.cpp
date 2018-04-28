40a52909295de97e111154a8d184d1ae4297730e
Aaron Chan <achan1058@hotmail.com>
2018-04-28 10:32:13 -0700

    AC

---
+++ UVa/400/484.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	unordered_map<int, int> m;
+	vi order;
+	int n;
+	while (cin >> n) {
+		m[n]++;
+		if (m[n] == 1)
+			order.pb(n);
+	}
+	irep(v, order)
+		print("%d %d", v, m[v]);
+}
