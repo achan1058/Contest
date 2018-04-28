5628773f4069dac53d33fa777fbaad3c697297e6
Aaron Chan <achan1058@hotmail.com>
2018-04-28 07:34:27 -0700

    AC

---
+++ UVa/500/562.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, v, total = 0, mn = inf;
+		cin >> n;
+		vb poss(50001);
+		poss[0] = true;
+		rep(i, 0, n) {
+			cin >> v;
+			total += v;
+			rrep(j, 50000, v)
+				poss[j] = poss[j] || poss[j - v];
+		}
+
+		rep(i, 0, total / 2 + 1) {
+			if (poss[i])
+				mn = min(mn, total - 2 * i);
+		}
+		print("%d", mn);
+	}
+}
