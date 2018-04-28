c8c02a810edbef96821f6325099dec6fd4cd4f09
Aaron Chan <achan1058@hotmail.com>
2018-04-27 22:49:31 -0700

    AC

---
+++ UVa/10000/10008.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	map<char, int> m;
+	forX() {
+		string s;
+		getline(cin, s);
+		irep(c, s) {
+			if (isalpha(c))
+				m[toupper(c)]++;
+		}
+	}
+
+	vector<pair<int, char>> ans;
+	irep(p, m)
+		ans.pb({ -p.second, p.first });
+	sort(all(ans));
+	irep(p, ans)
+		print("%c %d", p.second, -p.first);
+}
