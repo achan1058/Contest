5c83a1f9c5c79a6cec7ba3e62f10e639776b0b8e
Aaron Chan <achan1058@hotmail.com>
2018-04-28 10:43:07 -0700

    AC

---
+++ UVa/10400/10474.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k, v;
+	whileX(cin >> n >> k && (n | k != 0)) {
+		vi tally(10001);
+		vb hit(10001);
+		rep(i, 0, n) {
+			cin >> v;
+			tally[v]++;
+			hit[v] = true;
+		}
+		rep(i, 1, 10000)
+			tally[i] += tally[i - 1];
+		print("CASE# %d:", X);
+		rep(i, 0, k) {
+			cin >> v;
+			if (hit[v])
+				print("%d found at %d", v, tally[v - 1] + 1);
+			else
+				print("%d not found", v);
+		}
+	}
+}
