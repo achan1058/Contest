5b6cfa23f988fcb5cc8f2974d4ca518d62f39371
Aaron Chan <achan1058@hotmail.com>
2018-04-28 08:23:36 -0700

    AC

---
+++ UVa/900/900.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vl ans(51);
+	ans[0] = ans[1] = 1;
+	rep(i, 2, 51)
+		ans[i] = ans[i - 1] + ans[i - 2];
+
+	int n;
+	while (cin >> n && n != 0)
+		print("%lld", ans[n]);
+}
