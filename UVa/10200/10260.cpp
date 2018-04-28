a98c5f1d5a9771c206675c98639e22a99a421212
Aaron Chan <achan1058@hotmail.com>
2018-04-28 08:19:43 -0700

    AC

---
+++ UVa/10200/10260.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vs decode = { "BFPV", "CGJKQSXZ", "DT", "L", "MN", "R" };
+	map<char, int> encode;
+	rep(i, 0, sz(decode)) {
+		irep(c, decode[i])
+			encode[c] = i + 1;
+	}
+
+	string s;
+	while (getline(cin, s)) {
+		int cur = 0;
+		rep(i, 0, sz(s)) {
+			if (encode[s[i]] == cur)
+				continue;
+			cur = encode[s[i]];
+			if (cur != 0)
+				printf("%d", cur);
+		}
+		print("");
+	}
+}
