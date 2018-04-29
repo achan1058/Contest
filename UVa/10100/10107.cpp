868de931858bfceea7e357f78436af8a112ff5b5
Aaron Chan <achan1058@hotmail.com>
2018-04-29 12:52:32 -0700

    AC

---
+++ UVa/10100/10107.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	vi nums;
+	while (cin >> n) {
+		nums.pb(n);
+		sort(all(nums));
+		int d = sz(nums) / 2;
+		print("%d", sz(nums) % 2 ? nums[d] : (nums[d - 1] + nums[d]) / 2);
+	}
+}
