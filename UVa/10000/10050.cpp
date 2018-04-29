54b84823562580b51500b5eadd90050ee5d6a597
Aaron Chan <achan1058@hotmail.com>
2018-04-28 22:49:56 -0700

    AC

---
+++ UVa/10000/10050.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int N, ans = 0, v, p;
+		cin >> N >> p;
+		vb days(N + 1);
+		rep(i, 0, p) {
+			cin >> v;
+			for (int j = v; j <= N; j += v) {
+				if (j % 7 == 6 || j % 7 == 0)
+					continue;
+				ans += days[j] ? 0 : 1;
+				days[j] = true;
+			}
+		}
+		print("%d", ans);
+	}
+}
