f1ae048e9d960eb692cf54cb6c4906458aeca8fd
Aaron Chan <achan1058@hotmail.com>
2018-04-29 07:58:55 -0700

    AC

---
+++ UVa/10400/10420.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	map<string, int> conquests;
+	forX() {
+		string s;
+		getline(cin, s);
+		stringstream ss(s);
+		ss >> s;
+		conquests[s]++;
+	}
+	irep(con, conquests)
+		print("%s %d", con.x.c_str(), con.y);
+}
