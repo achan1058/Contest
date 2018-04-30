6f4bf2fe726ff67696d3b49cb161ba8374d10b17
Aaron Chan <achan1058@hotmail.com>
2018-04-29 22:42:21 -0700

    AC

---
+++ UVa/11400/11417.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	while (cin >> n && n != 0) {
+		int g = 0;
+		rep(i, 1, n) {
+			rep(j, i + 1, n + 1)
+				g += gcd(i, j);
+		}
+		print("%d", g);
+	}
+}
