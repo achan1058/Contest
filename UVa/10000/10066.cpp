52921dabc9f194cc0a801ec5d3101294833ea7a5
Aaron Chan <achan1058@hotmail.com>
2018-04-30 07:56:58 -0700

    AC

---
+++ UVa/10000/10066.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+#include "header.h"
+
+int main() {
+	int n1, n2;
+	whileX(cin >> n1 >> n2 && (n1 | n2 != 0)) {
+		vi t1(n1), t2(n2);
+		read(t1);
+		read(t2);
+		print("Twin Towers #%d", X);
+		print("Number of Tiles : %d\n", sz(lcs(t1, t2)));
+	}
+}
