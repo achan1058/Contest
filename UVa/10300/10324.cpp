7e09c2f0f8c3fe26a8a96eeef2e0cceb10006542
Aaron Chan <achan1058@hotmail.com>
2018-04-30 00:15:01 -0700

    AC

---
+++ UVa/10300/10324.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	whileX(cin >> s) {
+		int n, a, b;
+		vi tally(sz(s) + 1);
+		rep(i, 0, sz(s))
+			tally[i + 1] = tally[i] + s[i] - '0';
+		cin >> n;
+		print("Case %d:", X);
+		rep(i, 0, n) {
+			cin >> a >> b;
+			if (a > b)
+				swap(a, b);
+			int diff = tally[b + 1] - tally[a];
+			print("%s", diff == 0 || diff == b - a + 1 ? "Yes" : "No");
+		}
+	}
+}
