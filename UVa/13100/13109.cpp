a125bdf4d8b2c70121c285ade4a12381b7e5a1f4
Aaron Chan <achan1058@hotmail.com>
2018-04-30 21:05:30 -0700

    AC

---
+++ UVa/13100/13109.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, w, ans = 0;
+		cin >> n >> w;
+		vi elephants(n);
+		read(elephants);
+		sort(all(elephants));
+		irep(e, elephants) {
+			if (e > w)
+				break;
+			ans++;
+			w -= e;
+		}
+		print("%d", ans);
+	}
+}
