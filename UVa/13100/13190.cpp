0684ac0a4ae794cc0d5ad46cad49d4992bb8ca29
Aaron Chan <achan1058@hotmail.com>
2018-04-30 23:03:51 -0700

    AC

---
+++ UVa/13100/13190.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, k, f;
+		string s;
+		cin >> n >> k;
+		priority_queue<tuple<int, int, string>> q;
+		unordered_map<int, int> freq;
+
+		rep(i, 0, n) {
+			cin >> s >> f;
+			freq[-i] = -f;
+			q.push({ -f, -i, s });
+		}
+
+		rep(i, 0, k) {
+			auto t = q.top();
+			q.pop();
+			print("%d %s", -get<0>(t), get<2>(t).c_str());
+			q.push({ get<0>(t) + freq[get<1>(t)], get<1>(t), get<2>(t) });
+		}
+	}
+}
