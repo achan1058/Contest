05fd9c5efe9b371868fee7c0fd77aa668cc64a60
Aaron Chan <achan1058@hotmail.com>
2018-04-30 22:15:00 -0700

    AC

---
+++ UVa/13100/13130.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		vi dice(5);
+		read(dice);
+		bool good = true;
+		rep(i, 0, 4) {
+			if (dice[i] + 1 != dice[i + 1])
+				good = false;
+		}
+		print("%c", good ? 'Y' : 'N');
+	}
+}
