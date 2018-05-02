548b77e4f4056d8473b63842a7b25c275863d404
Aaron Chan <achan1058@hotmail.com>
2018-05-01 23:25:03 -0700

    AC

---
+++ UVa/11200/11292.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, m;
+	while (cin >> n >> m && (n | m != 0)) {
+		vi heads(n), knights(m);
+		ll cost = 0;
+		read(heads);
+		read(knights);
+		sort(all(heads));
+		sort(all(knights));
+		int hi = 0, ki = 0;
+		while (hi < sz(heads) && ki < sz(knights)) {
+			if (knights[ki] >= heads[hi]) {
+				cost += knights[ki];
+				hi++;
+			}
+			ki++;
+		}
+		if (hi == sz(heads))
+			print("%lld", cost);
+		else
+			print("Loowater is doomed!");
+	}
+}
