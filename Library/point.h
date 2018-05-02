8b46b3192849174d189d091d7d51736d034abab7
Aaron Chan <achan1058@hotmail.com>
2018-05-02 12:38:01 -0700

    Minor cleanup of point

--- Library/point.h
+++ Library/point.h
@@ -14,7 +14,7 @@ struct point {
 	point operator-(point p) const { return point(x - p.x, y - p.y); }
 	point operator*(T c) const { return point(x * c, y * c); }
 	point operator/(T c) const { return point(x / c, y / c); }
-	bool operator<(point p) const { return x < p.x || (x == p.x && y < p.y); }
+	bool operator<(point p) const { return x < p.x || (x == p.x && y < p.y); } // use above()? is this even needed?
 	bool operator==(point p) const { return x == p.x && y == p.y; }
 
 	T dot(point p) const { return x * p.x + y * p.y; }
@@ -26,7 +26,7 @@ struct point {
 	double dist(point p) const { return sqrt(dist2(p)); }
 	int above(point p) const { return p.y > y || (p.y == y && p.x > x) ? 1 : (p.y < y || p.x < x ? -1 : 0); }
 
-	static inline point<double> anglePoint(double dist, double angle) {
+	static point<double> anglePoint(double dist, double angle) {
 		return point<double>(dist * cos(angle), dist * sin(angle));
 	}
 
@@ -34,11 +34,11 @@ struct point {
 	void angle_sort(typename vector<point<T>>::iterator begin, typename vector<point<T>>::iterator end) {
 		sort(begin, end, [&](point<T> p1, point<T> p2) {
 			T d = sa2(p1, p2);
-			return d > T(0) ? true : (d < T(0) ? false : dist2(p1) < dist2(p2));
+			return d > T(0) || (d == T(0) && dist2(p1) < dist2(p2))); // test this
 		});
 	}
 };
 template<class T> istream& operator >> (istream& in, point<T>& p) { return in >> p.x >> p.y; }
-template<class T> ostream& operator << (ostream& out, const point<T>& p) { return out << p.x << ',' << p.y; }
+template<class T> ostream& operator << (ostream& out, const point<T>& p) { return out << p.x << ',' << p.y; } // need this?
 typedef point<int> pti;
 typedef point<double> ptd;
