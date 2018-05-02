933194e1a59ca2faa9ef973e11742a79d995499f
Aaron Chan <achan1058@hotmail.com>
2018-05-02 16:45:34 -0700

    AC

---
+++ Euler/1/1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int sum(int n) {
+	int total = 0;
+	rep(i, 1, n) {
+		if (i % 3 == 0 || i % 5 == 0)
+			total += i;
+	}
+	return total;
+}
+
+int main() {
+	assert(sum(10) == 23);
+	print("%d", sum(1000));
+}
