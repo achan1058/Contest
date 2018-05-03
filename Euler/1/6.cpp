214b4764e3f5782ba1ad9bda5b38baad27b60a61
Aaron Chan <achan1058@hotmail.com>
2018-05-03 10:21:19 -0700

    AC

---
+++ Euler/1/6.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int diff(int n) {
+	int sum_sq = 0, sum = 0;
+	rep(i, 1, n + 1) {
+		sum_sq += i * i;
+		sum += i;
+	}
+	return sum * sum - sum_sq;
+}
+int main() {
+	assert(diff(10) == 2640);
+	print("%d", diff(100));
+}
