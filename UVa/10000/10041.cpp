a15d897e7199598417f2439ca3252d98c7c46bc6
Aaron Chan <achan1058@hotmail.com>
2018-05-04 22:59:11 -0700

    AC

---
+++ UVa/10000/10041.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, ans = 0;
+		cin >> n;
+		vi houses(n);
+		read(houses);
+		sort(all(houses));
+		irep(v, houses)
+			ans += abs(v - houses[n / 2]);
+		print("%d", ans);
+	}
+}
