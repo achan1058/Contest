dfbbe8f790f6a6e0997476b9f70bbe22a11a104a
Aaron Chan <achan1058@hotmail.com>
2018-05-05 00:15:28 -0700

    AC

---
+++ UVa/11500/11547.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n;
+		cin >> n;
+		n = (n * 567 / 9 + 7492) * 235 / 47 - 498;
+		print("%d", abs(n / 10) % 10);
+	}
+}
