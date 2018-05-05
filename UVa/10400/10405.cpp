e223c55609b658e1268292ffa9260d94724b6dec
Aaron Chan <achan1058@hotmail.com>
2018-05-05 00:43:45 -0700

    AC

---
+++ UVa/10400/10405.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+#include "header.h"
+
+int main() {
+	string s1, s2;
+	while (getline(cin, s1)) {
+		getline(cin, s2);
+		print("%d", sz(lcs(s1, s2)));
+	}
+}
