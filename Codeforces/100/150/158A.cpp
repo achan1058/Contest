f364ba14f67a586cfb689fdd0c56cd5530166eb9
Aaron Chan <achan1058@hotmail.com>
2018-05-06 11:15:03 -0700

    AC

---
+++ Codeforces/100/150/158A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k, ans = 0, prev = inf, v;
+	cin >> n >> k;
+	rep(i, 0, n) {
+		cin >> v;
+		if (v == 0 || (i >= k && v != prev))
+			break;
+		prev = v;
+		ans++;
+	}
+	print("%d", ans);
+}
