8e17c1526c3aca35957c97d49a136a0fc354f97a
Aaron Chan <achan1058@hotmail.com>
2018-05-06 10:15:26 -0700

    AC

---
+++ UVa/10000/10013.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, a, b;
+		cin >> n;
+		vi digits(n);
+		irep(v, digits) {
+			cin >> a >> b;
+			v = a + b;
+		}
+		rrep(i, sz(digits) - 1, 1) {
+			if (digits[i] >= 10) {
+				digits[i] -= 10;
+				digits[i - 1]++;
+			}
+		}
+		printX("");
+		irep(v, digits)
+			printf("%c", v + '0');
+		print("");
+	}
+}
