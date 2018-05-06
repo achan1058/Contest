1952f86320564d4495b6de73a7947393bc01df51
Aaron Chan <achan1058@hotmail.com>
2018-05-05 22:47:04 -0700

    AC

---
+++ UVa/500/575.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	while (cin >> s && s != "0") {
+		ll ans = 0, base = 1;
+		rrep(i, sz(s) - 1, 0) {
+			ans += base * (s[i] - '0');
+			base = 2 * base + 1;
+		}
+		print("%lld", ans);
+	}
+}
