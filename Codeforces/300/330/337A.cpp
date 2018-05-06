22cd670e92a147b4a8d233bcdc85c353e9188234
Aaron Chan <achan1058@hotmail.com>
2018-05-06 12:05:38 -0700

    AC

---
+++ Codeforces/300/330/337A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k, ans = inf;
+	cin >> k >> n;
+	vi puzzles(n);
+	read(puzzles);
+	sort(all(puzzles));
+	rep(i, 0, n - k + 1)
+		ans = min(ans, puzzles[i + k - 1] - puzzles[i]);
+	print("%d", ans);
+}
