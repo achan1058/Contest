b66adfdfea9cf7568ecfe8ac762996f1f74ba575
Aaron Chan <achan1058@hotmail.com>
2018-05-06 23:18:32 -0700

    AC

---
+++ Codeforces/200/260/268A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, ans = 0;
+	cin >> n;
+	vi home(n), guest(n);
+	rep(i, 0, n)
+		cin >> home[i] >> guest[i];
+	drep(i, j, n, n) {
+		if (i == j)
+			continue;
+		if (home[i] == guest[j])
+			ans++;
+	}
+	print("%d", ans);
+}
