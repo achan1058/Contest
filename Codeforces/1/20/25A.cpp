0b8b68f068b54775178a9d01153efde74de6bf84
Aaron Chan <achan1058@hotmail.com>
2018-05-06 23:22:29 -0700

    AC

---
+++ Codeforces/1/20/25A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, v, e = -1, o = -1;
+	cin >> n;
+	rep(i, 0, n) {
+		cin >> v;
+		if (v % 2) {
+			if (o == -1)
+				o = i + 1;
+			else
+				o = -2;
+		} else {
+			if (e == -1)
+				e = i + 1;
+			else
+				e = -2;
+		}
+	}
+
+	print("%d", o > 0 ? o : e);
+}
