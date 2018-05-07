ba7f2d51de67be1b3f84ee3e58507f621f7a071f
Aaron Chan <achan1058@hotmail.com>
2018-05-07 09:24:38 -0700

    AC

---
+++ Codeforces/200/230/231A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int ans = 0, v;
+	forX() {
+		int tally = 0;
+		rep(i, 0, 3) {
+			cin >> v;
+			tally += v;
+		}
+		ans += tally >= 2 ? 1 : 0;
+	}
+	print("%d", ans);
+}
