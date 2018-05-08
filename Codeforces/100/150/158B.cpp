849b3eecf79d0fcc4c8501bf602c42125c2e3785
Aaron Chan <achan1058@hotmail.com>
2018-05-07 21:36:58 -0700

    AC

---
+++ Codeforces/100/150/158B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vi tally(5);
+	int v;
+	forX() {
+		cin >> v;
+		tally[v]++;
+	}
+	int ans = tally[4];
+	ans += tally[3];
+	tally[1] = max(tally[1] - tally[3], 0);
+	ans += tally[2] / 2;
+	tally[2] %= 2;
+	if (tally[2]) {
+		ans++;
+		tally[1] = max(tally[1] - 2, 0);
+	}
+	ans += (tally[1] + 3) / 4;
+	print("%d", ans);
+}
