d24a7fba7ac6f611291dcb231ea0380c08f09fc7
Aaron Chan <achan1058@hotmail.com>
2018-05-08 00:02:38 -0700

    AC

---
+++ Codeforces/200/260/266A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, ans = 0;
+	string s;
+	cin >> n >> s;
+	rep(i, 1, n) {
+		if (s[i] == s[i - 1])
+			ans++;
+	}
+	printf("%d", ans);
+}
