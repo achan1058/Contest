0b860c6be6bef63c7c025a2ad55c8fc6ea63b6d1
Aaron Chan <achan1058@hotmail.com>
2018-05-09 00:09:43 -0700

    AC

---
+++ Codeforces/1/60/61A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s1, s2;
+	cin >> s1 >> s2;
+	rep(i, 0, sz(s1)) {
+		if (s2[i] == '1')
+			s1[i] = 1 - s1[i] + 2 * '0';
+	}
+	print("%s", s1.c_str());
+}
