b788bdc7a9b79c129ce5152373bbe73396d36d20
Aaron Chan <achan1058@hotmail.com>
2018-05-09 08:33:10 -0700

    AC

---
+++ Codeforces/300/370/379A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k, ans = 0;
+	cin >> n >> k;
+	ans += n;
+	while (n >= k) {
+		ans += n / k;
+		n = n % k + n / k;
+	}
+	print("%d", ans);
+}
