4f1dae8756d0910b7796b6720583287a4c5131ba
Aaron Chan <achan1058@hotmail.com>
2018-05-09 08:29:34 -0700

    AC

---
+++ Codeforces/100/140/144A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	cin >> n;
+	vi order(n);
+	read(order);
+	int low = 0, high = 0;
+	rep(i, 1, n) {
+		if (order[i] <= order[low])
+			low = i;
+		if (order[i] > order[high])
+			high = i;
+	}
+	int ans = high + n - low - 1;
+	if (ans >= n)
+		ans--;
+	print("%d", ans);
+}
