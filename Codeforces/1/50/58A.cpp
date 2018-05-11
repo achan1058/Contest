4389df6a82d21baa86392c964f9e0dc2c586b6bc
Aaron Chan <achan1058@hotmail.com>
2018-05-11 08:43:27 -0700

    AC

---
+++ Codeforces/1/50/58A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s, hello = "hello";
+	cin >> s;
+	int i = 0, hi = 0;
+	while (i < sz(s) && hi < 5) {
+		if (s[i] == hello[hi])
+			hi++;
+		i++;
+	}
+	print("%s", hi == 5 ? "YES" : "NO");
+}
