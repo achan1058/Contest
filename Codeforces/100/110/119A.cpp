0221621d5a9f89950e4d0f2b6d41c683b955ce86
Aaron Chan <achan1058@hotmail.com>
2018-05-11 08:59:13 -0700

    AC

---
+++ Codeforces/100/110/119A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vi num(2);
+	int n, turn = 0;
+	read(num);
+	cin >> n;
+	while (n > 0) {
+		n -= gcd(n, num[turn]);
+		turn = 1 - turn;
+	}
+	print("%d", 1 - turn);
+}
