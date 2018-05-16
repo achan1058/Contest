52ae6ca2739003d4d912d43aee1ea799bcc1fb5b
Aaron Chan <achan1058@hotmail.com>
2018-05-15 23:35:41 -0700

    AC

---
+++ UVa/600/673.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	forX() {
+		getline(cin, s);
+		string remain;
+		bool good = true;
+		irep(c, s) {
+			if (c == '(' || c == '[') {
+				remain.pb(c);
+			} else if (c == ']') {
+				if (remain.empty() || remain.back() != '[') {
+					good = false;
+					break;
+				}
+				remain.pop_back();
+			} else if (c == ')') {
+				if (remain.empty() || remain.back() != '(') {
+					good = false;
+					break;
+				}
+				remain.pop_back();
+			}
+		}
+		print("%s", good && remain.empty() ? "Yes" : "No");
+	}
+}
