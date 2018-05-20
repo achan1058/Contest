ebb34370b2162b71ab4833a3db22a0eb4175f81b
Aaron Chan <achan1058@hotmail.com>
2018-05-20 12:45:35 -0700

    AC

---
+++ UVa/10200/10252.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s1, s2;
+	while (getline(cin, s1)) {
+		getline(cin, s2);
+		sort(all(s1));
+		sort(all(s2));
+		int t1 = 0, t2 = 0;
+		while (t1 < sz(s1) && t2 < sz(s2)) {
+			if (s1[t1] == s2[t2]) {
+				printf("%c", s1[t1]);
+				t1++;
+				t2++;
+			} else if (s1[t1] < s2[t2]) {
+				t1++;
+			} else {
+				t2++;
+			}
+		}
+		print("");
+	}
+}
