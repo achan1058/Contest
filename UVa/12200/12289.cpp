c41473385ba7656710fe7e1a39810918b8e872ba
Aaron Chan <achan1058@hotmail.com>
2018-05-23 22:33:39 -0700

    AC

---
+++ UVa/12200/12289.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		string s, one = "one";
+		cin >> s;
+		if (sz(s) == 5) {
+			print("3");
+			continue;
+		}
+
+		int count = 0;
+		rep(i, 0, 3) {
+			if (s[i] == one[i])
+				count++;
+		}
+		print("%d", count >= 2 ? 1 : 2);
+	}
+}
