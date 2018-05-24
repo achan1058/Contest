fb663d79f1703566cd31d610837ae64b1fc31779
Aaron Chan <achan1058@hotmail.com>
2018-05-24 00:58:54 -0700

    AC

---
+++ UVa/500/514.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	while (cin >> n && n != 0) {
+		vi trains(n);
+
+		while (cin >> trains[0] && trains[0] != 0) {
+			rep(i, 1, n)
+				cin >> trains[i];
+			int next = 0;
+			stack<int> todo;
+			rep(i, 1, n + 1) {
+				todo.push(i);
+				while (!todo.empty() && todo.top() == trains[next]) {
+					todo.pop();
+					next++;
+				}
+			}
+			print("%s", next == n ? "Yes" : "No");
+		}
+		print("");
+	}
+}
