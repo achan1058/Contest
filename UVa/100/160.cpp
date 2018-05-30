217d4c70a20a82219551bfe57a93568a3827b9d9
Aaron Chan <achan1058@hotmail.com>
2018-05-30 08:50:00 -0700

    AC

---
+++ UVa/100/160.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+#include "header.h"
+
+int main() {
+	vector<map<int, int>> primes(101);
+	rep(n, 2, 101) {
+		primes[n] = primes[n - 1];
+		irep(p, factor(n))
+			primes[n][p]++;
+	}
+	int n;
+	while (cin >> n && n != 0) {
+		printf("%3d! =", n);
+		int t = 0;
+		irep(p, primes[n]) {
+			t++;
+			printf("%3d", p.y);
+			if (t == sz(primes[n])) {
+				print("");
+			} else if (t % 15 == 0) {
+				print("");
+				printf("      ");
+			}
+		}
+	}
+}
