e91280e2ab86fcb7d2331437f0e89288cda07458
Aaron Chan <achan1058@hotmail.com>
2018-05-30 09:28:32 -0700

    Modified 10394.

--- UVa/10300/10394.cpp
+++ UVa/10300/10394.cpp
@@ -3,11 +3,13 @@
 #include "header.h"
 
 int main() {
-	vi primes = sieve(20000000), twins;
+	vb primes = sieveVector(20000000);
+	vi twins;
 	rep(i, 0, sz(primes) - 1) {
-		if (primes[i] + 2 == primes[i + 1])
-			twins.pb(primes[i]);
+		if (primes[i] && primes[i + 1])
+			twins.push_back(2 * i + 1);
 	}
+
 	int n;
 	while (cin >> n) {
 		print("(%d, %d)", twins[n - 1], twins[n - 1] + 2);
