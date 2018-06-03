c7184c3c80f0d3644f78f9a08927323daa233cfe
Aaron Chan <achan1058@hotmail.com>
2018-06-03 00:11:11 -0700

    AC

---
+++ UVa/100/116.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int r, c;
+	while (cin >> r >> c) {
+		vvi grid = mi(r, c, 0), next = mi(r, c, 0), tally = mi(r, c, inf);
+		read(grid);
+		rep(i, 0, r)
+			tally[i][c - 1] = grid[i][c - 1];
+		rrep(j, c - 1, 1) {
+			rep(i, 0, r) {
+				int i2 = i;
+				if (tally[i][j] + grid[i2][j - 1] < tally[i2][j - 1]) {
+					tally[i2][j - 1] = tally[i][j] + grid[i2][j - 1];
+					next[i2][j - 1] = i;
+				}
+				i2 = (i + 1) % r;
+				if (tally[i][j] + grid[i2][j - 1] < tally[i2][j - 1]) {
+					tally[i2][j - 1] = tally[i][j] + grid[i2][j - 1];
+					next[i2][j - 1] = i;
+				}
+				i2 = (i + r - 1) % r;
+				if (tally[i][j] + grid[i2][j - 1] < tally[i2][j - 1]) {
+					tally[i2][j - 1] = tally[i][j] + grid[i2][j - 1];
+					next[i2][j - 1] = i;
+				}
+			}
+		}
+
+		int best = inf, start = 0;
+		rep(i, 0, r) {
+			if (tally[i][0] < best) {
+				best = tally[i][0];
+				start = i;
+			}
+		}
+		rep(j, 0, c - 1) {
+			printf("%d ", start + 1);
+			start = next[start][j];
+		}
+		print("%d", start + 1);
+		print("%d", best);
+	}
+}
