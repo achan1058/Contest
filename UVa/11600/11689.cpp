a271c1c1e608e668a74626bdf8d8f4ce3b2b3165
Aaron Chan <achan1058@hotmail.com>
2018-06-08 09:21:38 -0700

    AC

---
+++ UVa/11600/11689.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, e, c, ans = 0;
+		cin >> n >> e >> c;
+		n += e;
+
+		while (n >= c) {
+			ans += n / c;
+			n = n / c + n % c;
+		}
+		print("%d", ans);
+	}
+}
