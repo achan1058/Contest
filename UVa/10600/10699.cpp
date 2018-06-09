4f6f55f5514a90bc6f83a0cca10059c09e2ef9a3
Aaron Chan <achan1058@hotmail.com>
2018-06-09 12:14:43 -0700

    AC

---
+++ UVa/10600/10699.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+#include "utils.h"
+#include "header.h"
+
+int main() {
+	int n;
+	while (cin >> n && n != 0)
+		print("%d : %d", n, sz(tally(factor(n))));
+}
