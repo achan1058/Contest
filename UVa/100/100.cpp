ae2da31778ed258d9809a4d91e106797047ef71f
Aaron Chan <achan1058@hotmail.com>
2018-06-12 08:58:18 -0700

    AC

---
+++ UVa/100/100.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int a, b;
+	while (cin >> a >> b) {
+		int mx = 0;
+		rep(i, min(a, b), max(a, b) + 1) {
+			ll n = i;
+			int cur = 1;
+			while (n != 1) {
+				if (n % 2)
+					n = 3 * n + 1;
+				else
+					n /= 2;
+				cur++;
+			}
+			mx = max(mx, cur);
+		}
+		print("%d %d %d", a, b, mx);
+	}
+}
