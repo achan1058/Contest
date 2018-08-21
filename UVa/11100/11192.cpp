1c95d74c8258ea99437b01748f2c8cdfc0446b68
Aaron Chan <achan1058@hotmail.com>
2018-08-20 22:44:51 -0700

    AC

---
+++ UVa/11100/11192.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	string s;
+	while (cin >> n >> s && n != 0) {
+		int t = sz(s) / n;
+		rep(i, 0, n)
+			reverse(s.begin() + i * t, s.begin() + (i + 1) * t);
+		print("%s", s.c_str());
+	}
+}
