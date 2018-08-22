66bc5b31d1bde405585adc1061e1b903994776f6
Aaron Chan <achan1058@hotmail.com>
2018-08-22 08:15:12 -0700

    AC

---
+++ UVa/400/482.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		map<int, int> perm;
+		getline(cin, s);
+
+		{
+			getline(cin, s);
+			stringstream ss(s);
+			int i = 0, v;
+			while (ss >> v) {
+				perm[i] = v - 1;
+				i++;
+			}
+		}
+
+		vs ans(sz(perm));
+
+		{
+			getline(cin, s);
+			stringstream ss2(s);
+			rep(i, 0, sz(perm))
+				ss2 >> ans[perm[i]];
+		}
+
+		printX("");
+		irep(out, ans)
+			print("%s", out.c_str());
+	}
+}
