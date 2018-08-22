9ebec2e872ab40ec8ac65df728013c1fb5e135fb
Aaron Chan <achan1058@hotmail.com>
2018-08-22 07:46:31 -0700

    AC

---
+++ UVa/200/231.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+#include "header.h"
+
+int main() {
+	vi heights;
+	int n;
+	whileX(cin >> n && n != -1) {
+		heights.clear();
+		heights.pb(-n);
+		while (cin >> n && n != -1)
+			heights.pb(-n);
+		printX("Test #%d:\n", X);
+		print("  maximum possible interceptions: %d", sz(lis(heights).x));
+	}
+}
