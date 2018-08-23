5eb0e3ec7dbc3e3e59df26a7d49d4a76a688cf4c
Aaron Chan <achan1058@hotmail.com>
2018-08-23 08:09:02 -0700

    AC

---
+++ UVa/10900/10954.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	while (cin >> n && n != 0) {
+		priority_queue<int> q;
+		int u, v, ans = 0;
+		rep(i, 0, n) {
+			cin >> v;
+			q.push(-v);
+		}
+
+		while (sz(q) > 1) {
+			u = q.top();
+			q.pop();
+			v = q.top();
+			q.pop();
+			ans += u + v;
+			q.push(u + v);
+		}
+		print("%d", -ans);
+	}
+}
