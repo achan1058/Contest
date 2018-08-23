ccc993771afac476bae4150ea6ceddff8c428464
Aaron Chan <achan1058@hotmail.com>
2018-08-22 20:47:03 -0700

    AC

---
+++ UVa/12200/12279.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	whileX(cin >> n && n != 0) {
+		int ans = 0, v;
+		rep(i, 0, n) {
+			cin >> v;
+			if (v > 0)
+				ans++;
+			else
+				ans--;
+		}
+		printc("%d", ans);
+	}
+}
