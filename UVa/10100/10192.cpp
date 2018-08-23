a7ea4a09b446a6923953a58b7b323b5a099325b8
Aaron Chan <achan1058@hotmail.com>
2018-08-22 21:12:44 -0700

    AC

---
+++ UVa/10100/10192.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+#include "header.h"
+
+int main() {
+	string s1, s2;
+	whileX(getline(cin, s1) && s1[0] != '#') {
+		getline(cin, s2);
+		printp("you can visit at most %d cities.", sz(lcs(s1, s2).x));
+	}
+}
