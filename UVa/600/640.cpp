af712c0457412693f2bc7e2f78d202112bd5244d
Aaron Chan <achan1058@hotmail.com>
2018-08-24 08:14:31 -0700

    AC

---
+++ UVa/600/640.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int getNext(int n) {
+	int ans = n;
+	while (n > 0) {
+		ans += n % 10;
+		n /= 10;
+	}
+	return ans;
+}
+
+int main() {
+	vb gen(1000001);
+	rep(i, 0, 1000001) {
+		int g = getNext(i);
+		if (g <= 1000000)
+			gen[g] = true;
+	}
+	rep(i, 0, 1000001) {
+		if (!gen[i])
+			print("%d", i);
+	}
+}
