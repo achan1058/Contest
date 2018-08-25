39c2fcec2a89ed661bc37115c3454df506fbc06d
Aaron Chan <achan1058@hotmail.com>
2018-08-24 22:44:44 -0700

    AC

---
+++ UVa/500/536.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+void recurse(const string& s1, const string& s2) {
+	rep(i, 0, sz(s2)) {
+		if (s1[0] == s2[i]) {
+			recurse(s1.substr(1, i), s2.substr(0, i));
+			recurse(s1.substr(i + 1, sz(s2) - i - 1), s2.substr(i + 1, sz(s2) - i - 1));
+			printf("%c", s1[0]);
+			return;
+		}
+	}
+}
+
+int main() {
+	string s1, s2;
+	while (cin >> s1 >> s2) {
+		recurse(s1, s2);
+		print("");
+	}
+}
