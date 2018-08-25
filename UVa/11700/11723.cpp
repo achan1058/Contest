5353d4b410ea4786f3d19feb25589df21c4fa102
Aaron Chan <achan1058@hotmail.com>
2018-08-24 21:39:13 -0700

    AC

---
+++ UVa/11700/11723.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, r;
+	whileX(cin >> n >> r && (n | r) != 0) {
+		int d = (n - 1) / r;
+		if (d <= 26)
+			printc("%d", d);
+		else
+			printc("impossible");
+	}
+}
