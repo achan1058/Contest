d484d297454aed91ae1154202de9f77f1a3aba47
Aaron Chan <achan1058@hotmail.com>
2018-08-26 01:22:18 -0700

    AC

---
+++ UVa/10400/10450.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vl ans(51);
+	ans[0] = 1;
+	ans[1] = 2;
+	rep(i, 2, 51)
+		ans[i] = ans[i - 1] + ans[i - 2];
+	forX() {
+		int n;
+		cin >> n;
+		print("Scenario #%d:", X);
+		print("%lld\n", ans[n]);
+	}
+}
