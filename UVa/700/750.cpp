5bd73ea71543024116254be24f8a01a5f2342d96
Aaron Chan <achan1058@hotmail.com>
2018-08-26 02:05:57 -0700

    AC

---
+++ UVa/700/750.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+bool isGood(const vi& queens) {
+	drep(i, j, 8, i) {
+		if (i - j == abs(queens[i] - queens[j]))
+			return false;
+	}
+	return true;
+}
+
+int main() {
+	vi queens = { 1, 2, 3, 4, 5, 6, 7, 8 };
+	vvi solutions;
+	do {
+		if (isGood(queens))
+			solutions.pb(queens);
+	} while (next_permutation(all(queens)));
+
+	forX() {
+		int r, c, t = 1;
+		cin >> r >> c;
+		printX("SOLN       COLUMN\n");
+		print(" #      1 2 3 4 5 6 7 8\n");
+		irep(sol, solutions) {
+			if (sol[c - 1] == r) {
+				printf("%2d     ", t);
+				rep(i, 0, 8)
+					printf(" %d", sol[i]);
+				print("");
+				t++;
+			}
+		}
+	}
+}
