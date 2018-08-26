b6c79282badba7573a8d4236548e5009fb04c3e8
Aaron Chan <achan1058@hotmail.com>
2018-08-26 09:20:10 -0700

    AC

---
+++ UVa/10100/10170.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	ll s, d;
+	while (cin >> s >> d) {
+		int n = ceil((-1 + sqrt(1 + 4 * s * s - 4 * s + 8 * d)) / 2);
+		print("%d", n);
+	}
+}
