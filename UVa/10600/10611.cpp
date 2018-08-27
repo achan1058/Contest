ac9e7a36726317be2e7496c4ea4f5bf37b19d812
Aaron Chan <achan1058@hotmail.com>
2018-08-27 07:52:08 -0700

    AC

---
+++ UVa/10600/10611.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k, h;
+	cin >> n;
+	vi heights(n);
+	read(heights);
+
+	forX() {
+		cin >> h;
+		auto lit = lower_bound(all(heights), h);
+		if (lit == heights.begin())
+			printf("X ");
+		else
+			printf("%d ", *(lit - 1));
+		auto rit = upper_bound(all(heights), h);
+		if (rit == heights.end())
+			print("X");
+		else
+			print("%d", *rit);
+	}
+}
