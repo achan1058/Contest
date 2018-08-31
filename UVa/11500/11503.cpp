e4ea22b32cbdcf47bea8226bc68ef6235bdb288f
Aaron Chan <achan1058@hotmail.com>
2018-08-31 09:18:30 -0700

    AC

---
+++ UVa/11500/11503.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "utils.h"
+#include "union_find.h"
+#include "header.h"
+
+int main() {
+	forX() {
+		int n;
+		cin >> n;
+		string s1, s2;
+		mapper<string> m;
+		UnionFind un;
+
+		rep(i, 0, n) {
+			cin >> s1 >> s2;
+			int v1 = m.map(s1), v2 = m.map(s2);
+			un.join(v1, v2);
+			print("%d", un.getSize(v1));
+		}
+	}
+}
