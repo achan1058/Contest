bc462e44355e3cd97f4a5fee2562d0e4aac72bfb
Aaron Chan <achan1058@hotmail.com>
2018-08-30 22:02:40 -0700

    AC

---
+++ UVa/10200/10295.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k, v;
+	cin >> n >> k;
+	string s;
+	map<string, int> value;
+	rep(i, 0, n) {
+		cin >> s >> v;
+		value[s] = v;
+	}
+	rep(i, 0, k) {
+		ll total = 0;
+		while (cin >> s && s != ".")
+			total += value[s];
+		print("%lld", total);
+	}
+}
