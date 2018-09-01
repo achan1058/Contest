b552e0e3f1c062f7de2e0fde1670a901ff989dfe
Aaron Chan <achan1058@hotmail.com>
2018-08-31 22:35:21 -0700

    AC

---
+++ UVa/400/446.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		char c;
+		string s1, s2;
+		cin >> s1 >> c >> s2;
+		int v1 = stoi(s1, 0, 16), v2 = stoi(s2, 0, 16), ans;
+		if (c == '+')
+			ans = v1 + v2;
+		else
+			ans = v1 - v2;
+
+		print("%s %c %s = %d", bitset<13>(v1).to_string().c_str(), c, bitset<13>(v2).to_string().c_str(), ans);
+	}
+}
