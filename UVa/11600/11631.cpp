79f6f59505d2d86c4fb0b100a4f74236bc3f6e30
Aaron Chan <achan1058@hotmail.com>
2018-08-31 20:17:41 -0700

    AC

---
+++ UVa/11600/11631.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "spanning_tree.h"
+#include "header.h"
+
+int main() {
+	int v, e, v1, v2, c;
+	while (cin >> v >> e && (v | e) != 0) {
+		int total = 0;
+		edge_list<int> edges;
+
+		rep(i, 0, e) {
+			cin >> v1 >> v2 >> c;
+			total += c;
+			edges.pb({ v1, v2, c });
+		}
+		print("%d", total - get<0>(kruskal(edges, v)));
+	}
+}
