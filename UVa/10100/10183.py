f26eb6977e620be730cab6635ea99fe9563cf71f
Aaron Chan <achan1058@hotmail.com>
2018-08-31 22:01:14 -0700

    AC

---
+++ UVa/10100/10183.py
@@ -0,0 +1,11 @@
+from bisect import *
+
+fib = [1, 2]
+while fib[-1] < 10**100:
+    fib.append(fib[-1] + fib[-2])
+while True:
+    small,big = map(int,input().split())
+    if (small | big) == 0:
+        exit()
+
+    print(bisect_right(fib, big) - bisect_left(fib, small))
