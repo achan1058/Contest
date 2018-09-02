e3e9cf04f33b223dcb71c67cc7f59252042664a6
Aaron Chan <achan1058@hotmail.com>
2018-09-02 10:29:39 -0700

    AC

---
+++ UVa/13100/13115.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, n2;
+		cin >> n2;
+		vvi grid = mi(n2, n2, 0);
+		read(grid);
+		n = sqrt(n2);
+
+		bool good = true;
+		rep(i, 0, n2) {
+			vb hit(n2);
+			rep(j, 0, n2) {
+				if (hit[grid[i][j] - 1]) {
+					good = false;
+					goto end;
+				}
+				hit[grid[i][j] - 1] = true;
+			}
+		}
+
+		rep(j, 0, n2) {
+			vb hit(n2);
+			rep(i, 0, n2) {
+				if (hit[grid[i][j] - 1]) {
+					good = false;
+					goto end;
+				}
+				hit[grid[i][j] - 1] = true;
+			}
+		}
+
+		drep(i, j, n, n) {
+			vb hit(n2);
+			drep(i2, j2, n, n) {
+				if (hit[grid[i * n + i2][j * n + j2] - 1]) {
+					good = false;
+					goto end;
+				}
+				hit[grid[i * n + i2][j * n + j2] - 1] = true;
+			}
+		}
+	end:;
+		print("%s", good ? "yes" : "no");
+	}
+}
