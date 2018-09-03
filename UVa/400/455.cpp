bbfd4886876ab764ba07dd21b6c3b56893d2a18b
Aaron Chan <achan1058@hotmail.com>
2018-09-03 16:07:52 -0700

    AC

---
+++ UVa/400/455.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		cin >> s;
+		string s2 = s + s;
+		printX("%d\n", kmp(s2, s)[1]);
+	}
+}
