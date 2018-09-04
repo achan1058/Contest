4afc709f2ef68103ee6a29d7ebf1b2f358e2913f
Aaron Chan <achan1058@hotmail.com>
2018-09-03 17:45:21 -0700

    AC

---
+++ UVa/400/469.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include "floodfill.h"
+#include "utils.h"
+#include "header.h"
+
+int main() {
+	forX() {
+		dx = dx8;
+		dy = dy8;
+		vs board;
+		string s;
+		if (X == 1)
+			getline(cin, s);
+		while (getline(cin, s) && (s[0] == 'L' || s[0] == 'W'))
+			board.pb(s);
+
+		printX("");
+		do {
+			int r, c;
+			stringstream ss(s);
+			ss >> r >> c;
+			char fill = board[r - 1][c - 1] == 'W' ? 'U' : 'W';
+			int ans = floodfill(board, r - 1, c - 1, fill);
+			print("%d", ans);
+		} while (getline(cin, s) && s != "");
+	}
+}
