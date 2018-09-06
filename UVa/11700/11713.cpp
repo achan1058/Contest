b4bc7533bfb617962c97d0bf4210829400c53088
Aaron Chan <achan1058@hotmail.com>
2018-09-06 08:34:43 -0700

    AC

---
+++ UVa/11700/11713.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+void transform(string& s) {
+	irep(c, s) {
+		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+			c = ' ';
+	}
+}
+
+int main() {
+	forX() {
+		string s1, s2;
+		cin >> s1 >> s2;
+		transform(s1);
+		transform(s2);
+		print("%s", s1 == s2 ? "Yes" : "No");
+	}
+}
