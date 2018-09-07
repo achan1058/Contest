83d63eeeeba28483455cfe405ccd1db3063f64db
Aaron Chan <achan1058@hotmail.com>
2018-09-07 16:29:37 -0700

    AC

---
+++ UVa/11600/11611.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	string s;
+	while (cin >> n >> s && n != 0) {
+		int nextr = inf, nextd = inf, best = inf;
+		irep(c, s) {
+			nextr++;
+			nextd++;
+			if (c == 'Z') {
+				best = 0;
+				break;
+			} else if (c == 'R') {
+				nextr = 0;
+				best = min(best, nextd);
+			} else if (c == 'D') {
+				nextd = 0;
+				best = min(best, nextr);
+			}
+		}
+		print("%d", best);
+	}
+}
