50f49281fda8d48ed35c41b143dbb2e6cc3d7f7a
Aaron Chan <achan1058@hotmail.com>
2018-09-07 12:00:03 -0700

    Fixed tabs.

--- UVa/10100/10183.py
+++ UVa/10100/10183.py
@@ -2,10 +2,10 @@ from bisect import *
 
 fib = [1, 2]
 while fib[-1] < 10**100:
-    fib.append(fib[-1] + fib[-2])
+	fib.append(fib[-1] + fib[-2])
 while True:
-    small,big = map(int,input().split())
-    if (small | big) == 0:
-        exit()
+	small,big = map(int,input().split())
+	if (small | big) == 0:
+		exit()
 
-    print(bisect_right(fib, big) - bisect_left(fib, small))
+	print(bisect_right(fib, big) - bisect_left(fib, small))
