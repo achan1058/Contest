d3d2b169d3634f4f7c504c9a14e22a413eea3ff3
Aaron Chan <achan1058@hotmail.com>
2018-09-06 22:33:04 -0700

    AC

---
+++ UVa/11500/11541.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		cin >> s;
+		char last = 0;
+		int count = 0;
+		printf("Case %d: ", X);
+		irep(c, s) {
+			if (c >= '0' && c <= '9') {
+				count *= 10;
+				count += c - '0';
+			} else {
+				rep(i, 0, count)
+					printf("%c", last);
+				last = c;
+				count = 0;
+			}
+		}
+		rep(i, 0, count)
+			printf("%c", last);
+		print("");
+	}
+}
