d22b33f0dcc606ca1d5a3abb9f7273d31491b6d0
Aaron Chan <achan1058@hotmail.com>
2018-09-07 16:49:08 -0700

    AC

---
+++ UVa/10400/10465.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int m, n, t;
+	while (cin >> m >> n >> t) {
+		int bestn = inf, bestb = inf;
+		rep(i, 0, t + 1) {
+			if (i * m > t)
+				break;
+			int rem = t - i * m;
+			int burger = rem / n + i, beer = rem % n;
+			if (beer < bestb || (beer == bestb && burger > bestn)) {
+				bestb = beer;
+				bestn = burger;
+			}
+		}
+		if (bestb == 0)
+			print("%d", bestn);
+		else
+			print("%d %d", bestn, bestb);
+	}
+}
