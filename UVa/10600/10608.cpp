c566a6e1c5351fad91802e92ebe582c96371c49c
Aaron Chan <achan1058@hotmail.com>
2018-09-07 13:57:36 -0700

    AC

---
+++ UVa/10600/10608.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "union_find.h"
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, m;
+		cin >> n >> m;
+		UnionFind un(n);
+		rep(i, 0, m) {
+			int v1, v2;
+			cin >> v1 >> v2;
+			un.join(v1 - 1, v2 - 1);
+		}
+
+		int ans = 1;
+		rep(i, 0, n)
+			ans = max(ans, un.getSize(i));
+		print("%d", ans);
+	}
+}
