4e26faa487dcc211d149ff55814b2b587fd13d2c
Aaron Chan <achan1058@hotmail.com>
2018-09-08 08:57:29 -0700

    AC

---
+++ UVa/10900/10986.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "shortest_path.h"
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, m, s, t, v1, v2, w;
+		cin >> n >> m >> s >> t;
+		adj_list<int> graph(n);
+		rep(i, 0, m) {
+			cin >> v1 >> v2 >> w;
+			graph[v1].pb({ v2, w });
+			graph[v2].pb({ v1, w });
+		}
+		int ans = dijkstra(graph, s).x[t];
+		if (ans == inf)
+			printp("unreachable");
+		else
+			printp("%d", ans);
+	}
+}
