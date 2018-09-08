545f7395f4dba20572848f454cea2ff726f399ba
Aaron Chan <achan1058@hotmail.com>
2018-09-07 22:41:31 -0700

    AC

---
+++ UVa/400/490.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	vs grid;
+	while (getline(cin, s)) {
+		if (!grid.empty() && sz(s) < sz(grid.back()))
+			s.resize(sz(grid.back()), ' ');
+		grid.pb(s);
+	}
+	rep(j, 0, sz(grid.back())) {
+		rrep(i, sz(grid) - 1, 0) {
+			if (j >= sz(grid[i]))
+				break;
+			printf("%c", grid[i][j]);
+		}
+		print("");
+	}
+}
