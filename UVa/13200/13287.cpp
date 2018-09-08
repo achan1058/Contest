fd5162d192d60a00bd537dda084dd04a28537139
Aaron Chan <achan1058@hotmail.com>
2018-09-07 22:28:31 -0700

    AC

---
+++ UVa/13200/13287.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int w, n, v1, v2;
+	while (cin >> w) {
+		cin >> n;
+		int total = 0;
+		rep(i, 0, n) {
+			cin >> v1 >> v2;
+			total += v1 * v2;
+		}
+		print("%d", total / w);
+	}
+}
