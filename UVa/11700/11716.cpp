7181ee91d9567fd28e819d0e1d0a36183a9d1f99
Aaron Chan <achan1058@hotmail.com>
2018-09-07 18:05:15 -0700

    AC

---
+++ UVa/11700/11716.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		getline(cin, s);
+		int n = sqrt(sz(s));
+		if (n * n != sz(s)) {
+			print("INVALID");
+			continue;
+		}
+		drep(i, j, n, n)
+			printf("%c", s[j * n + i]);
+		print("");
+	}
+}
