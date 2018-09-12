afce51ec8c23272ee16dce26d4466f067f13dd3f
Aaron Chan <achan1058@hotmail.com>
2018-09-12 09:18:22 -0700

    AC

---
+++ UVa/11800/11827.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		getline(cin, s);
+		stringstream ss(s);
+		int v, ans = 0;
+		vi nums;
+		while (ss >> v)
+			nums.pb(v);
+		drep(i, j, sz(nums), i)
+			ans = max(ans, gcd(nums[i], nums[j]));
+		print("%d", ans);
+	}
+}
