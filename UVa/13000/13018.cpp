5b60f0e9fdc7faab1f628ebd897a8bbf884a6f3d
Aaron Chan <achan1058@hotmail.com>
2018-09-13 09:12:59 -0700

    AC

---
+++ UVa/13000/13018.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int v1, v2;
+	whileX(cin >> v1 >> v2) {
+		printX("");
+		rep(i, min(v1, v2) + 1, max(v1, v2) + 2)
+			print("%d", i);
+	}
+}
