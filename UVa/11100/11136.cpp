66151bb17804db1434abf221a083aa67c0198b23
Aaron Chan <achan1058@hotmail.com>
2018-09-14 08:55:51 -0700

    AC

---
+++ UVa/11100/11136.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k, v;
+	while(cin >> n && n != 0) {
+		ll ans = 0;
+		multiset<int> urn;
+		rep(i, 0, n) {
+			cin >> k;
+			rep(j, 0, k) {
+				cin >> v;
+				urn.insert(v);
+			}
+
+			auto it1 = urn.begin();
+			int mn = *it1;
+			urn.erase(it1);
+
+			auto it2 = --urn.end();
+			int mx = *it2;
+			urn.erase(it2);
+
+			ans += mx - mn;
+		}
+		print("%lld", ans);
+	}
+}
