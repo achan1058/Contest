3f5e004fa75cc7a090416282ffcb3a74ee366911
Aaron Chan <achan1058@hotmail.com>
2018-09-13 21:55:16 -0700

    AC

---
+++ UVa/11000/11057.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, cost;
+	while (cin >> n) {
+		vi books(n);
+		read(books);
+		cin >> cost;
+		map<int, int> small, med, large;
+		irep(b, books) {
+			if (2 * b == cost)
+				med[b]++;
+			else if (2 * b < cost)
+				small[b]++;
+			else
+				large[b]++;
+		}
+		if (med[cost / 2] >= 2) {
+			print("Peter should buy books whose prices are %d and %d.\n", cost / 2, cost / 2);
+			continue;
+		}
+
+		irep(p, large) {
+			if (small[cost - p.x] > 0) {
+				print("Peter should buy books whose prices are %d and %d.\n", cost - p.x, p.x);
+				break;
+			}
+		}
+	}
+}
