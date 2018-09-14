c817e9b7b6197d53e6751c99d5ff511d0ac36045
Aaron Chan <achan1058@hotmail.com>
2018-09-14 00:12:28 -0700

    AC

---
+++ UVa/10000/10042.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+#include "header.h"
+
+int digitSum(int n) {
+	int result = 0;
+	while (n > 0) {
+		result += n % 10;
+		n /= 10;
+	}
+	return result;
+}
+
+int main() {
+	int n;
+	forX() {
+		cin >> n;
+		while (n++, true) {
+			if (isPrime(n))
+				continue;
+			int n1 = digitSum(n), n2 = 0;
+			vi fact = factor(n);
+			irep(f, fact)
+				n2 += digitSum(f);
+			if (n1 == n2) {
+				print("%d", n);
+				break;
+			}
+		}
+	}
+}
