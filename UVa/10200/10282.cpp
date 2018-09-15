df0fa9cbfa01c71e6dd798e21de1107e35d0ea37
Aaron Chan <achan1058@hotmail.com>
2018-09-15 01:33:51 -0700

    AC

---
+++ UVa/10200/10282.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	unordered_map<string, string> dict;
+	string s;
+	while (getline(cin, s) && s != "") {
+		string s1, s2;
+		stringstream ss(s);
+		ss >> s1 >> s2;
+		dict[s2] = s1;
+	}
+	while (cin >> s) {
+		if (dict[s].empty())
+			print("eh");
+		else
+			print("%s", dict[s].c_str());
+	}
+}
