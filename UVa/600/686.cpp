1b7f49dde36d1e85512bc2151e2586450185df79
Aaron Chan <achan1058@hotmail.com>
2019-04-21 23:27:03 -0700

    AC

---
+++ UVa/600/686.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+#include "header.h"
+
+int main() {
+	vb primes = sieveVector(40000);
+	int n;
+	while (cin >> n && n != 0) {
+		if (n == 4) {
+			print("1");
+			continue;
+		}
+
+		int ans = 0;
+		for (int i = 3; 2 * i <= n; i += 2) {
+			if (sieveIsPrime(i, primes) && sieveIsPrime(n - i, primes))
+				ans++;
+		}
+		print("%d", ans);
+	}
+}
