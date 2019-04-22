7cd767cebf9f002f350998e84a925ab8d6ca76da
Aaron Chan <achan1058@hotmail.com>
2019-04-22 08:17:03 -0700

    AC

---
+++ UVa/11500/11559.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, b, h, w;
+	while (cin >> n >> b >> h >> w) {
+		int best = inf;
+		int p, a;
+		rep(i, 0, h) {
+			cin >> p;
+			rep(j, 0, w) {
+				cin >> a;
+				if (a >= n)
+					best = min(best, p * n);
+			}
+		}
+		if (best <= b)
+			print("%d", best);
+		else
+			print("stay home");
+	}
+}
