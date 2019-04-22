889b47d51e53179f00db101ae448a8bf461964b0
Aaron Chan <achan1058@hotmail.com>
2019-04-22 08:55:05 -0700

    AC

---
+++ UVa/300/350.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int z, i, m, l;
+	whileX(cin >> z >> i >> m >> l && (z | i | m | l) != 0) {
+		vi cycle(m);
+		int cur = 1;
+		while (cycle[l] == 0) {
+			cycle[l] = cur;
+			l = (z * l + i) % m;
+			cur++;
+		}
+
+		printc("%d", cur - cycle[l]);
+	}
+}
