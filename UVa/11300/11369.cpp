a04789befa2e39814ad951be3bb92ead335d10c2
Aaron Chan <achan1058@hotmail.com>
2019-04-23 08:21:46 -0700

    ac

---
+++ UVa/11300/11369.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, ans = 0;
+		cin >> n;
+		vi items(n);
+		read(items);
+		sort(all(items));
+		for (int i = n - 3; i >= 0; i -= 3)
+			ans += items[i];
+		print("%d", ans);
+	}
+}
