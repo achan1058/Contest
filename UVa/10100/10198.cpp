6ef2bff1a0ba1c1fd9753be2b8818eb2a3dbeee6
Aaron Chan <achan1058@hotmail.com>
2019-04-23 00:14:41 -0700

    ac

---
+++ UVa/10100/10198.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main() {
+	system("python UVa/10100/10198.py < input.txt");
+}
---
+++ UVa/10100/10198.py
@@ -0,0 +1,14 @@
+arr = [0] * 1001
+arr[0] = 1
+arr[1] = 2
+arr[2] = 5
+for i in range(3, 1001):
+	arr[i] = 2 * arr[i - 1] + arr[i - 2] + arr[i - 3];
+
+while True:
+	try:
+		n = int(input())
+	except EOFError:
+		exit()
+	print(arr[n])
+    
