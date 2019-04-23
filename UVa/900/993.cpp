8c9a42cdb9831d33feed1fe97e5a195a2c07a62c
Aaron Chan <achan1058@hotmail.com>
2019-04-22 22:41:05 -0700

    ac

---
+++ UVa/900/993.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n;
+		cin >> n;
+		if (n <= 1) {
+			print("%d", n);
+			continue;
+		}
+		string ans;
+		rrep(i, 9, 2) {
+			while (n % i == 0) {
+				n /= i;
+				ans += i + '0';
+			}
+		}
+		if (n != 1) {
+			print("-1");
+			continue;
+		}
+
+		reverse(all(ans));
+		print("%s", ans.c_str());
+	}
+}
