69333d71f1983c5fc833dd7fc6474577eb196cca
Aaron Chan <achan1058@hotmail.com>
2019-04-23 23:39:11 -0700

    ac

---
+++ UVa/11600/11608.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int p;
+	whileX(cin >> p && p >= 0) {
+		vi prob(12);
+		read(prob);
+		int n;
+		print("Case %d:", X);
+
+		rep(i, 0, 12) {
+			cin >> n;
+			if (p >= n) {
+				print("No problem! :D");
+				p -= n;
+			} else {
+				print("No problem. :(");
+			}
+			p += prob[i];
+		}
+	}
+}
