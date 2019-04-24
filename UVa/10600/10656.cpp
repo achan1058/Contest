39619d9ee66bb0723cfa55e4499db90cfffc6985
Aaron Chan <achan1058@hotmail.com>
2019-04-24 07:31:36 -0700

    ac

---
+++ UVa/10600/10656.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	while (cin >> n && n != 0) {
+		bool printed = false;
+		int t;
+		rep(i, 0, n) {
+			cin >> t;
+			if (t != 0) {
+				printf("%s%d", printed ? " " : "", t);
+				printed = true;
+			}
+		}
+		print("%s", printed ? "" : "0");
+	}
+}
