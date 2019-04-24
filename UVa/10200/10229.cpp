17ec469785ca8b496b786f4526dfcbb7df3a8c2e
Aaron Chan <achan1058@hotmail.com>
2019-04-24 00:02:06 -0700

    ac

---
+++ UVa/10200/10229.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int iter = 0, cur = 0, next = 1;
+	vi ans = { 0, 1 };
+	do {
+		ans.pb((ans[sz(ans) - 2] + ans[sz(ans) - 1]) % (1 << 20));
+	} while (ans[sz(ans) - 2] != 0 || ans[sz(ans) - 1] != 1);
+	ans.pop_back();
+	ans.pop_back();
+	int n, m;
+	while (cin >> n >> m) {
+		print("%d", ans[n % sz(ans)] % (1 << m));
+	}
+}
