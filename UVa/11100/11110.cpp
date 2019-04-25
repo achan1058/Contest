df8452684a5f47517a79ef58665da305aa198ad9
Aaron Chan <achan1058@hotmail.com>
2019-04-25 09:39:11 -0700

    ac

---
+++ UVa/11100/11110.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include "floodfill.h"
+#include "header.h"
+
+int main() {
+	int n;
+	string s;
+	while (cin >> n && n != 0) {
+		getline(cin, s);
+		vvi grid = mi(n, n, 0);
+		int r, c;
+		rep(i, 1, n) {
+			getline(cin, s);
+			stringstream ss(s);
+			while (ss >> r >> c)
+				grid[r - 1][c - 1] = i;
+		}
+
+		bool good = true;
+		drep(i, j, n, n) {
+			if (grid[i][j] >= 0) {
+				int t = floodfill(grid, i, j, -1);
+				if (t != n) {
+					good = false;
+					goto end;
+				}
+			}
+		}
+	end:;
+
+		print("%s", good ? "good" : "wrong");
+	}
+}
