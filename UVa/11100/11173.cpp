873ce1bd45a3f0a2d2369a48dd59c0ae15810f9b
Aaron Chan <achan1058@hotmail.com>
2019-04-25 08:47:42 -0700

    ac

---
+++ UVa/11100/11173.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, b, ans = 0;
+		cin >> b >> n;
+		rrep(i, b - 1, 0) {
+			if (n >= (1 << i)) {
+				ans += 1 << i;
+				n = (1 << (i + 1)) - n - 1;
+			}
+		}
+		print("%d", ans);
+	}
+}
