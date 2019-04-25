0ff5d4b49bca3cb67d5c19d0f9e7fd904f5ce4cb
Aaron Chan <achan1058@hotmail.com>
2019-04-24 23:38:22 -0700

    ac

---
+++ UVa/11100/11151.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		getline(cin, s);
+		string s2 = s;
+		reverse(all(s2));
+		print("%d", sz(lcs(s, s2).x));
+	}
+}
