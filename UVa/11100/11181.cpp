86b66b6a53b4794e88085b2774f19a6eb9e27b62
Aaron Chan <achan1058@hotmail.com>
2019-04-26 08:29:16 -0700

    ac

---
+++ UVa/11100/11181.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, r;
+	whileX(cin >> n >> r && (n | r) != 0) {
+		vd prob(n), per_prob(n);
+		vi perm(n);
+		read(prob);
+
+		rep(i, 0, r)
+			perm[n - r + i] = 1;
+
+		double total = 0;
+		do {
+			double p = 1;
+			rep(i, 0, n) {
+				if (perm[i] == 1)
+					p *= prob[i];
+				else
+					p *= 1 - prob[i];
+			}
+			total += p;
+			rep(i, 0, n) {
+				if (perm[i])
+					per_prob[i] += p;
+			}
+		} while (next_permutation(all(perm)));
+		print("Case %d:", X);
+		rep(i, 0, n)
+			print("%.6f", per_prob[i] / total);
+	}
+}
