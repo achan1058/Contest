bb82921352568b64cbb006a6a657c67b2e2dfdaf
Aaron Chan <achan1058@hotmail.com>
2019-04-25 20:31:08 -0700

    ac

---
+++ UVa/11100/11100.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	whileX(cin >> n && n != 0) {
+		vi bag(n);
+		read(bag);
+		sort(all(bag));
+		int count = 1, best = 1;
+		rep(i, 1, n) {
+			if (bag[i] == bag[i - 1]) {
+				count++;
+				best = max(best, count);
+			} else {
+				count = 1;
+			}
+		}
+		printX("%d\n", best);
+		rep(i, 0, best) {
+			printf("%d", bag[i]);
+			for (int j = i + best; j < n; j += best)
+				printf(" %d", bag[j]);
+			print("");
+		}
+	}
+}
