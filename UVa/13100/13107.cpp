7055b992f21784396acc478d1fbecf6c8f74269a
Aaron Chan <achan1058@hotmail.com>
2019-04-28 00:22:47 -0700

    ac

---
+++ UVa/13100/13107.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	while (cin >> s) {
+		map<char, string> m;
+		string ans;
+		int cur = 1;
+		irep(c, s) {
+			if (m[c] == "") {
+				m[c] = to_string(cur);
+				cur++;
+			}
+			ans += m[c];
+		}
+		irep(c, ans) {
+			if (c == '2')
+				c = '5';
+			else if (c == '5')
+				c = '2';
+			else if (c == '6')
+				c = '9';
+			else if (c == '9')
+				c = '6';
+		}
+		print("%s", ans.c_str());
+	}
+}
