16887c26e05bc05e2554771f895271a15ee2c43f
Aaron Chan <achan1058@hotmail.com>
2019-04-28 12:18:05 -0700

    ac

---
+++ UVa/12800/12820.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include "utils.h"
+#include "header.h"
+
+bool isCool(const string& s) {
+	map<char, bool> m;
+	auto tal = tally(s);
+
+	if (sz(tal) == 1)
+		return false;
+	irep(t, tal) {
+		if (m[t.y])
+			return false;
+		m[t.y] = true;
+	}
+	return true;
+}
+
+int main() {
+	int n;
+	whileX(cin >> n) {
+		int ans = 0;
+		string s;
+		rep(i, 0, n) {
+			cin >> s;
+			ans += (isCool(s) ? 1 : 0);
+		}
+		printc("%d", ans);
+	}
+}
