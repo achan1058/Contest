f125e040e255ab60967f2c8015cb09cbf1ae0023
Aaron Chan <achan1058@hotmail.com>
2019-04-30 09:16:31 -0700

    ac

---
+++ UVa/12600/12626.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "utils.h"
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		cin >> s;
+		auto t = tally(s);
+		int ans = min(min(min(min(min(t['M'], t['A'] / 3), t['R'] / 2), t['G']), t['I']), t['T']);
+		print("%d", ans);
+	}
+}
