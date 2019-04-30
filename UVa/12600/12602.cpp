9b1029b470aa798679182bcb870eaffbff13df71
Aaron Chan <achan1058@hotmail.com>
2019-04-30 09:29:33 -0700

    ac

---
+++ UVa/12600/12602.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		char c1, c2, c3, c;
+		string s;
+		cin >> c1 >> c2 >> c3 >> c >> s;
+		int a = (c1 - 'A') * 26 * 26 + (c2 - 'A') * 26 + (c3 - 'A');
+		int b = stoi(s);
+		print("%s", abs(a - b) <= 100 ? "nice" : "not nice");
+	}
+}
