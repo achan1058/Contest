dafa4b24b73bb56c341791793af17e7b9bf2e7e8
Aaron Chan <achan1058@hotmail.com>
2019-05-03 08:46:07 -0700

    ac

---
+++ UVa/12600/12614.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, best = 0, t;
+		cin >> n;
+		rep(i, 0, n) {
+			cin >> t;
+			best = max(best, t);
+		}
+		printc("%d", best);
+	}
+}
