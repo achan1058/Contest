a40543843bb5e1cf9f2ea088f6bcff5a7d2f7f86
Aaron Chan <achan1058@hotmail.com>
2019-05-02 22:37:15 -0700

    ac

---
+++ UVa/12600/12650.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, r, p;
+	while (cin >> n >> r) {
+		vb ppl(n);
+		rep(i, 0, r) {
+			cin >> p;
+			ppl[p - 1] = true;
+		}
+
+		rep(i, 0, n) {
+			if (!ppl[i])
+				printf("%d ", i + 1);
+		}
+		print("%s", n == r ? "*" : "");
+	}
+}
