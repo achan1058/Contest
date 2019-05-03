c68e7c04d59c7be4fff7ef4463bafde6874e32f2
Aaron Chan <achan1058@hotmail.com>
2019-05-02 22:26:15 -0700

    ac

---
+++ UVa/12600/12696.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	double l, w, d, t;
+	int allowed = 0;
+	forX() {
+		cin >> l >> w >> d >> t;
+		if (((l > 56 || w > 45 || d > 25) && l + w + d > 125 + eps) || t > 7) {
+			print("0");
+		} else {
+			print("1");
+			allowed++;
+		}
+	}
+	print("%d", allowed);
+}
