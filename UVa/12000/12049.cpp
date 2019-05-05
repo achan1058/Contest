4e6810a1357b4d14d6b90b1935241f4aea994c56
Aaron Chan <achan1058@hotmail.com>
2019-05-05 15:56:03 -0700

    ac

---
+++ UVa/12000/12049.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, m, v, count = 0;
+		unordered_map<int, int> t1, t2;
+		cin >> n >> m;
+		rep(i, 0, n) {
+			cin >> v;
+			t1[v]++;
+		}
+		rep(i, 0, m) {
+			cin >> v;
+			t2[v]++;
+		}
+		irep(p, t1)
+			count += min(p.y, t2[p.x]);
+		print("%d", n + m - 2 * count);
+	}
+}
