6663ce53768d766d82f5b68a9312c523fcbbe63a
Aaron Chan <achan1058@hotmail.com>
2019-05-04 18:00:47 -0700

    ac

---
+++ UVa/12400/12405.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, ans = 0;
+		string s;
+		cin >> n >> s;
+		rep(i, 0, n) {
+			if (s[i] == '.') {
+				ans++;
+				i += 2;
+			}
+		}
+		printc("%d",ans);
+	}
+}
