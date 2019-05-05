d0f665c214ac0cfbb3471d751fe0d9ea6063b601
Aaron Chan <achan1058@hotmail.com>
2019-05-04 17:54:39 -0700

    ac

---
+++ UVa/12500/12541.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vector<pair<vi, string>> ppl;
+	forX() {
+		string s;
+		vi date(3);
+		cin >> s >> date[2] >> date[1] >> date[0];
+		ppl.pb({ date, s });
+	}
+	sort(all(ppl));
+	print("%s", ppl.back().y.c_str());
+	print("%s", ppl[0].y.c_str());
+}
