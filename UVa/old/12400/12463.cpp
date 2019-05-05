5736c5266f161b2c56d7e32072dd58dabe53765f
Aaron Chan <achan1058@hotmail.com>
2019-05-05 00:42:51 -0700

    Remove old ac

--- UVa/old/12400/12463.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-#include "header.h"
-
-int main() {
-	int a, b, c, d, e;
-	while (cin >> a >> b >> c >> d >> e && (a | b | c | d | e) != 0)
-		printf("%d\n", a * b * c * d * d * e * e);
-}
--- UVa/old/12400/12478.cpp
+++
@@ -1,6 +0,0 @@
-#include <bits/stdc++.h>
-#include "header.h"
-
-int main() {
-	printf("KABIR\n");
-}
