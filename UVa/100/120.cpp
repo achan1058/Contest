3ef9ff4365f59213e2c7bcca9d4bc77de6b80d07
Aaron Chan <achan1058@hotmail.com>
2019-05-06 08:39:10 -0700

    ac

---
+++ UVa/100/120.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+void pancake(vi& nums, int n) {
+	int best = 0;
+	rep(i, 1, n) {
+		if (nums[i] > nums[best])
+			best = i;
+	}
+	if (best == n - 1)
+		return;
+	if (best != 0) {
+		reverse(nums.begin(), nums.begin() + best + 1);
+		printf("%d ", sz(nums) - best);
+	}
+	reverse(nums.begin(), nums.begin() + n);
+	printf("%d ", sz(nums) - n + 1);
+}
+
+int main() {
+	string s;
+	while (getline(cin, s)) {
+		print("%s", s.c_str());
+		stringstream ss(s);
+		vi nums;
+		int t;
+		while (ss >> t)
+			nums.pb(t);
+		rrep(i, sz(nums), 2)
+			pancake(nums, i);
+		print("0");
+	}
+}
