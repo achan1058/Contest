ebdfd59d27ba874532270b0e2735d53198436382
Aaron Chan <achan1058@hotmail.com>
2019-05-06 09:13:01 -0700

    ac

---
+++ UVa/10200/10219.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k;
+	while (cin >> n >> k) {
+		double ans = 1;
+		rep(i, 0, k)
+			ans += log10(n - i) - log10(i + 1);
+		print("%d", (int)(ans));
+	}
+}
