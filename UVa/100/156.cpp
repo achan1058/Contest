07372c40aeb7d69ea22b06963476a47164a97941
Aaron Chan <achan1058@hotmail.com>
2019-05-07 23:35:30 -0700

    ac

---
+++ UVa/100/156.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	map<string, vs> anagram;
+	while (cin >> s && s != "#") {
+		string s2 = s;
+		transform(all(s2), s2.begin(), ::tolower);
+		sort(all(s2));
+		anagram[s2].pb(s);
+	}
+	vs ans;
+	irep(p, anagram) {
+		if (sz(p.y) == 1)
+			ans.pb(p.y[0]);
+	}
+	sort(all(ans));
+	irep(a, ans)
+		print("%s", a.c_str());
+}
