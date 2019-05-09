139673f99187b2aa82481b112bbfe3e2836592ad
Aaron Chan <achan1058@hotmail.com>
2019-05-09 07:39:07 -0700

    ac

---
+++ UVa/11400/11428.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	map<int, pii> ans;
+	rrep(x, 101, 1) {
+		rep(y, 1, x)
+			ans[x * x * x - y * y * y] = { x, y };
+	}
+	int n;
+	while (cin >> n && n != 0) {
+		if (ans[n].x == 0)
+			print("No solution");
+		else
+			print("%d %d", ans[n].x, ans[n].y);
+	}
+}
