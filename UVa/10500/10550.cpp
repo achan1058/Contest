5061138e9394907307f4730849aafaa6216b2d64
Aaron Chan <achan1058@hotmail.com>
2019-05-09 00:08:14 -0700

    ac

---
+++ UVa/10500/10550.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int a, b, c, d;
+	while (cin >> a >> b >> c >> d && (a | b | c | d) != 0) {
+		int ans = 1080;
+		ans += (40 + a - b) % 40 * 9;
+		ans += (40 + c - b) % 40 * 9;
+		ans += (40 + c - d) % 40 * 9;
+		print("%d", ans);
+	}
+}
