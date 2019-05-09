f2a5640a581d7361eaa53894fde945a0e9ebdf3b
Aaron Chan <achan1058@hotmail.com>
2019-05-09 08:44:34 -0700

    ac

---
+++ UVa/700/755.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	map<char, int> cmap = {
+		{'A', '2'},
+		{'B', '2'},
+		{'C', '2'},
+		{'D', '3'},
+		{'E', '3'},
+		{'F', '3'},
+		{'G', '4'},
+		{'H', '4'},
+		{'I', '4'},
+		{'J', '5'},
+		{'K', '5'},
+		{'L', '5'},
+		{'M', '6'},
+		{'N', '6'},
+		{'O', '6'},
+		{'P', '7'},
+		{'R', '7'},
+		{'S', '7'},
+		{'T', '8'},
+		{'U', '8'},
+		{'V', '8'},
+		{'W', '9'},
+		{'X', '9'},
+		{'Y', '9'}
+	};
+	forX() {
+		int n;
+		string s;
+		map<string, int> m;
+		cin >> n;
+		rep(i, 0, n) {
+			cin >> s;
+			string s2;
+			irep(c, s) {
+				if (c >= '0' && c <= '9')
+					s2 += c;
+				else if (c >= 'A' && c <= 'Z')
+					s2 += cmap[c];
+			}
+			m[s2]++;
+		}
+
+		bool dup = false;
+		printX("");
+		irep(p, m) {
+			if (p.y > 1) {
+				print("%s-%s %d", p.x.substr(0, 3).c_str(), p.x.substr(3, 4).c_str(), p.y);
+				dup = true;
+			}
+		}
+		if (!dup)
+			print("No duplicates.");
+	}
+}
