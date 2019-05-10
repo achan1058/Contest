7bd2de066818711467c3cea06945f4d550dbdd97
Aaron Chan <achan1058@hotmail.com>
2019-05-10 00:10:12 -0700

    ac

---
+++ UVa/11500/11586.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		getline(cin, s);
+		stringstream ss(s);
+		int males = 0, count = 0;
+		while (ss >> s) {
+			if (s == "MM")
+				males++;
+			else if (s == "FF")
+				males--;
+			count++;
+		}
+		print("%s", males == 0 && count >= 2 ? "LOOP" : "NO LOOP");
+	}
+}
