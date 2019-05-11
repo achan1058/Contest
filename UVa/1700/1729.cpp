4b581ff6cb5889e212ff347102a2ee42f30cb600
Aaron Chan <achan1058@hotmail.com>
2019-05-11 08:42:48 -0700

    ac

---
+++ UVa/1700/1729.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		cin >> s;
+		vi tally(26);
+		irep(c, s)
+			tally[c - 'a']++;
+		int ans = inf;
+		irep(t, tally)
+			ans = min(t, ans);
+		printc("%d", ans);
+	}
+}
