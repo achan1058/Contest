095a4286a9bf0344e98ecd94a66ead98b014a9ce
Aaron Chan <achan1058@hotmail.com>
2019-05-10 22:10:26 -0700

    ac

---
+++ UVa/10200/10226.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int total = 0;
+		string s;
+		map<string, int> count;
+		if (X == 1)
+			getline(cin, s);
+		while (getline(cin, s) && s != "") {
+			count[s]++;
+			total++;
+		}
+
+		printX("");
+		irep(p, count)
+			print("%s %.4f", p.x.c_str(), 100. * p.y / total);
+	}
+}
