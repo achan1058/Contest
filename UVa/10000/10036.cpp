b70508e0e6a712ab29707b2040215e7d955f2f34
Aaron Chan <achan1058@hotmail.com>
2019-05-10 22:00:15 -0700

    ac

---
+++ UVa/10000/10036.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int pmod(int n, int m) {
+	return (n % m + m) % m;
+}
+
+int main() {
+	forX() {
+		int n, m, t;
+		cin >> n >> m;
+		vb hit(m);
+		cin >> t;
+		hit[pmod(t, m)] = true;
+		rep(i, 1, n) {
+			vb hit2(m);
+			cin >> t;
+			rep(j, 0, m) {
+				if (hit[j]) {
+					hit2[pmod(j + t, m)] = true;
+					hit2[pmod(j - t, m)] = true;
+				}
+			}
+			hit = hit2;
+		}
+		print("%s", hit[0] ? "Divisible" : "Not divisible");
+	}
+}
