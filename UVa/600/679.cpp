6c60c972ef63edfc71360497f946ec712c10ee6a
Aaron Chan <achan1058@hotmail.com>
2019-05-10 23:49:31 -0700

    ac

---
+++ UVa/600/679.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, d, ans = 1;
+		cin >> d >> n;
+		n--;
+		rep(i, 0, d - 1) {
+			ans *= 2;
+			ans += n % 2;
+			n /= 2;
+		}
+		print("%d", ans);
+	}
+}
