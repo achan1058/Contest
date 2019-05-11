eb29aa5f9197b9291a35d45d3b103a4bc7c6635c
Aaron Chan <achan1058@hotmail.com>
2019-05-11 08:30:45 -0700

    ac

---
+++ UVa/1700/1726.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, d, l;
+		char c;
+		cin >> n;
+		vi inc(10), cor(10);
+		rep(i, 0, n) {
+			cin >> d >> l >> c;
+			if (l == 1 && c == 'c')
+				cor[d - 1]++;
+			else if (l == 0 && c == 'i')
+				inc[d - 1]++;
+		}
+		int ans = 0;
+		drep(i, j, 10, i)
+			ans += inc[j] * cor[i];
+		print("%d", ans);
+	}
+}
