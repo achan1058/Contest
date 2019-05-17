b040cdf50a88b2eba1aaadad52e3bf6b69934f54
Aaron Chan <achan1058@hotmail.com>
2019-05-17 09:08:54 -0700

    ac

---
+++ Euler/1/31.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	vi coins = { 1, 2, 5, 10, 20, 50, 100, 200 };
+	vi nums(201);
+	nums[0] = 1;
+	irep(c, coins) {
+		rep(j, c, 201)
+			nums[j] += nums[j - c];
+	}
+	print("%d", nums[200]);
+}
