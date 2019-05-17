9b17b5688df5f88ea2262bb76d96c24f009d2b29
Aaron Chan <achan1058@hotmail.com>
2019-05-17 08:04:55 -0700

    ac

---
+++ Euler/1/5.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int prod(int n) {
+	int ans = 1;
+	rep(i, 1, n + 1) {
+		ans /= gcd(ans, i);
+		ans *= i;
+	}
+	return ans;
+}
+
+int main() {
+	assert(prod(10) == 2520);
+	print("%d", prod(20));
+}
