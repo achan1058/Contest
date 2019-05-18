d893931f7edc6af85508e38eabe44a2171d6b270
Aaron Chan <achan1058@hotmail.com>
2019-05-18 00:44:56 -0700

    ac

---
+++ Euler/1/12.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+#include "header.h"
+
+ll tri(int n) {
+	for (ll i = 1; ; i++) {
+		ll t = i * (i + 1) / 2;
+		if (numFactors(factor(t)) > n)
+			return t;
+	}
+}
+
+int main() {
+	assert(tri(5) == 28);
+	print("%lld", tri(500));
+}
