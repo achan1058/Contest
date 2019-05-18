c9a05a2f91e1cb186225beb90d244cfdbabb9411
Aaron Chan <achan1058@hotmail.com>
2019-05-18 00:41:12 -0700

    ac

---
+++ Euler/1/8.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+ll prod(int n, const string& s) {
+	ll ans = 0;
+	rep(i, n, sz(s) + 1) {
+		ll p = 1;
+		rep(j, i - n, i)
+			p *= s[j] - '0';
+		ans = max(ans, p);
+	}
+	return ans;
+}
+
+int main() {
+	fstream fin("Euler/input/8.txt");
+	string digits, s;
+	while (fin >> s)
+		digits += s;
+	assert(prod(4, digits) == 5832);
+	print("%lld", prod(13, digits));
+}
