2f5c9833be3784fd99c5b8adcfabcba52b1772b5
Aaron Chan <achan1058@hotmail.com>
2019-05-20 08:28:00 -0700

    ac

---
+++ UVa/11900/11991.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, q, v, p;
+	while (cin >> n >> q) {
+		unordered_map<int, vi> pos;
+		rep(i, 0, n) {
+			cin >> v;
+			pos[v].pb(i + 1);
+		}
+		rep(i, 0, q) {
+			cin >> p >> v;
+			print("%d", p > sz(pos[v]) ? 0 : pos[v][p - 1]);
+		}
+	}
+}
