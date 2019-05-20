ab33fe4faa9a506c49fdf1417228d030276cc036
Aaron Chan <achan1058@hotmail.com>
2019-05-20 00:13:01 -0700

    Changed 10074 to use largestRectangle().

--- Library/dp_algs.h
+++ Library/dp_algs.h
@@ -69,6 +69,34 @@ pair<T, vector<int>> lis(const T& s, bool nondecrease = false) {
 	return{ result, index };
 }
 
+// returns maximum rectangle under histogram, given by the range [left, right)
+template<class T>
+tuple<T, int, int> largestRectangle(vector<T>& s) {
+	int n = s.size(), left = 0, right = 0;
+	stack<pair<int, T>> st;
+	T best = 0;
+	st.push({ -1, -1 });
+	s.push_back(0);
+
+	for (int i = 0; i <= n; i++) {
+		int l = i;
+		while (st.top().second >= s[i]) {
+			l = st.top().first;
+			T h = st.top().second;
+			int w = i - l;
+			st.pop();
+			if (w * h > best) {
+				best = w * h;
+				left = l;
+				right = i;
+			}
+		}
+		st.push({ l, s[i] });
+	}
+	s.pop_back();
+	return{ best, left, right };
+}
+
 template<class T>
 vector<int> kmp_pre(const T& p) {
 	int n = p.size(), start = 0;
--- UVa/10000/10074.cpp
+++ UVa/10000/10074.cpp
@@ -1,28 +1,24 @@
 #include <bits/stdc++.h>
+#include "dp_algs.h"
 #include "header.h"
 
 int main() {
 	int r, c;
 	while (cin >> r >> c && (r | c) != 0) {
-		vvi grid = mi(r, c, 0), tally = mi(r, c + 1, 0);
+		vvi grid = mi(r, c, 0);
 		read(grid);
-		rep(i, 0, r) {
-			rep(j, 0, c) {
-				if (grid[i][j] == 0)
-					tally[i][j + 1] = tally[i][j] + 1;
-			}
+		drep(i, j, r, c) {
+			if (i == 0)
+				grid[0][j] = 1 - grid[i][j];
+			else if (grid[i][j] == 1)
+				grid[i][j] = 0;
+			else
+				grid[i][j] = grid[i - 1][j] + 1;
 		}
 
 		int best = 0;
-		rep(j, 1, c + 1) {
-			rep(i1, 0, r) {
-				int height = tally[i1][j];
-				rep(i2, i1, r) {
-					height = min(height, tally[i2][j]);
-					best = max(best, height * (i2 - i1 + 1));
-				}
-			}
-		}
+		rep(i, 0, r)
+			best = max(best, get<0>(largestRectangle(grid[i])));
 		print("%d", best);
 	}
 }
