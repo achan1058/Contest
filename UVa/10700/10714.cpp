dae31ff7f4d0d9d33c14317c6668d687a04539b8
Aaron Chan <achan1058@hotmail.com>
2019-05-21 08:57:55 -0700

    ac

---
+++ UVa/10700/10714.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int l, n, mn = 0, mx = 0, v;
+		cin >> l >> n;
+		rep(i, 0, n) {
+			cin >> v;
+			mn = max(mn, min(v, l - v));
+			mx = max(mx, max(v, l - v));
+		}
+		print("%d %d", mn, mx);
+	}
+}
