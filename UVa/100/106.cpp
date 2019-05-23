131a5889e5e59b02e9a02bca4b4dee3966d26f38
Aaron Chan <achan1058@hotmail.com>
2019-05-23 00:07:45 -0700

    ac

---
+++ UVa/100/106.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	while (cin >> n) {
+		vb rel(n + 1, true);
+		int ans1 = 0, ans2 = 0;
+		for (int a = 1; 2 * a * a <= n; a++) {
+			for (int b = a + 1; a * a + b * b <= n; b += 2) {
+				if (gcd(a, b) != 1)
+					continue;
+				int x = b * b - a * a;
+				int y = 2 * a * b;
+				int z = b * b + a * a;
+				ans1++;
+
+				for (int k = 1; k * z <= n; k++)
+					rel[k * x] = rel[k * y] = rel[k * z] = false;
+			}
+		}
+		rep(i, 1, n + 1) {
+			if (rel[i])
+				ans2++;
+		}
+		print("%d %d", ans1, ans2);
+	}
+}
