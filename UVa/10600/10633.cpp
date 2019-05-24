db738144db6d4714aa0de41e07a5936c4c31ab8f
Aaron Chan <achan1058@hotmail.com>
2019-05-24 08:47:58 -0700

    ac

---
+++ UVa/10600/10633.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	ll n;
+	while (cin >> n && n != 0) {
+		if (n % 9 == 0)
+			printf("%lld ", n + n / 9 - 1);
+		print("%lld", n + n / 9);
+	}
+}
