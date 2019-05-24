31037c22ea6add9a5f2e44d6c2d65983f0d3e48d
Aaron Chan <achan1058@hotmail.com>
2019-05-24 00:42:46 -0700

    ac

---
+++ UVa/1200/1203.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	int id, t;
+	priority_queue<tuple<int, int, int>> q;
+	while (cin >> s && s != "#") {
+		cin >> id >> t;
+		q.push({ -t, -id, -t });
+	}
+	cin >> t;
+	rep(i, 0, t) {
+		auto v = q.top();
+		q.pop();
+		print("%d", -get<1>(v));
+		q.push({ get<0>(v) + get<2>(v), get<1>(v), get<2>(v) });
+	}
+}
