0da7fb62c038abbef94cc57df6c4813822b35630
Aaron Chan <achan1058@hotmail.com>
2019-05-25 13:55:42 -0700

    ac

---
+++ UVa/11500/11572.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, v, ans = 0;
+		queue<int> q;
+		unordered_set<int> s;
+		cin >> n;
+		rep(i, 0, n) {
+			cin >> v;
+			while (s.find(v) != s.end()) {
+				int t = q.front();
+				q.pop();
+				s.erase(t);
+			}
+			q.push(v);
+			s.insert(v);
+			ans = max(ans, sz(q));
+		}
+		print("%d", ans);
+	}
+}
