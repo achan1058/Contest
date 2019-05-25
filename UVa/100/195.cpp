89c686d3d86b7a276f76966d8b666e7879c460c4
Aaron Chan <achan1058@hotmail.com>
2019-05-24 22:37:26 -0700

    ac

---
+++ UVa/100/195.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		string s;
+		cin >> s;
+		auto comp = [](char c1, char c2) {
+			char d1 = tolower(c1), d2 = tolower(c2);
+			return d1 == d2 ? c1 < c2 : d1 < d2;
+		};
+		sort(all(s), comp);
+		do {
+			print("%s", s.c_str());
+		} while (next_permutation(all(s), comp));
+	}
+}
