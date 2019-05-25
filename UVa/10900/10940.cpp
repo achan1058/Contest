6a3d102cbe9701dfc4f3241b15a4d33cff73ca8d
Aaron Chan <achan1058@hotmail.com>
2019-05-25 10:59:03 -0700

    ac

---
+++ UVa/10900/10940.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+// n people, every k'th person is selected
+// if n = 5 and k = 2, selects 1, 3, 0, 4 remain 2
+vi joseph(int n, int k) {
+	vi ans = { -1, 0 };
+	for (int i = 2; i <= n; i++)
+		ans.pb((ans.back() + k) % i);
+	return ans;
+}
+
+int main() {
+	vi ans = joseph(500000, 2);
+	int n;
+	while (cin >> n && n != 0)
+		print("%d", ans[n - 1] + 2);
+}
