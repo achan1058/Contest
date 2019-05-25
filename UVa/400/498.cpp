bbf25be7e913af7037bb274ac6930c8c1311afef
Aaron Chan <achan1058@hotmail.com>
2019-05-24 23:44:50 -0700

    ac

---
+++ UVa/400/498.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	while (getline(cin, s)) {
+		vl coeff;
+		ll t;
+
+		{
+			stringstream ss(s);
+			while (ss >> t)
+				coeff.pb(t);
+		}
+
+		getline(cin, s);
+		stringstream ss(s);
+		bool first = true;
+		while (ss >> t) {
+			ll ans = 0;
+			irep(c, coeff) {
+				ans *= t;
+				ans += c;
+			}
+			printf("%s%lld", first ? "" : " ", ans);
+			first = false;
+		}
+		print("");
+	}
+}
