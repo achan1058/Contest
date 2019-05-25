4fd49529505334d1a874c309d8d54b94d732b984
Aaron Chan <achan1058@hotmail.com>
2019-05-24 22:09:05 -0700

    ac

---
+++ UVa/600/642.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	map<string, vs> dict;
+	while (cin >> s && s != "XXXXXX") {
+		string d = s;
+		sort(all(d));
+		dict[d].pb(s);
+	}
+	irep(d, dict)
+		sort(all(d.second));
+
+	while (cin >> s && s != "XXXXXX") {
+		sort(all(s));
+		if (dict[s].empty()) {
+			print("NOT A VALID WORD");
+		} else {
+			irep(d, dict[s])
+				print("%s", d.c_str());
+		}
+		print("******");
+	}
+}
