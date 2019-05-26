404baafd17caa35fa7200fa04535db6d403dcd66
Aaron Chan <achan1058@hotmail.com>
2019-05-25 22:15:45 -0700

    ac

---
+++ UVa/400/457.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string code = " .xW";
+	forX() {
+		vi dense(10), plate(42);
+		read(dense);
+		plate[20] = 1;
+
+		printX("");
+		rep(i, 0, 50) {
+			vi p(42);
+			rep(j, 1, 41) {
+				printf("%c", code[plate[j]]);
+				p[j] = dense[plate[j - 1] + plate[j] + plate[j + 1]];
+			}
+			print("");
+			plate = p;
+		}
+	}
+}
