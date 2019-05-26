d25a7503c85f94292225ea78aed7aa70e02251b1
Aaron Chan <achan1058@hotmail.com>
2019-05-25 18:55:20 -0700

    ac

---
+++ UVa/10700/10721.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k, m;
+	while (cin >> n >> k >> m) {
+		vl nums(n + m);
+		nums[0] = 1;
+		rep(i, 0, k) {
+			vl nums2(n + m);
+			drep(j, l, n, m)
+				nums2[j + l + 1] += nums[j];
+			nums = nums2;
+		}
+		print("%lld", nums[n]);
+	}
+}
