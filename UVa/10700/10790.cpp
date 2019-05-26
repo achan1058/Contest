ef0263736e18babf37f7c55921f474a647742cfc
Aaron Chan <achan1058@hotmail.com>
2019-05-25 18:14:02 -0700

    ac

---
+++ UVa/10700/10790.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	ll a, b;
+	whileX(cin >> a >> b && (a | b) != 0) {
+		a = a * (a - 1) / 2;
+		b = b * (b - 1) / 2;
+		printc("%lld", a * b);
+	}
+}
