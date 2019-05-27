aaa89bcba1086634859589ef7f3d5c2c69c0e848
Aaron Chan <achan1058@hotmail.com>
2019-05-26 22:38:05 -0700

    ac

---
+++ UVa/500/516.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+#include "header.h"
+
+int main() {
+	string s;
+	while (getline(cin, s) && s != "0") {
+		int p, e, n = 1;
+		stringstream ss(s);
+		while (ss >> p >> e) {
+			rep(i, 0, e)
+				n *= p;
+		}
+		auto ans = tally(factor(n - 1));
+		bool first = true;
+		for (auto it = ans.rbegin(); it != ans.rend(); it++) {
+			printf("%s%d %d", first ? "" : " ", it->first, it->second);
+			first = false;
+		}
+		print("");
+	}
+}
