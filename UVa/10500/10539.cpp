f29c82038f394ea1418adc39f91cc539735be648
Aaron Chan <achan1058@hotmail.com>
2019-05-26 21:52:20 -0700

    ac

---
+++ UVa/10500/10539.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+#include "header.h"
+
+int main() {
+	vl primepow;
+	vi primes = sieve(1000000);
+	irep(p, primes) {
+		for (ll t = ll(p) * p; t <= 1000000000000; t *= p)
+			primepow.pb(t);
+	}
+	sort(all(primepow));
+	
+	ll l, h;
+	forX() {
+		cin >> l >> h;
+		print("%d", upper_bound(all(primepow), h) - lower_bound(all(primepow), l));
+	}
+}
