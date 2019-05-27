d04aa6d9352c81f47b34108920ac3780240c3586
Aaron Chan <achan1058@hotmail.com>
2019-05-27 10:26:49 -0700

    Removed unused labels from header, as all functions are now used.

--- Library/header.h
+++ Library/header.h
@@ -12,14 +12,14 @@ typedef vector<bool> vb;
 typedef vector<double> vd;
 typedef vector<vd> vvd;
 typedef vector<vb> vvb;
-typedef vector<vs> vvs; // unused
+typedef vector<vs> vvs;
 typedef vector<vl> vvl;
 template<class T> inline istream& read(vector<T>& v) { for (auto& p : v) cin >> p; return cin; }
 template<class T> inline istream& read(vector<vector<T>>& v) { for (auto& p : v) read(p); return cin; }
 
 int inf = 0x3f3f3f3f;
 double eps = 1e-8;
-ll mod = 1000000007ll; // unused
+ll mod = 1000000007ll;
 
 #define rep(k, a, b) for (int k = (a); k < int(b); k++)
 #define irep(k, a) for (auto& k : (a))
@@ -31,11 +31,11 @@ ll mod = 1000000007ll; // unused
 #define drep(i, j, a, b) for (int i = 0; i < (a); i++) for (int j = 0; j < (b); j++)
 #define md(r, c, v) vvd(r, vd(c, v))
 #define mb(r, c, v) vvb(r, vb(c, v))
-#define ms(r, c, v) vvs(r, vs(c, v)) // unused
-#define ml(r, c, v) vvl(r, vl(c, v)) // unused
+#define ms(r, c, v) vvs(r, vs(c, v))
+#define ml(r, c, v) vvl(r, vl(c, v))
 #define mc(r, c, v) vs(r, string(c, v))
-#define add(i, j) ((i) + (j)) % mod // unused
-#define mul(i, j) ((i) * (j)) % mod // unused
+#define add(i, j) ((i) + (j)) % mod
+#define mul(i, j) ((i) * (j)) % mod
 #define bits(n) int(__builtin_popcount(n))
 #define gcd(a, b) abs(__gcd(a, b))
 #define forX() int XT; string XS; cin >> XT; getline(cin, XS); for (int X = 1; X <= XT; X++)
