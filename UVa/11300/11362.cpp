814a5062b2c8f3b85a6a44346bc6ae3c282425e4
Aaron Chan <achan1058@hotmail.com>
2019-05-26 19:26:54 -0700

    ac

---
+++ UVa/11300/11362.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n;
+		cin >> n;
+		vs nums(n);
+		read(nums);
+		sort(all(nums));
+
+		bool good = true;
+		rep(i, 1, n) {
+			if (nums[i].find(nums[i - 1]) == 0) {
+				good = false;
+				break;
+			}
+		}
+
+		print("%s", good ? "YES" : "NO");
+	}
+}
