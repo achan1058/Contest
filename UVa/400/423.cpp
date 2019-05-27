1269b2c3b5e00bb0b4ba7e41d471fe2e1d7c7da4
Aaron Chan <achan1058@hotmail.com>
2019-05-26 17:46:55 -0700

    ac

---
+++ UVa/400/423.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "shortest_path.h"
+#include "header.h"
+
+int main() {
+	int n;
+	string s;
+	cin >> n;
+	adj_list<int> graph(n);
+	drep(i, j, n - 1, i + 1) {
+		cin >> s;
+		if (s == "x")
+			continue;
+		graph[i + 1].pb({ j, stoi(s) });
+		graph[j].pb({ i + 1, stoi(s) });
+	}
+
+	vi dist = dijkstra(graph, 0).first;
+	int ans = 0;
+	irep(d, dist)
+		ans = max(ans, d);
+	print("%d", ans);
+}
