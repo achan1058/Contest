5616fd35112716398300732b843909fdc93748b6
Aaron Chan <achan1058@hotmail.com>
2019-05-27 18:00:24 -0700

    ac

---
+++ UVa/100/111.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include "dp_algs.h"
+#include "header.h"
+
+vi rd(int n, const string& s) {
+	vi nums(n);
+	stringstream ss(s);
+	rep(i, 0, n) {
+		int v;
+		ss >> v;
+		nums[v - 1] = i;
+	}
+	return nums;
+}
+
+int main() {
+	int n;
+	string s;
+	vi ans, guess;
+	while (getline(cin, s)) {
+		if (s.find(' ') == string::npos) {
+			n = stoi(s);
+			getline(cin, s);
+			ans = rd(n, s);
+			continue;
+		}
+		guess = rd(n, s);
+		print("%d", sz(lcs(guess, ans).first));
+	}
+}
