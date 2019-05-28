3046fcda2e2be6d4798640798f912a92c10c55de
Aaron Chan <achan1058@hotmail.com>
2019-05-27 21:13:36 -0700

    ac

---
+++ UVa/100/193.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+vi cur, best;
+vvi adj;
+vb used;
+int numv;
+
+void backtrack(int n = 0) {
+	if (n == numv) {
+		if (sz(cur) > sz(best))
+			best = cur;
+		return;
+	}
+
+	bool good = true;
+	irep(v, adj[n]) {
+		if (used[v]) {
+			good = false;
+			break;
+		}
+	}
+
+	if (good) {
+		cur.pb(n);
+		used[n] = true;
+		backtrack(n + 1);
+		used[n] = false;
+		cur.pop_back();
+	}
+	backtrack(n + 1);
+}
+
+int main() {
+	forX() {
+		int e, v1, v2;
+		cin >> numv >> e;
+		adj = vvi(numv);
+		used = vb(numv);
+		best.clear();
+		rep(i, 0, e) {
+			cin >> v1 >> v2;
+			adj[v1 - 1].pb(v2 - 1);
+			adj[v2 - 1].pb(v1 - 1);
+		}
+		backtrack();
+		print("%d", sz(best));
+		rep(i, 0, sz(best))
+			printf("%d%c", best[i] + 1, i == sz(best) - 1 ? '\n' : ' ');
+	}
+}
