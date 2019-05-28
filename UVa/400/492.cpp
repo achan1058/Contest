9b7cc11220ac74e0f76daf5c73fa74fef450f1f7
Aaron Chan <achan1058@hotmail.com>
2019-05-27 23:25:40 -0700

    ac

---
+++ UVa/400/492.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+void process(string s) {
+	char c = tolower(s[0]);
+	if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')
+		s = s.substr(1) + s[0];
+	printf("%say", s.c_str());
+}
+
+int main() {
+	string s;
+	while (getline(cin, s)) {
+		string t;
+		irep(c, s) {
+			if (isalpha(c)) {
+				t += c;
+			} else {
+				if (!t.empty()) {
+					process(t);
+					t.clear();
+				}
+				printf("%c", c);
+			}
+		}
+		if (!t.empty())
+			process(t);
+		print("");
+	}
+}
