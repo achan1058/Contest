f97b6fe2cee256cc60dd8eac6b1c0944d31b23e6
Aaron Chan <achan1058@hotmail.com>
2019-06-01 14:47:51 -0700

    ac

---
+++ UVa/11000/11078.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	forX() {
+		int n, ans = -inf, cur, v;
+		cin >> n >> cur;
+		rep(i, 1, n) {
+			cin >> v;
+			ans = max(ans, cur - v);
+			cur = max(cur, v);
+		}
+		print("%d", ans);
+	}
+}
