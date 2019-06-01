7be210651587141cf8095486f2107ebd70c2749c
Aaron Chan <achan1058@hotmail.com>
2019-06-01 10:40:25 -0700

    Added and tested binary search with 714.

---
+++ Library/binary_search.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// Finds x such that func(x) = 0. Returns left most of such x if func is monotone.
+// test floating point.
+template<class T, class U = double>
+T binarySearch(U func(T value), T m = 0, T M = inf, T bound = eps, bool precise = true, T not_found = -inf) {
+	auto sgn = [](T v) { return (T(0) < v) - (v < T(0)); };
+	int m_sign = sgn(func(m));
+	T mid = (m + M) / 2;
+
+	if (m_sign == 0)
+		return m;
+	else if (m_sign == sgn(func(M)))
+		return not_found;
+	do {
+		int mid_sign = sgn(func(mid));
+		if (mid_sign == m_sign)
+			m = mid;
+		else
+			M = mid;
+
+		mid = (m + M) / 2;
+	} while (bound < mid - m);
+	return (sgn(func(M)) == 0 || !precise ? M : not_found);
+}
---
+++ UVa/700/714.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include "binary_search.h"
+#include "header.h"
+
+vl books;
+vvl best;
+int n, m;
+int greedy(ll k) {
+	int t = n - 1;
+	vvl ans(m);
+	rrep(i, m - 1, 0) {
+		ll cur = 0;
+		if (books[t] > k)
+			return -1;
+		while (t >= i && cur + books[t] <= k) {
+			ans[i].pb(books[t]);
+			cur += books[t];
+			t--;
+		}
+	}
+	if (t >= 0)
+		return -1;
+	best = ans;
+	return 0;
+}
+
+int main() {
+	forX() {
+		cin >> n >> m;
+		best.clear();
+		books.resize(n);
+		read(books);
+		binarySearch(greedy, 0ll, 5000000000ll);
+		rep(i, 0, m) {
+			rrep(j, sz(best[i]) - 1, 0)
+				printf("%lld%s", best[i][j], j == 0 ? "" : " ");
+			printf("%s", i == m - 1 ? "\n" : " / ");
+		}
+	}
+}
