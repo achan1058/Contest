a1ebb3ae5180245082c2df8c4e59b20b7fa4529d
Aaron Chan <achan1058@hotmail.com>
2019-06-02 07:18:31 -0700

    ac

---
+++ UVa/10300/10392.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "primes.h"
+#include "header.h"
+
+int main() {
+	ll n;
+	while (cin >> n && n >= 0) {
+		vl primes = factor(n);
+		irep(p, primes)
+			print("    %lld", p);
+		print("");
+	}
+}
