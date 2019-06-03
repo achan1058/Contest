b1f3c67d80a4bcf5dbd9693ea5da8ff736b1f5b9
Aaron Chan <achan1058@hotmail.com>
2019-06-02 22:12:53 -0700

    ac

---
+++ UVa/11000/11054.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	while (cin >> n && n != 0) {
+		ll ans = 0;
+		int t = 0, v;
+		rep(i, 0, n) {
+			cin >> v;
+			t += v;
+			ans += abs(t);
+		}
+		print("%lld", ans);
+	}
+}
