4fca5cc9233108d0757e9412ad47f76543414b49
Aaron Chan <achan1058@hotmail.com>
2019-06-02 22:25:13 -0700

    ac

---
+++ UVa/10100/10182.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+vi dx = { -1, 0, 1, 1, 0, -1 }, dy = { 0, -1, -1, 0, 1, 1 };
+int main() {
+	int x = 0, y = 1, cur = 5;
+	vector<pii> ans(100001);
+	ans[1] = { 0, 0 };
+	rep(i, 2, 100001) {
+		ans[i] = { x, y };
+		if (x == 0 || (y == 0 && x < 0) || (y == 1 && x > 0) || x == -y)
+			cur = (cur + 1) % 6;
+		x += dx[cur];
+		y += dy[cur];
+	}
+
+	int n;
+	while (cin >> n)
+		print("%d %d", ans[n].first, ans[n].second);
+}
