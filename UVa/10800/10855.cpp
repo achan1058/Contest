93c12d6c055fd971f8d73fced9ae0edd863146a2
Aaron Chan <achan1058@hotmail.com>
2019-06-03 08:41:13 -0700

    ac

---
+++ UVa/10800/10855.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, k;
+	while (cin >> n >> k && (n | k) != 0) {
+		vs grid(n), small(k);
+		read(grid);
+		read(small);
+		vi ans(4);
+		rep(t, 0, 4) {
+			drep(i, j, n - k + 1, n - k + 1) {
+				bool good = true;
+				drep(x, y, k, k) {
+					if (small[x][y] != grid[x + i][y + j]) {
+						good = false;
+						goto end;
+					}
+				}
+			end:;
+				ans[t] += (good ? 1 : 0);
+			}
+			drep(i, j, (k + 1) / 2, k / 2) {
+				swap(small[i][j], small[k - 1 - j][i]);
+				swap(small[k - 1 - i][k - 1 - j], small[k - 1 - j][i]);
+				swap(small[k - 1 - i][k - 1 - j], small[j][k - i - 1]);
+			}
+		}
+		print("%d %d %d %d", ans[0], ans[1], ans[2], ans[3]);
+	}
+}
