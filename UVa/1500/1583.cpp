e654c36b2637c1d29a649adef1591c006a31106e
Aaron Chan <achan1058@hotmail.com>
2019-06-04 10:15:45 -0700

    ac

---
+++ UVa/1500/1583.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int gen(int n) {
+	int ans = n;
+	while (n > 0) {
+		ans += n % 10;
+		n /= 10;
+	}
+	return ans;
+}
+
+int main() {
+	vi ans(100100, inf);
+	rrep(i, 100000, 1)
+		ans[gen(i)] = i;
+	forX() {
+		int n;
+		cin >> n;
+		print("%d", ans[n] == inf ? 0 : ans[n]);
+	}
+}
