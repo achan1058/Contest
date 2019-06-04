0efefb810105a36119f60ed0bf5741e16cf5cd0c
Aaron Chan <achan1058@hotmail.com>
2019-06-03 23:38:13 -0700

    ac

---
+++ UVa/10900/10919.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, m;
+	whileX(cin >> n >> m && n != 0) {
+		int v;
+		set<int> s;
+		rep(i, 0, n) {
+			cin >> v;
+			s.insert(v);
+		}
+
+		bool good = true;
+		rep(i, 0, m) {
+			int c, r;
+			cin >> c >> r;
+			rep(j, 0, c) {
+				cin >> v;
+				if (s.find(v) != s.end())
+					r--;
+			}
+			if (r > 0)
+				good = false;
+		}
+		print("%s", good ? "yes" : "no");
+	}
+}
