8982ac12808f66710a2139fa40e2ed18b9f53ca5
Aaron Chan <achan1058@hotmail.com>
2019-06-05 08:19:04 -0700

    ac

---
+++ Codeforces/100/120/122A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+vi lucky = { 4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777 };
+int main() {
+	int n;
+	bool good = false;
+	cin >> n;
+	irep(l, lucky) {
+		if (n % l == 0)
+			good = true;
+	}
+	print("%s", good ? "YES" : "NO");
+}
