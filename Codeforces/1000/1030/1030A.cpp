82b85ff2dcdc89a7d414f05ca10566840343fa0f
Aaron Chan <achan1058@hotmail.com>
2019-06-05 00:39:53 -0700

    ac

---
+++ Codeforces/1000/1030/1030A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, v;
+	bool easy = true;
+	cin >> n;
+	rep(i, 0, n) {
+		cin >> v;
+		easy = easy && v == 0;
+	}
+	print("%s", easy ? "EASY" : "HARD");
+}
