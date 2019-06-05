f345142611d8081407e59622cacfacab5218730b
Aaron Chan <achan1058@hotmail.com>
2019-06-04 22:40:15 -0700

    ac

---
+++ Codeforces/400/469/469A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, t, v;
+	cin >> n;
+	vb good(n);
+	rep(i, 0, 2) {
+		cin >> t;
+		rep(j, 0, t) {
+			cin >> v;
+			if (!good[v - 1]) {
+				good[v - 1] = true;
+				n--;
+			}
+		}
+	}
+	print("%s", n == 0 ? "I become the guy." : "Oh, my keyboard!");
+}
