24210e58b4232e6d3401f953ba28d0e8fb588868
Aaron Chan <achan1058@hotmail.com>
2019-06-05 08:33:23 -0700

    ac

---
+++ Codeforces/500/510/510A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int r, c;
+	cin >> r >> c;
+	vs grid = mc(r, c, '.');
+	for (int i = 0; i < r; i += 2)
+		grid[i] = string(c, '#');
+	for (int i = 1; i < r; i += 4)
+		grid[i].back() = '#';
+	for (int i = 3; i < r; i += 4)
+		grid[i][0] = '#';
+	irep(s, grid)
+		print("%s", s.c_str());
+}
