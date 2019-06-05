8e0bf4de35b9994d9707e75810530177b644c9f6
Aaron Chan <achan1058@hotmail.com>
2019-06-04 22:29:33 -0700

    ac

---
+++ Codeforces/100/110/110A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s;
+	int num = 0;
+	cin >> s;
+	irep(c, s) {
+		if (c == '4' || c == '7')
+			num++;
+	}
+	print("%s", num == 4 || num == 7 ? "YES" : "NO");
+}
