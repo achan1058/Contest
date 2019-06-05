4677a3436d82598a0cec830989a1235235404aeb
Aaron Chan <achan1058@hotmail.com>
2019-06-04 22:10:29 -0700

    ac

---
+++ Codeforces/200/260/266B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, t;
+	string s;
+	cin >> n >> t >> s;
+	rep(k, 0, t) {
+		rep(i, 0, n - 1) {
+			if (s[i] == 'B' && s[i + 1] == 'G') {
+				swap(s[i], s[i + 1]);
+				i++;
+			}
+		}
+	}
+	printf("%s", s.c_str());
+}
