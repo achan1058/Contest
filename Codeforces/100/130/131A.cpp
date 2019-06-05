d8687b056b3140801e70a1a9a910cfca50269639
Aaron Chan <achan1058@hotmail.com>
2019-06-05 08:08:36 -0700

    ac

---
+++ Codeforces/100/130/131A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+bool islock(const string& s) {
+	rep(i, 1, sz(s)) {
+		if (islower(s[i]))
+			return false;
+	}
+	return true;
+}
+
+int main() {
+	string s;
+	cin >> s;
+	if (islock(s)) {
+		irep(c, s) {
+			if (isupper(c))
+				c = tolower(c);
+			else
+				c = toupper(c);
+		}
+	}
+	print("%s", s.c_str());
+}
