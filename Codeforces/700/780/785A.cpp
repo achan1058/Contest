a219243dbce1f1ef236db0a26729e1cd5a72131e
Aaron Chan <achan1058@hotmail.com>
2019-06-05 08:43:02 -0700

    ac

---
+++ Codeforces/700/780/785A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+map<string, int> face = {
+	{"Tetrahedron", 4},
+	{"Cube", 6},
+	{"Octahedron", 8},
+	{"Dodecahedron", 12},
+	{"Icosahedron", 20},
+};
+
+int main() {
+	int n, ans = 0;
+	string s;
+	cin >> n;
+	rep(i, 0, n) {
+		cin >> s;
+		ans += face[s];
+	}
+	print("%d", ans);
+}
