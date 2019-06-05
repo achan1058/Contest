383dc023729fbe757aa7f2beff2905e5a03bb99c
Aaron Chan <achan1058@hotmail.com>
2019-06-05 08:27:18 -0700

    ac

---
+++ Codeforces/400/460/467A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, p, q, ans = 0;
+	cin >> n;
+	rep(i, 0, n) {
+		cin >> p >> q;
+		ans += (p + 2 <= q ? 1 : 0);
+	}
+	print("%d", ans);
+}
