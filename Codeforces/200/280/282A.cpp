73a21dee153dabeb95615fbe68f2021c6caa838f
Aaron Chan <achan1058@hotmail.com>
2019-06-05 22:53:27 -0700

    ac

---
+++ Codeforces/200/280/282A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, ans = 0;
+	string s;
+	cin >> n;
+	rep(i, 0, n) {
+		cin >> s;
+		if (s[1] == '+')
+			ans++;
+		else
+			ans--;
+	}
+	print("%d", ans);
+}
