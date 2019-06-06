03868f598916c5580bea7056451c8a51f7a344aa
Aaron Chan <achan1058@hotmail.com>
2019-06-05 23:08:42 -0700

    ac

---
+++ Codeforces/100/110/112A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	string s1, s2;
+	cin >> s1 >> s2;
+	transform(all(s1), s1.begin(), ::tolower);
+	transform(all(s2), s2.begin(), ::tolower);
+	print("%d", s1 < s2 ? -1 : s1 > s2 ? 1 : 0);
+}
