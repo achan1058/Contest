e336dbe77d0d4b239d1faa4475519c25ffb714b5
Aaron Chan <achan1058@hotmail.com>
2019-06-06 08:43:11 -0700

    ac

---
+++ Codeforces/800/850/855A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n;
+	cin >> n;
+	set<string> used;
+	string s;
+	rep(i, 0, n) {
+		cin >> s;
+		print("%s", used.find(s) == used.end() ? "NO" : "YES");
+		used.insert(s);
+	}
+}
