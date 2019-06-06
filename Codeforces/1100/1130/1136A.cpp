2d8095c59483b81e23c947443e99334d1a871c13
Aaron Chan <achan1058@hotmail.com>
2019-06-06 00:06:31 -0700

    ac

---
+++ Codeforces/1100/1130/1136A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, t;
+	cin >> n;
+	vector<pii> book(n);
+	irep(b, book)
+		cin >> b.first >> b.second;
+
+	cin >> t;
+	rep(i, 0, n) {
+		if (t >= book[i].first && t <= book[i].second) {
+			print("%d", n - i);
+			break;
+		}
+	}
+}
