40ee05c8e2300bf85f7bc83a0312ca89b101ca91
Aaron Chan <achan1058@hotmail.com>
2019-06-06 22:54:43 -0700

    ac

---
+++ Codeforces/100/110/116A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, ans = 0, a, b, cur = 0;
+	cin >> n;
+	rep(i, 0, n) {
+		cin >> a >> b;
+		cur += b - a;
+		ans = max(ans, cur);
+	}
+	print("%d", ans);
+}
