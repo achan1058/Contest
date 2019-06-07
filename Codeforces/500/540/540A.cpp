ba70e055ce92a63b94551011fe63b1aff0a18228
Aaron Chan <achan1058@hotmail.com>
2019-06-06 20:20:02 -0700

    ac

---
+++ Codeforces/500/540/540A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, ans = 0;
+	string s1, s2;
+	cin >> n >> s1 >> s2;
+	rep(i, 0, n) {
+		int d = abs(s1[i] - s2[i]);
+		ans += min(d, 10 - d);
+	}
+	print("%d", ans);
+}
