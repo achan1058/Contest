d4ccfe73de0aec8f94aaeac7c7b6e5234a41818a
Aaron Chan <achan1058@hotmail.com>
2019-06-07 23:13:11 -0700

    ac

---
+++ Codeforces/300/340/344A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, ans = 0;
+	cin >> n;
+	string s;
+	char c = ' ';
+	rep(i, 0, n) {
+		cin >> s;
+		if (s[0] != c)
+			ans++;
+		c = s[0];
+	}
+	print("%d", ans);
+}
