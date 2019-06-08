264066610752ee4d0239dcc80cf31a8c375d2d1c
Aaron Chan <achan1058@hotmail.com>
2019-06-07 23:50:46 -0700

    ac

---
+++ Codeforces/900/990/996A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+vi nums = { 100, 20, 10, 5, 1 };
+int main() {
+	int n, ans = 0;
+	cin >> n;
+	irep(v, nums) {
+		ans += n / v;
+		n %= v;
+	}
+	print("%d", ans);
+}
