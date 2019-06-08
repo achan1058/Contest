eaa45bed691de4b0a0e2bb232a23eb80b2fb3cc6
Aaron Chan <achan1058@hotmail.com>
2019-06-07 23:18:25 -0700

    ac

---
+++ Codeforces/600/670/677A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, h, ans = 0, v;
+	cin >> n >> h;
+	rep(i, 0, n) {
+		cin >> v;
+		ans += (v > h ? 2 : 1);
+	}
+	print("%d", ans);
+}
