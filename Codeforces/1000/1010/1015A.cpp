7a8d9f280400479457343249619a539ad468dd9e
Aaron Chan <achan1058@hotmail.com>
2019-06-08 07:00:35 -0700

    ac

---
+++ Codeforces/1000/1010/1015A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int n, m, l, r;
+	vi ans;
+	cin >> n >> m;
+	vb points(m + 1);
+	rep(i, 0, n) {
+		cin >> l >> r;
+		rep(j, l, r + 1)
+			points[j] = true;
+	}
+	rep(i, 1, m + 1) {
+		if (!points[i])
+			ans.pb(i);
+	}
+	print("%d", sz(ans));
+	rep(i, 0, sz(ans))
+		printf("%d%c", ans[i], i == sz(ans) - 1 ? '\n' : ' ');
+}
