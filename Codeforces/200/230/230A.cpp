7c87e5f754310485096a4a0b8286e2ccfa4c10e6
Aaron Chan <achan1058@hotmail.com>
2019-06-17 00:00:50 -0700

    ac

---
+++ Codeforces/200/230/230A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "header.h"
+
+int main() {
+	int s, n;
+	cin >> s >> n;
+	vector<pii> dragons(n);
+	irep(d, dragons)
+		cin >> d.first >> d.second;
+	sort(all(dragons));
+
+	bool good = true;
+	irep(d, dragons) {
+		if (s <= d.first) {
+			good = false;
+			break;
+		}
+		s += d.second;
+	}
+	print(good ? "YES" : "NO");
+}
