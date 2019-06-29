53f515e7ba65962503d3fd2de5505c54c5dc8e50
Aaron Chan <achan1058@hotmail.com>
2019-06-28 23:26:02 -0700

    Remove old ac.

--- UVa/old/10200/10297.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-#include "header.h"
-
-int main() {
-	double D, V;
-	while (cin >> D >> V && (D != 0 || V != 0))
-		printf("%.3f\n", pow(D * D * D - 6 * V / M_PI, 1. / 3));
-}
