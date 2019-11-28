4d037a500c60044ebcbc2030a05ca93a0ab2f1c7
Aaron Chan <achan1058@hotmail.com>
2019-11-28 07:33:37 -0800

    Remove old ac.

--- UVa/old/11200/11231.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-#include "header.h"
-
-int main() {
-	int row, col, cell;
-	while (cin >> row >> col >> cell && (row | col | cell) != 0)
-		printf("%d\n", ((row - 7) * (col - 7) + cell) / 2);
-}
